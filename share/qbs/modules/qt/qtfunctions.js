2f4879b2856a06cfdaacaab42dc6d81371fb37d1
Joerg Bornemann <joerg.bornemann@nokia.com>
2012-02-20 10:32:55 +0100

    fix Qt's library names for MinGW
    Change-Id: I58eba319e1e7b0559b796ae0161654689fa480d5
    Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

--- share/qbs/modules/qt/qtfunctions.js
+++ share/qbs/modules/qt/qtfunctions.js
@@ -2,11 +2,13 @@
 
 function getLibraryName(qtModule, targetOS, debugInfo)
 {
-    var isUnix = (targetOS == 'linux' || targetOS == 'mac')
     var libName
-    if (isUnix)
+    if (targetOS === 'linux' || targetOS === 'mac') {
         libName = qtModule
-    else if (targetOS == 'windows')
-        libName = qtModule + (debugInfo ? 'd' : '') + '4.lib'
+    } else if (targetOS === 'windows') {
+        libName = qtModule + (debugInfo ? 'd' : '') + '4'
+        if (qbs.toolchain !== "mingw")
+            libName += '.lib'
+    }
     return libName
 }
