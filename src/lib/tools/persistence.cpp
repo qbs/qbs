1aa36dc259ede1408901909b49f30ed862406e6a
Joerg Bornemann <joerg.bornemann@nokia.com>
2012-02-24 18:11:56 +0100

    increase build graph version number
    Change-Id: Ia6df4ff811670d21551e0763578246fdd4b73ad7
    Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

--- src/lib/tools/persistence.cpp
+++ src/lib/tools/persistence.cpp
@@ -42,7 +42,7 @@
 
 namespace qbs {
 
-static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE0_0_1__2";
+static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE0_0_1__3";
 
 PersistentPool::PersistentPool()
 {
