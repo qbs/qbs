qbs/qbs

commit 392fdd492dc0cf00c63344a929ce603856299772

Make used environment and configuration available
Make the used environment variables as well as the configuration
available as part of the qbs::Project API.

This allows creator to be more intelligent about when a reparse is
needed due to environment and/or configuration changes.

Change-Id: If6c7d5886db353137ddc346667690e3db97c0670
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/api/project.h
@@ -100,6 +100,9 @@ public:
 
     bool operator==(const Project &other) const { return d.data() == other.d.data(); }
 
+    QVariantMap projectConfiguration() const;
+    QHash<QString, QString> usedEnvironment() const;
+
 private:
     Project();
     Project(const Internal::ResolvedProjectPtr &internalProject, const Internal::Logger &logger);

src/lib/api/project.cpp
@@ -514,4 +514,14 @@ void Project::updateTimestamps(const QList<ProductData> &products)
                                          d->logger);
 }
 
+QVariantMap Project::projectConfiguration() const
+{
+    return d->internalProject->buildConfiguration();
+}
+
+QHash<QString, QString> Project::usedEnvironment() const
+{
+    return d->internalProject->usedEnvironment;
+}
+
 } // namespace qbs

commit 53da25e9f894471c487c91cb476d2b48b1984ecd

Fix build.
Was broken by 392fdd492dc0cf00c63344a929ce603856299772.

Change-Id: Iaf4d968de8bdb59353aebbfcfe29b7752d8fd573
Reviewed-by: Tobias Hunger <tobias.hunger@digia.com>

src/lib/api/project.h
@@ -33,7 +33,9 @@
 #include "../tools/qbs_export.h"
 
 #include <QExplicitlySharedDataPointer>
+#include <QHash>
 #include <QList>
+#include <QVariantMap>
 
 QT_BEGIN_NAMESPACE
 class QObject;