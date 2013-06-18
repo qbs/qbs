qbs/qbs

commit aa74bd71956464adc12249055f8f9e29fe3bd5ad

Move expanding of configuration to SetupProjectParameters
This allows Creator to call this explicitly which will then allow to
compare the expanded settings with those used by the project before.

Change-Id: I68aab208c4d1832c502667207054da2bcb8fa84b
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/language/tst_language.cpp
@@ -116,9 +116,9 @@ void TestLanguage::handleInitCleanupDataTags(const char *projectFileName, bool *
 QString TestLanguage::buildDir(const SetupProjectParameters &params) const
 {
     return FileInfo::resolvePath(params.buildRoot(),
-        getConfigProperty(params.buildConfiguration(),
+        getConfigProperty(params.buildConfigurationTree(),
                           QStringList() << "qbs" << "profile").toString() + QLatin1Char('-')
-            + getConfigProperty(params.buildConfiguration(),
+            + getConfigProperty(params.buildConfigurationTree(),
                             QStringList() << "qbs" << "buildVariant").toString());
 }
 
@@ -137,7 +137,7 @@ void TestLanguage::initTestCase()
     loader = new Loader(m_engine, m_logger);
     loader->setSearchPaths(QStringList()
                            << QLatin1String(SRCDIR "/../../share/qbs"));
-    QVariantMap buildConfig = defaultParameters.buildConfiguration();
+    QVariantMap buildConfig = defaultParameters.buildConfigurationTree();
     setConfigProperty(buildConfig, QStringList() << "qbs" << "targetOS", "linux");
     setConfigProperty(buildConfig, QStringList() << "qbs" << "profile", "qbs_autotests");
     defaultParameters.setBuildConfiguration(buildConfig);
@@ -681,8 +681,7 @@ void TestLanguage::jsImportUsedInMultipleScopes()
     bool exceptionCaught = false;
     try {
         QVariantMap customBuildConfig = defaultParameters.buildConfiguration();
-        setConfigProperty(customBuildConfig, QStringList() << "qbs" << "buildVariant",
-                          buildVariant);
+        customBuildConfig.insert(QLatin1String("qbs.buildVariant"), buildVariant);
         SetupProjectParameters params = defaultParameters;
         params.setProjectFilePath(testProject("jsimportsinmultiplescopes.qbs"));
         params.setBuildConfiguration(customBuildConfig);

src/app/qbs/commandlinefrontend.cpp
@@ -108,7 +108,12 @@ void CommandLineFrontend::start()
         params.setLogElapsedTime(m_parser.logTime());
         foreach (const QVariantMap &buildConfig, m_parser.buildConfigurations()) {
             params.setBuildConfiguration(buildConfig);
-            SetupProjectJob * const job = Project::setupProject(params, m_settings,
+
+            Error err = params.expandBuildConfiguration(m_settings);
+            if (!err.entries().isEmpty())
+                throw err;
+
+            SetupProjectJob * const job = Project::setupProject(params,
                     ConsoleLogger::instance().logSink(), this);
             connectJob(job);
             m_resolveJobs << job;

src/lib/api/project.cpp
@@ -46,10 +46,8 @@
 #include <tools/installoptions.h>
 #include <tools/preferences.h>
 #include <tools/processresult.h>
-#include <tools/profile.h>
 #include <tools/scannerpluginmanager.h>
 #include <tools/scripttools.h>
-#include <tools/settings.h>
 #include <tools/setupprojectparameters.h>
 #include <tools/qbsassert.h>
 
@@ -290,64 +288,6 @@ Project &Project::operator=(const Project &other)
     return *this;
 }
 
-// Generates a full build configuration from user input, using the settings.
-static QVariantMap expandBuildConfiguration(const QVariantMap &buildConfig, Settings *settings)
-{
-    QVariantMap expandedConfig = buildConfig;
-
-    const QString buildVariant = expandedConfig.value(QLatin1String("qbs.buildVariant")).toString();
-    if (buildVariant.isEmpty())
-        throw Error(Tr::tr("No build variant set."));
-    if (buildVariant != QLatin1String("debug") && buildVariant != QLatin1String("release")) {
-        throw Error(Tr::tr("Invalid build variant '%1'. Must be 'debug' or 'release'.")
-                    .arg(buildVariant));
-    }
-
-    // Fill in buildCfg in this order (making sure not to overwrite a key already set by a previous stage)
-    // 1) Things specified on command line (already in buildCfg at this point)
-    // 2) Everything from the profile key
-    QString profileName = expandedConfig.value("qbs.profile").toString();
-    if (profileName.isNull()) {
-        profileName = settings->defaultProfile();
-        if (profileName.isNull()) {
-            const QString profileNames = settings->profiles().join(QLatin1String(", "));
-            throw Error(Tr::tr("No profile given and no default profile set.\n"
-                    "Either set the configuration value 'defaultProfile' to a valid profile name\n"
-                    "or specify the profile with the command line parameter 'profile:name'.\n"
-                    "The following profiles are available:\n%1").arg(profileNames));
-        }
-        expandedConfig.insert("qbs.profile", profileName);
-    }
-
-    // (2)
-    const Profile profile(profileName, settings);
-    const QStringList profileKeys = profile.allKeys(Profile::KeySelectionRecursive);
-    if (profileKeys.isEmpty())
-        throw Error(Tr::tr("Unknown or empty profile '%1'.").arg(profileName));
-    foreach (const QString &profileKey, profileKeys) {
-        if (!expandedConfig.contains(profileKey))
-            expandedConfig.insert(profileKey, profile.value(profileKey));
-    }
-
-    if (!expandedConfig.value("qbs.buildVariant").isValid())
-        throw Error(Tr::tr("Property 'qbs.buildVariant' missing in build configuration."));
-
-    foreach (const QString &property, expandedConfig.keys()) {
-        QStringList nameElements = property.split('.');
-        if (nameElements.count() > 2) { // ### workaround for submodules being represented internally as a single module of name "module/submodule" rather than two nested modules "module" and "submodule"
-            QStringList allButLast = nameElements;
-            allButLast.removeLast();
-            QStringList newElements(allButLast.join("/"));
-            newElements.append(nameElements.last());
-            nameElements = newElements;
-        }
-        setConfigProperty(expandedConfig, nameElements, expandedConfig.value(property));
-        expandedConfig.remove(property);
-    }
-
-    return expandedConfig;
-}
-
 /*!
  * \brief Sets up a \c Project from a source file, possibly re-using previously stored information.
  * The function will finish immediately, returning a \c SetupProjectJob which can be used to
@@ -357,16 +297,13 @@ static QVariantMap expandBuildConfiguration(const QVariantMap &buildConfig, Sett
  *       Similarly, the value of \c parameters.searchPaths will not have an effect if
  *       a stored build graph is available.
  */
-SetupProjectJob *Project::setupProject(const SetupProjectParameters &_parameters,
-                                       Settings *settings, ILogSink *logSink, QObject *jobOwner)
+SetupProjectJob *Project::setupProject(const SetupProjectParameters &parameters,
+                                       ILogSink *logSink, QObject *jobOwner)
 {
     Logger logger(logSink);
     SetupProjectJob * const job = new SetupProjectJob(logger, jobOwner);
     try {
-        loadPlugins(_parameters.pluginPaths(), logger);
-        SetupProjectParameters parameters = _parameters;
-        parameters.setBuildConfiguration(expandBuildConfiguration(
-                                             parameters.buildConfiguration(), settings));
+        loadPlugins(parameters.pluginPaths(), logger);
         job->resolve(parameters);
     } catch (const Error &error) {
         // Throwing from here would complicate the API, so let's report the error the same way

src/lib/api/project.h
@@ -68,7 +68,7 @@ class QBS_EXPORT Project
     friend uint qHash(const Project &p);
 public:
     static SetupProjectJob *setupProject(const SetupProjectParameters &parameters,
-                                         Settings *settings, ILogSink *logSink, QObject *jobOwner);
+                                         ILogSink *logSink, QObject *jobOwner);
 
     Project(const Project &other);
     Project &operator=(const Project &other);

src/lib/buildgraph/buildgraph.cpp
@@ -483,7 +483,7 @@ BuildGraphLoader::LoadResult BuildGraphLoader::load(const SetupProjectParameters
     m_result = LoadResult();
     m_evalContext = evalContext;
 
-    const QString projectId = TopLevelProject::deriveId(parameters.buildConfiguration());
+    const QString projectId = TopLevelProject::deriveId(parameters.buildConfigurationTree());
     const QString buildDir
             = TopLevelProject::deriveBuildDirectory(parameters.buildRoot(), projectId);
     const QString buildGraphFilePath
@@ -493,7 +493,7 @@ BuildGraphLoader::LoadResult BuildGraphLoader::load(const SetupProjectParameters
     m_logger.qbsDebug() << "[BG] trying to load: " << buildGraphFilePath;
     if (!pool.load(buildGraphFilePath))
         return m_result;
-    if (!isConfigCompatible(parameters.buildConfiguration(), pool.headData().projectConfig)) {
+    if (!isConfigCompatible(parameters.buildConfigurationTree(), pool.headData().projectConfig)) {
         m_logger.qbsDebug() << "[BG] Cannot use stored build graph: "
                                "Incompatible project configuration.";
         return m_result;

src/lib/language/loader.cpp
@@ -92,14 +92,14 @@ TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &parameters)
     // we have enough information.
     if (m_progressObserver) {
         m_progressObserver->initialize(Tr::tr("Resolving project for configuration %1")
-                .arg(TopLevelProject::deriveId(parameters.buildConfiguration())), 1);
+                .arg(TopLevelProject::deriveId(parameters.buildConfigurationTree())), 1);
     }
     ModuleLoaderResult loadResult
             = m_moduleLoader->load(parameters.projectFilePath(),
-                                   parameters.buildConfiguration(),
+                                   parameters.buildConfigurationTree(),
                                    true);
     return m_projectResolver->resolve(loadResult, parameters.buildRoot(),
-                                      parameters.buildConfiguration(),
+                                      parameters.buildConfigurationTree(),
                                       parameters.environment());
 }
 

src/lib/tools/setupprojectparameters.cpp
@@ -28,6 +28,12 @@
 ****************************************************************************/
 #include "setupprojectparameters.h"
 
+#include <logging/translator.h>
+#include <tools/profile.h>
+#include <tools/qbsassert.h>
+#include <tools/scripttools.h>
+#include <tools/settings.h>
+
 namespace qbs {
 namespace Internal {
 
@@ -50,6 +56,7 @@ public:
     QStringList searchPaths;
     QStringList pluginPaths;
     QVariantMap buildConfiguration;
+    mutable QVariantMap buildConfigurationTree;
     bool ignoreDifferentProjectFilePath;
     bool dryRun;
     bool logElapsedTime;
@@ -160,10 +167,109 @@ QVariantMap SetupProjectParameters::buildConfiguration() const
 
 /*!
  * Sets the collection of properties to use for resolving the project.
+ *
+ * Keys are expected to be in dotted syntax (e.g. Qt.declarative.qmlDebugging) that is
+ * used by "qbs config".
  */
 void SetupProjectParameters::setBuildConfiguration(const QVariantMap &buildConfiguration)
 {
+    // warn if somebody tries to set a build configuration tree:
+    for (QVariantMap::const_iterator i = buildConfiguration.constBegin();
+         i != buildConfiguration.constEnd(); ++i) {
+        QBS_ASSERT(i.value().type() != QVariant::Map, return);
+    }
     d->buildConfiguration = buildConfiguration;
+    d->buildConfigurationTree.clear();
+}
+
+/*!
+ * \brief Returns the build configuration in tree form.
+ * \return the tree form of the build configuration.
+ */
+QVariantMap SetupProjectParameters::buildConfigurationTree() const
+{
+    if (!d->buildConfigurationTree.isEmpty() && !d->buildConfiguration.isEmpty())
+        return d->buildConfigurationTree;
+
+    QVariantMap result;
+    foreach (const QString &property, d->buildConfiguration.keys()) {
+        QStringList nameElements = property.split('.');
+        if (nameElements.count() > 2) { // ### workaround for submodules being represented internally as a single module of name "module/submodule" rather than two nested modules "module" and "submodule"
+            QStringList allButLast = nameElements;
+            allButLast.removeLast();
+            QStringList newElements(allButLast.join("/"));
+            newElements.append(nameElements.last());
+            nameElements = newElements;
+        }
+        Internal::setConfigProperty(result, nameElements, d->buildConfiguration.value(property));
+    }
+    d->buildConfigurationTree = result;
+    return result;
+}
+
+/*!
+ * \brief Expands the build configuration based on the given settings.
+ *
+ * Expansion is the process by which the build configuration is completed based on the given
+ * settings. E.g. the information configured in a profile is filled into the build
+ * configuration by this step.
+ *
+ * This method returns an Error. The list of entries in this error will be empty is the
+ * expansion was successful.
+ */
+Error SetupProjectParameters::expandBuildConfiguration(Settings *settings)
+{
+    Error err;
+
+    // Generates a full build configuration from user input, using the settings.
+    QVariantMap expandedConfig = d->buildConfiguration;
+
+    const QString buildVariant = expandedConfig.value(QLatin1String("qbs.buildVariant")).toString();
+    if (buildVariant.isEmpty())
+        throw Error(Internal::Tr::tr("No build variant set."));
+    if (buildVariant != QLatin1String("debug") && buildVariant != QLatin1String("release")) {
+        err.append(Internal::Tr::tr("Invalid build variant '%1'. Must be 'debug' or 'release'.")
+                   .arg(buildVariant));
+        return err;
+    }
+
+    // Fill in buildCfg in this order (making sure not to overwrite a key already set by a previous stage)
+    // 1) Things specified on command line (already in buildCfg at this point)
+    // 2) Everything from the profile key
+    QString profileName = expandedConfig.value("qbs.profile").toString();
+    if (profileName.isNull()) {
+        profileName = settings->defaultProfile();
+        if (profileName.isNull()) {
+            const QString profileNames = settings->profiles().join(QLatin1String(", "));
+            err.append(Internal::Tr::tr("No profile given and no default profile set.\n"
+                                        "Either set the configuration value 'defaultProfile' to a "
+                                        "valid profile name\n"
+                                        "or specify the profile with the command line parameter "
+                                        "'profile:name'.\n"
+                                        "The following profiles are available:\n%1")
+                       .arg(profileNames));
+            return err;
+        }
+        expandedConfig.insert("qbs.profile", profileName);
+    }
+
+    // (2)
+    const Profile profile(profileName, settings);
+    const QStringList profileKeys = profile.allKeys(Profile::KeySelectionRecursive);
+    if (profileKeys.isEmpty()) {
+        err.append(Internal::Tr::tr("Unknown or empty profile '%1'.").arg(profileName));
+        return err;
+    }
+    foreach (const QString &profileKey, profileKeys) {
+        if (!expandedConfig.contains(profileKey))
+            expandedConfig.insert(profileKey, profile.value(profileKey));
+    }
+
+    if (d->buildConfiguration != expandedConfig) {
+        d->buildConfigurationTree.clear();
+        d->buildConfiguration = expandedConfig;
+    }
+    return err;
 }
 
 /*!

src/lib/tools/setupprojectparameters.h
@@ -31,12 +31,17 @@
 
 #include "qbs_export.h"
 
+#include <tools/error.h>
+
 #include <QProcessEnvironment>
 #include <QSharedDataPointer>
 #include <QStringList>
 #include <QVariantMap>
 
 namespace qbs {
+
+class Settings;
+
 namespace Internal { class SetupProjectParametersPrivate; }
 
 class QBS_EXPORT SetupProjectParameters
@@ -62,6 +67,8 @@ public:
 
     QVariantMap buildConfiguration() const;
     void setBuildConfiguration(const QVariantMap &buildConfiguration);
+    QVariantMap buildConfigurationTree() const;
+    Error expandBuildConfiguration(Settings *settings);
 
     bool ignoreDifferentProjectFilePath() const;
     void setIgnoreDifferentProjectFilePath(bool doIgnore);

commit f2f5d60dafeeb7030b7155b7fa8437fc2dfe55f5

adjust tst_language to latest changes
SetupProjectParameters takes "flat" build configs now.
This fixes the soft assert in the language test that
was introduced in aa74bd71956464adc12249055f8f9e29fe3bd5ad.

Change-Id: Idddf7eca9b5fbac06ae1e56bc1b7e1a93003e75f
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/language/tst_language.cpp
@@ -138,8 +138,8 @@ void TestLanguage::initTestCase()
     loader->setSearchPaths(QStringList()
                            << QLatin1String(SRCDIR "/../../share/qbs"));
     QVariantMap buildConfig = defaultParameters.buildConfigurationTree();
-    setConfigProperty(buildConfig, QStringList() << "qbs" << "targetOS", "linux");
-    setConfigProperty(buildConfig, QStringList() << "qbs" << "profile", "qbs_autotests");
+    buildConfig.insert("qbs.targetOS", "linux");
+    buildConfig.insert("qbs.profile", "qbs_autotests");
     defaultParameters.setBuildConfiguration(buildConfig);
     QVERIFY(QFileInfo(m_wildcardsTestDirPath).isAbsolute());
 }