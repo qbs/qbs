qbs/qbs

commit e0a8ec897c15a471f69a7d8bf1dc15d4a361505f

allow invalid bindings in disabled groups
This fixes a regression introduced in
7f83ff024e1fe56c71d193a95cc9af988c12660d.

It's now valid again to do something like
Group {
   condition: qbs.targetOS.contains("haiku")
   cpp.specialHaikuOption: "yes, please"
}

Task-number: QBS-409

Change-Id: I693df9202603c9af7fd7482fa537ed1fb16591a7
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/language/moduleloader.cpp
@@ -393,6 +393,7 @@ void ModuleLoader::createAdditionalModuleInstancesInProduct(ProductContext *prod
 void ModuleLoader::handleGroup(ProductContext *productContext, Item *item)
 {
     checkCancelation();
+    checkItemCondition(item);
     propagateModulesFromProduct(productContext, item);
 }
 

src/lib/language/tst_language.cpp
@@ -640,7 +640,7 @@ void TestLanguage::invalidBindingInDisabledItem()
         TopLevelProjectPtr project = loader->loadProject(defaultParameters);
         QVERIFY(project);
         QHash<QString, ResolvedProductPtr> products = productsFromProject(project);
-        QCOMPARE(products.count(), 1);
+        QCOMPARE(products.count(), 2);
     }
     catch (const ErrorInfo &e) {
         exceptionCaught = true;

src/lib/language/testdata/invalidBindingInDisabledItem.qbs
@@ -2,7 +2,15 @@ import qbs 1.0
 
 Project {
     Product {
+        name: "product1"
         condition: false
         someNonsense: "Bitte stellen Sie die Tassen auf den Tisch."
+    }
+    Product {
+        name: "product2"
+        Group {
+            condition: false
+            moreNonsense: "Follen. Follen. Hünuntergefollen. Auf dön Töppüch."
+        }
     }
 }

commit acddeb82e5df0d8f947c3e02f5da64a351855023

fix regression introduced in e0a8ec89
Accessing module properties in Group.condition didn't work anymore. We
must first propagate the modules to the group, then evaluate the
condition.

Change-Id: Id3eb799faf91f449740c8213671e32af47683c3f
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/language/moduleloader.cpp
@@ -394,8 +394,8 @@ void ModuleLoader::createAdditionalModuleInstancesInProduct(ProductContext *prod
 void ModuleLoader::handleGroup(ProductContext *productContext, Item *item)
 {
     checkCancelation();
-    checkItemCondition(item);
     propagateModulesFromProduct(productContext, item);
+    checkItemCondition(item);
 }
 
 void ModuleLoader::handleArtifact(ProductContext *productContext, Item *item)

src/lib/language/tst_language.cpp
@@ -459,6 +459,8 @@ void TestLanguage::groupConditions_data()
             << 2 << (QList<bool>() << true << true);
     QTest::newRow("true_condition_from_project")
             << 2 << (QList<bool>() << true << true);
+    QTest::newRow("condition_accessing_module_property")
+            << 2 << (QList<bool>() << true << false);
     QTest::newRow("cleanup") << 0 << QList<bool>();
 }
 

src/lib/language/testdata/groupconditions.qbs
@@ -41,4 +41,13 @@ Project {
             files: ["main.cpp"]
         }
     }
+
+    Product {
+        name: "condition_accessing_module_property"
+        Group {
+            condition: qbs.targetOS.contains("narf")
+            files: ["main.cpp"]
+            qbs.install: false
+        }
+    }
 }