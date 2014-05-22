qbs/qbs

commit 6d5de545c4e67289f973cf6ba9bdbbaeb4eb4a99

Introduce sensible build directory values.
Currently, all products share the same build directory,
and rules are responsible for making their artifact file
paths unique, which is annoying for module authors and
introduces lots of code duplication.
Instead, make the product build directories unique and let
these directories be the base directory of relative artifact
file paths.
Also export the top-level build dir as project.buildDirectory
to the JavaScript side so rules can refer to that one if necessary.

Task-number: QBS-567
Change-Id: I257d37cad01ec6dece3e214799b917149677d80c
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

share/qbs/imports/qbs/BundleTools/bundle-tools.js
@@ -143,7 +143,7 @@ function frameworksFolderPath(product, version) {
 function infoPlistPath(product, version) {
     var path;
     if (product.type.contains("application"))
-        path = ".tmp/" + product.name;
+        path = ".tmp";
     else if (product.type.contains("frameworkbundle"))
         path = unlocalizedResourcesFolderPath(product, version);
     else if (product.type.contains("inapppurchase"))

doc/reference/items/product.qdoc
@@ -97,8 +97,9 @@
         \row
             \li destinationDirectory
             \li string
-            \li "."
-            \li The directory where the target artifacts will be located. Relative to the build directory.
+            \li product.buildDirectory
+            \li The directory where the target artifacts will be located. If a relative path is
+                given, the base directory will be \c project.buildDirectory.
         \row
             \li files
             \li stringList
@@ -132,7 +133,7 @@
                 Info.plist files in OS X and iOS application and framework bundles, for example.
     \endtable
 
-    The following properties are automatically set by \QBS and usually are not changed by the user:
+    The following properties are automatically set by \QBS and cannot be changed by the user:
 
     \table
         \header
@@ -141,12 +142,12 @@
             \li Description
         \row
             \li buildDirectory
-            \li string
+            \li path
             \li The build directory for this product. This is the directory where generated files
                 are placed.
         \row
             \li sourceDirectory
-            \li string
+            \li path
             \li The source directory for this product. This is the directory of the file where this
                 product is defined.
     \endtable

doc/reference/items/project.qdoc
@@ -62,6 +62,11 @@
             \li Default
             \li Description
         \row
+            \li buildDirectory
+            \li path
+            \li n/a
+            \li The build directory of the top-level project. This property is read-only.
+        \row
             \li name
             \li string
             \li basename of the file the project is defined in

doc/reference/items/rule.qdoc
@@ -51,7 +51,7 @@
 
         Artifact {
             fileTags: ['obj']
-            fileName: '.obj/' + product.name + '/' + input.baseDir + '/' + input.fileName + '.o'
+            fileName: '.obj/' + input.baseDir + '/' + input.fileName + '.o'
         }
 
         prepare: {

share/qbs/modules/cpp/GenericGCC.qbs
@@ -342,7 +342,7 @@ CppModule {
 
         Artifact {
             fileTags: ["obj"]
-            fileName: ".obj/" + product.name + "/" + input.baseDir + "/" + input.fileName + ".o"
+            fileName: ".obj/" + input.baseDir + "/" + input.fileName + ".o"
         }
 
         prepare: {

share/qbs/modules/cpp/msvc.js
@@ -82,7 +82,7 @@ function prepareCompiler(product, input, outputs, platformDefines, defines, incl
             // use PCH
             var pchHeaderName = FileInfo.toWindowsSeparators(pch);
             var pchName = FileInfo.toWindowsSeparators(ModUtils.moduleProperty(product, "precompiledHeaderDir")
-                + "\\.obj\\" + product.name + "\\" + product.name + "_" + tag + ".pch");
+                + "\\.obj\\" + product.name + "_" + tag + ".pch");
             args.push("/FI" + pchHeaderName);
             args.push("/Yu" + pchHeaderName);
             args.push("/Fp" + pchName);

share/qbs/modules/cpp/windows-mingw.qbs
@@ -40,7 +40,7 @@ GenericGCC {
         inputs: ["rc"]
 
         Artifact {
-            fileName: ".obj/" + product.name + "/" + input.baseDir.replace(':', '') + "/" + input.completeBaseName + "_res.o"
+            fileName: ".obj/" + input.baseDir.replace(':', '') + "/" + input.completeBaseName + "_res.o"
             fileTags: ["obj"]
         }
 

share/qbs/modules/cpp/windows-msvc.qbs
@@ -38,12 +38,12 @@ CppModule {
             fileName: {
                 var completeBaseName = FileInfo.completeBaseName(product.moduleProperty("cpp",
                         "cPrecompiledHeader"));
-                return ".obj/" + product.name + "/" + completeBaseName + '_c.obj'
+                return ".obj/" + completeBaseName + '_c.obj'
             }
         }
         Artifact {
             fileTags: ['c_pch']
-            fileName: ".obj/" + product.name + "/" + product.name + '_c.pch'
+            fileName: ".obj/" + product.name + '_c.pch'
         }
         prepare: {
             var platformDefines = ModUtils.moduleProperty(input, 'platformDefines');
@@ -65,12 +65,12 @@ CppModule {
             fileName: {
                 var completeBaseName = FileInfo.completeBaseName(product.moduleProperty("cpp",
                         "cxxPrecompiledHeader"));
-                return ".obj/" + product.name + "/" + completeBaseName + '_cpp.obj'
+                return ".obj/" + completeBaseName + '_cpp.obj'
             }
         }
         Artifact {
             fileTags: ['cpp_pch']
-            fileName: ".obj/" + product.name + "/" + product.name + '_cpp.pch'
+            fileName: ".obj/" + product.name + '_cpp.pch'
         }
         prepare: {
             var platformDefines = ModUtils.moduleProperty(input, 'platformDefines');
@@ -91,7 +91,7 @@ CppModule {
 
         Artifact {
             fileTags: ['obj']
-            fileName: ".obj/" + product.name + "/" + input.baseDir.replace(':', '') + "/" + input.fileName + ".obj"
+            fileName: ".obj/" + input.baseDir.replace(':', '') + "/" + input.fileName + ".obj"
         }
 
         prepare: {
@@ -201,7 +201,7 @@ CppModule {
         inputs: ["rc"]
 
         Artifact {
-            fileName: ".obj/" + product.name + "/" + input.baseDir.replace(':', '') + "/" + input.completeBaseName + ".res"
+            fileName: ".obj/" + input.baseDir.replace(':', '') + "/" + input.completeBaseName + ".res"
             fileTags: ["obj"]
         }
 

commit 794a64df2926874009c707f1b27cfe3ba33abd9b

fix Info.plist creation for application products
The Info.plist files for different application products must
be created in separate build directories.
This fixes a regression introduced in 6d5de54.

Change-Id: I0b65d2a16fa78811f946515355e173a2840cd18e
Reviewed-by: Jake Petroules <jake.petroules@petroules.com>

share/qbs/imports/qbs/BundleTools/bundle-tools.js
@@ -143,7 +143,7 @@ function frameworksFolderPath(product, version) {
 function infoPlistPath(product, version) {
     var path;
     if (product.type.contains("application"))
-        path = ".tmp";
+        path = ".tmp/" + product.name;
     else if (product.type.contains("frameworkbundle"))
         path = unlocalizedResourcesFolderPath(product, version);
     else if (product.type.contains("inapppurchase"))