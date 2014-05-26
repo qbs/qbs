qbs/qbs

commit 7f5d54454373c6e4e50daf70a2be6c58d736076f

move import of windows.js out of GenericGCC.qbs
Change-Id: I5429af5524cc4adbf889e305cafce55f409624f1
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

share/qbs/modules/cpp/GenericGCC.qbs
@@ -3,7 +3,6 @@ import qbs.File
 import qbs.FileInfo
 import qbs.ModUtils
 import qbs.Process
-import 'windows.js' as Windows
 import 'gcc.js' as Gcc
 import 'bundle-tools.js' as BundleTools
 import 'path-tools.js' as PathTools

share/qbs/modules/cpp/gcc.js
@@ -1,3 +1,5 @@
+var Windows = loadFile("windows.js");
+
 function linkerFlags(product, inputs)
 {
     var libraryPaths = ModUtils.moduleProperties(product, 'libraryPaths');

commit 479c4ab1ae1e68543846c9a0b1fa271ae80f9cd5

fix app linker rule for MinGW
This fixes a regression introduced in 7f5d5445.

Task-number: QBS-597
Change-Id: Ifa453e1de8f4d1857fd4c247bd359c6c605e4f79
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

share/qbs/modules/cpp/GenericGCC.qbs
@@ -3,6 +3,7 @@ import qbs.File
 import qbs.FileInfo
 import qbs.ModUtils
 import qbs.Process
+import 'windows.js' as Windows
 import 'gcc.js' as Gcc
 import 'path-tools.js' as PathTools
 