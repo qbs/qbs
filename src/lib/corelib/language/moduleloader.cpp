qbs/qbs

commit 9297ea217284e2279f2d4471b3f8fc754dc5fe71

remove superfluous item cache
ItemReader::m_itemCache was only used for module items,
and those are cached in the module loader.
There's no reason for a second cache.

Change-Id: I52f8e1ca12119dcd72f737479373022f01c88a51
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/corelib/language/moduleloader.cpp
@@ -107,7 +107,6 @@ ModuleLoaderResult ModuleLoader::load(const SetupProjectParameters &parameters)
     if (m_logger.traceEnabled())
         m_logger.qbsTrace() << "[MODLDR] load" << parameters.projectFilePath();
     m_parameters = parameters;
-    m_reader->clearItemCache();
     m_validItemPropertyNamesPerItem.clear();
     m_disabledItems.clear();
 
@@ -804,7 +803,7 @@ Item *ModuleLoader::loadModuleFile(ProductContext *productContext, const QString
 
     m_logger.qbsTrace() << "[LDR] loadModuleFile " << filePath;
     *cacheHit = false;
-    module = m_reader->readFile(filePath, true);
+    module = m_reader->readFile(filePath);
     if (!isBaseModule) {
         DependsContext dependsContext;
         dependsContext.product = productContext;

src/lib/corelib/language/itemreader.cpp
@@ -128,11 +128,6 @@ QStringList ItemReader::searchPaths() const
     return paths;
 }
 
-void ItemReader::clearItemCache()
-{
-    m_itemCache.clear();
-}
-
 void ItemReader::cacheDirectoryEntries(const QString &dirPath, const QStringList &entries)
 {
     m_directoryEntries.insert(dirPath, entries);
@@ -147,9 +142,9 @@ bool ItemReader::findDirectoryEntries(const QString &dirPath, QStringList *entri
     return true;
 }
 
-Item *ItemReader::readFile(const QString &filePath, bool enableItemCache)
+Item *ItemReader::readFile(const QString &filePath)
 {
-    Item * const item = internalReadFile(filePath, enableItemCache).rootItem;
+    Item * const item = internalReadFile(filePath).rootItem;
     return item;
 }
 
@@ -158,15 +153,8 @@ QSet<QString> ItemReader::filesRead() const
     return m_filesRead;
 }
 
-ItemReaderResult ItemReader::internalReadFile(const QString &filePath, bool enableItemCache)
+ItemReaderResult ItemReader::internalReadFile(const QString &filePath)
 {
-    ItemReaderResult result;
-    if (enableItemCache) {
-        result = m_itemCache.value(filePath);
-        if (result.rootItem)
-            return result;
-    }
-
     ASTCacheValue &cacheValue = (*m_astCache)[filePath];
     if (cacheValue.isValid()) {
         if (Q_UNLIKELY(cacheValue.isProcessing()))
@@ -197,16 +185,13 @@ ItemReaderResult ItemReader::internalReadFile(const QString &filePath, bool enab
         cacheValue.setAst(parser.ast());
     }
 
+    ItemReaderResult result;
     ItemReaderASTVisitor itemReader(this, &result);
     itemReader.setFilePath(QFileInfo(filePath).absoluteFilePath());
     itemReader.setSourceCode(cacheValue.code());
     cacheValue.setProcessingFlag(true);
     cacheValue.ast()->accept(&itemReader);
     cacheValue.setProcessingFlag(false);
-
-    if (enableItemCache)
-        m_itemCache.insert(filePath, result);
-
     return result;
 }
 

src/lib/corelib/language/itemreader.h
@@ -79,13 +79,12 @@ public:
     void popExtraSearchPaths();
     QStringList searchPaths() const;
 
-    void clearItemCache();
-    Item *readFile(const QString &filePath, bool enableItemCache = false);
+    Item *readFile(const QString &filePath);
 
     QSet<QString> filesRead() const;
 
 private:
-    ItemReaderResult internalReadFile(const QString &filePath, bool enableItemCache);
+    ItemReaderResult internalReadFile(const QString &filePath);
 
     void cacheDirectoryEntries(const QString &dirPath, const QStringList &entries);
     bool findDirectoryEntries(const QString &dirPath, QStringList *entries) const;
@@ -99,7 +98,6 @@ private:
 
     class ASTCache;
     ASTCache *m_astCache;
-    QHash<QString, ItemReaderResult> m_itemCache;
     QSet<QString> m_filesRead;
     QHash<QString, QStringList> m_directoryEntries;
 };

src/lib/corelib/language/itemreaderastvisitor.cpp
@@ -312,7 +312,7 @@ bool ItemReaderASTVisitor::visit(AST::UiObjectDefinition *ast)
     const QStringList fullTypeName = toStringList(ast->qualifiedTypeNameId);
     const QString baseTypeFileName = m_typeNameToFile.value(fullTypeName);
     if (!baseTypeFileName.isEmpty()) {
-        const ItemReaderResult baseFile = m_reader->internalReadFile(baseTypeFileName, false);
+        const ItemReaderResult baseFile = m_reader->internalReadFile(baseTypeFileName);
 
         inheritItem(item, baseFile.rootItem);
         if (baseFile.rootItem->m_file->m_idScope) {

commit f185005c0ff34b2e477b2d2159e58837474fc3cc

fix speed regression
Commit 9297ea217284e2279f2d4471b3f8fc754dc5fe71 introduced a speed
penalty. This is a quasi-revert of said commit.

Change-Id: Ia0b0741cf818a9867f127a25830630fb3ea352ff
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/corelib/language/moduleloader.cpp
@@ -112,6 +112,7 @@ ModuleLoaderResult ModuleLoader::load(const SetupProjectParameters &parameters)
         m_logger.qbsTrace() << "[MODLDR] load" << parameters.projectFilePath();
     m_parameters = parameters;
     m_validItemPropertyNamesPerItem.clear();
+    m_globalModuleItemCache.clear();
     m_disabledItems.clear();
 
     ModuleLoaderResult result;
@@ -866,7 +867,7 @@ Item *ModuleLoader::loadModuleFile(ProductContext *productContext, const QString
     if (m_logger.traceEnabled())
         m_logger.qbsTrace() << "[MODLDR] trying to load " << fullModuleName << " from " << filePath;
 
-    const ContextBase::ModuleItemCache::key_type cacheKey(filePath, productContext->profileName);
+    const ModuleItemCache::key_type cacheKey(filePath, productContext->profileName);
     Item *module = productContext->moduleItemCache.value(cacheKey);
     if (module) {
         m_logger.qbsTrace() << "[LDR] loadModuleFile cache hit for " << filePath;
@@ -883,7 +884,11 @@ Item *ModuleLoader::loadModuleFile(ProductContext *productContext, const QString
 
     m_logger.qbsTrace() << "[LDR] loadModuleFile " << filePath;
     *cacheHit = false;
-    module = m_reader->readFile(filePath);
+    module = m_globalModuleItemCache.value(cacheKey);
+    if (!module) {
+        module = m_reader->readFile(filePath);
+        m_globalModuleItemCache.insert(cacheKey, module);
+    }
     if (!isBaseModule) {
         DependsContext dependsContext;
         dependsContext.product = productContext;

src/lib/corelib/language/moduleloader.h
@@ -106,6 +106,8 @@ public:
     static QString fullModuleName(const QStringList &moduleName);
 
 private:
+    typedef QMap<QPair<QString, QString>, Item *> ModuleItemCache;
+
     class ContextBase
     {
     public:
@@ -116,7 +118,6 @@ private:
         Item *item;
         Item *scope;
         QStringList extraSearchPaths;
-        typedef QMap<QPair<QString, QString>, Item *> ModuleItemCache;
         ModuleItemCache moduleItemCache;
     };
 
@@ -195,8 +196,6 @@ private:
     static void overrideItemProperties(Item *item, const QString &buildConfigKey,
                                        const QVariantMap &buildConfig);
 
-
-
     ScriptEngine *m_engine;
     ItemPool *m_pool;
     Logger m_logger;
@@ -205,6 +204,7 @@ private:
     Evaluator *m_evaluator;
     QStringList m_moduleSearchPaths;
     QMap<QString, QStringList> m_moduleDirListCache;
+    ModuleItemCache m_globalModuleItemCache;
     QHash<Item *, QSet<QString> > m_validItemPropertyNamesPerItem;
     QSet<Item *> m_disabledItems;
     SetupProjectParameters m_parameters;