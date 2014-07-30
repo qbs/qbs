qbs/qbs

commit 32c4d3d7d26937c8f2a0f2f99add2eb0b0f1e503

fix calculation of added/removed artifacts
We kept lists of added and removed artifacts in ProductBuildData. It was
never quite clear when to invalidate those lists, which led to QBS-635.
Instead we let the RuleNode decide which artifacts it considers as
"added or removed for this rule".

Task-number: QBS-635
Change-Id: I390e0ab775c695045c6e91ade3ac7326692cb314
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/corelib/buildgraph/projectbuilddata.cpp
@@ -35,7 +35,6 @@
 #include "command.h"
 #include "rulegraph.h"
 #include "rulenode.h"
-#include "rulesapplicator.h"
 #include "rulesevaluationcontext.h"
 #include "transformer.h"
 #include <language/language.h>
@@ -276,10 +275,6 @@ void ProjectBuildData::removeArtifact(Artifact *artifact,
         removeArtifactFromSet(artifact, artifact->product->buildData->artifactsByFileTag);
     }
 
-    // If removal is requested and the executor has not run since the time the artifact was last
-    // added, we must undo the "register" operation.
-    artifact->product->unregisterAddedArtifact(artifact);
-
     disconnectArtifact(artifact, logger);
     if (artifact->transformer) {
         artifact->product->unregisterArtifactWithChangedInputs(artifact);
@@ -428,7 +423,7 @@ void BuildDataResolver::resolveProductBuildData(const ResolvedProductPtr &produc
     evalContext()->checkForCancelation();
 
     product->buildData.reset(new ProductBuildData);
-    ArtifactsPerFileTagMap artifactsPerFileTag;
+    ProductBuildData::ArtifactSetByFileTag artifactsPerFileTag;
 
     foreach (ResolvedProductPtr dependency, product->dependencies) {
         if (Q_UNLIKELY(!dependency->enabled)) {
@@ -449,7 +444,6 @@ void BuildDataResolver::resolveProductBuildData(const ResolvedProductPtr &produc
     }
     qbsFileArtifact->fileTags.insert("qbs");
     artifactsPerFileTag["qbs"].insert(qbsFileArtifact);
-    product->registerAddedArtifact(qbsFileArtifact);
 
     // read sources
     foreach (const SourceArtifactConstPtr &sourceArtifact, product->allEnabledFiles()) {
@@ -458,7 +452,6 @@ void BuildDataResolver::resolveProductBuildData(const ResolvedProductPtr &produc
             continue; // ignore duplicate artifacts
 
         Artifact *artifact = createArtifact(product, sourceArtifact, m_logger);
-        product->registerAddedArtifact(artifact);
         foreach (const FileTag &fileTag, artifact->fileTags)
             artifactsPerFileTag[fileTag].insert(artifact);
     }
@@ -493,7 +486,6 @@ void BuildDataResolver::resolveProductBuildData(const ResolvedProductPtr &produc
                 safeConnect(outputArtifact, inputArtifact, m_logger);
             foreach (const FileTag &fileTag, outputArtifact->fileTags)
                 artifactsPerFileTag[fileTag].insert(outputArtifact);
-            product->registerAddedArtifact(outputArtifact);
 
             RuleArtifactPtr ruleArtifact = RuleArtifact::create();
             ruleArtifact->filePath = outputArtifact->filePath();

src/lib/corelib/buildgraph/rulenode.cpp
@@ -65,67 +65,49 @@ QString RuleNode::toString() const
 void RuleNode::apply(const Logger &logger, const ArtifactSet &changedInputs,
         ApplicationResult *result)
 {
-    bool hasAddedTags = false;
-    bool hasRemovedTags = false;
-    result->upToDate = changedInputs.isEmpty() && !usedDependenciesAdded();
+    const ArtifactSet oldInputs = oldInputArtifacts();
+    ArtifactSet allCompatibleInputs = currentInputArtifacts();
+    const ArtifactSet addedInputs = allCompatibleInputs - oldInputs;
+    const ArtifactSet removedInputs = oldInputs - allCompatibleInputs;
+    result->upToDate = changedInputs.isEmpty() && addedInputs.isEmpty() && removedInputs.isEmpty();
 
-    ProductBuildData::ArtifactSetByFileTag relevantArtifacts;
+    ArtifactSet inputs = changedInputs;
     if (product->isMarkedForReapplication(m_rule)) {
         QBS_CHECK(m_rule->multiplex);
         result->upToDate = false;
         product->unmarkForReapplication(m_rule);
         if (logger.traceEnabled())
             logger.qbsTrace() << "[BG] rule is marked for reapplication " << m_rule->toString();
-
-        foreach (Artifact *artifact, ArtifactSet::fromNodeSet(product->buildData->nodes)) {
-            if (m_rule->acceptsAsInput(artifact))
-                addArtifactToSet(artifact, relevantArtifacts);
-        }
+        inputs += allCompatibleInputs;
     } else {
-        foreach (const FileTag &tag, m_rule->inputs) {
-            if (product->addedArtifactsByFileTag(tag).count()) {
-                hasAddedTags = true;
-                result->upToDate = false;
-            }
-            if (product->removedArtifactsByFileTag(tag).count()) {
-                hasRemovedTags = true;
-                result->upToDate = false;
-            }
-            if (hasAddedTags && hasRemovedTags)
-                break;
-        }
-
-        relevantArtifacts = product->buildData->addedArtifactsByFileTag;
-        if (!changedInputs.isEmpty()) {
-            foreach (Artifact *artifact, changedInputs)
-                addArtifactToSet(artifact, relevantArtifacts);
-        }
+        inputs += addedInputs;
     }
     if (result->upToDate)
         return;
-    if (hasRemovedTags) {
+    if (!removedInputs.isEmpty()) {
         ArtifactSet outputArtifactsToRemove;
-        foreach (const FileTag &tag, m_rule->inputs) {
-            foreach (Artifact *artifact, product->removedArtifactsByFileTag(tag)) {
-                foreach (Artifact *parent, ArtifactSet::fromNodeSet(artifact->parents)) {
-                    if (!parent->transformer || parent->transformer->rule != m_rule
-                            || !parent->transformer->inputs.contains(artifact)) {
-                        // parent was not created by our rule.
-                        continue;
-                    }
-                    outputArtifactsToRemove += parent;
+        foreach (Artifact *artifact, removedInputs) {
+            foreach (Artifact *parent, ArtifactSet::fromNodeSet(artifact->parents)) {
+                if (!parent->transformer || parent->transformer->rule != m_rule
+                        || !parent->transformer->inputs.contains(artifact)) {
+                    // TODO: turn two of the three conditions above into QBS_CHECKs
+                    // parent must always have a transformer, because it's generated.
+                    //     QBS_CHECK(parent->transformer)
+                    // artifact is a former input of m_rule and parent was created by m_rule
+                    // the inputs of the transformer must contain artifact
+                    //     QBS_CHECK(parent->transformer->inputs.contains(artifact))
+
+                    // parent was not created by our rule.
+                    continue;
                 }
+                outputArtifactsToRemove += parent;
             }
         }
         RulesApplicator::handleRemovedRuleOutputs(outputArtifactsToRemove, logger);
     }
-    if (!relevantArtifacts.isEmpty()) {
-        RulesApplicator applicator(product, relevantArtifacts, logger);
-        result->createdNodes = applicator.applyRuleInEvaluationContext(m_rule);
-        foreach (BuildGraphNode *node, result->createdNodes) {
-            if (Artifact *artifact = dynamic_cast<Artifact *>(node))
-                product->registerAddedArtifact(artifact);
-        }
+    if (!inputs.isEmpty()) {
+        RulesApplicator applicator(product, logger);
+        result->createdNodes = applicator.applyRuleInEvaluationContext(m_rule, inputs);
     }
 }
 
@@ -141,17 +123,47 @@ void RuleNode::store(PersistentPool &pool) const
     pool.store(m_rule);
 }
 
-bool RuleNode::usedDependenciesAdded() const
+QList<TransformerPtr> RuleNode::createdTransformers() const
 {
-    foreach (const ResolvedProductConstPtr &dep, product->dependencies) {
-        if (!dep->buildData || !dep->fileTags.matches(rule()->usings))
+    QList<TransformerPtr> lst;
+    foreach (BuildGraphNode *parent, parents) {
+        if (parent->type() != ArtifactNodeType)
             continue;
-        foreach (Artifact *a, dep->targetArtifacts()) {
-            if (a->fileTags.matches(rule()->usings) && a->product->isAdded(a))
-                return true;
+        Artifact *artifact = static_cast<Artifact *>(parent);
+        if (!artifact->transformer || artifact->transformer->rule != m_rule)
+            continue;
+        lst.append(artifact->transformer);
+    }
+    return lst;
+}
+
+ArtifactSet RuleNode::oldInputArtifacts() const
+{
+    ArtifactSet s;
+    foreach (const TransformerPtr &t, createdTransformers())
+        s += t->inputs;
+    return s;
+}
+
+ArtifactSet RuleNode::currentInputArtifacts() const
+{
+    ArtifactSet s;
+    foreach (const FileTag &t, m_rule->inputs)
+        s += product->lookupArtifactsByFileTag(t);
+
+    foreach (const ResolvedProductConstPtr &dep, product->dependencies) {
+        if (!dep->buildData)
+            continue;
+        ArtifactSet artifactsToCheck;
+        foreach (Artifact *targetArtifact, dep->targetArtifacts())
+            artifactsToCheck += targetArtifact->transformer->outputs;
+        foreach (Artifact *artifact, artifactsToCheck) {
+            if (artifact->fileTags.matches(m_rule->usings))
+                s += artifact;
         }
     }
-    return false;
+
+    return s;
 }
 
 } // namespace Internal

src/lib/corelib/buildgraph/rulenode.h
@@ -32,6 +32,7 @@
 
 #include "artifactset.h"
 #include "buildgraphnode.h"
+#include "forward_decls.h"
 #include <language/forward_decls.h>
 
 namespace qbs {
@@ -65,7 +66,9 @@ protected:
     void store(PersistentPool &pool) const;
 
 private:
-    bool usedDependenciesAdded() const;
+    QList<TransformerPtr> createdTransformers() const;
+    ArtifactSet oldInputArtifacts() const;
+    ArtifactSet currentInputArtifacts() const;
 
     RuleConstPtr m_rule;
 };

src/lib/corelib/tools/persistence.cpp
@@ -40,7 +40,7 @@
 namespace qbs {
 namespace Internal {
 
-static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-72";
+static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-73";
 
 PersistentPool::PersistentPool(const Logger &logger) : m_logger(logger)
 {

src/lib/corelib/api/project.cpp
@@ -398,10 +398,8 @@ void ProjectPrivate::addFiles(const ProductData &product, const GroupData &group
             resolvedGroup->files << artifact;
         }
         if (resolvedProduct->enabled) {
-            foreach (const SourceArtifactConstPtr &sa, addedSourceArtifacts) {
-                Artifact * const artifact = createArtifact(resolvedProduct, sa, logger);
-                resolvedProduct->registerAddedArtifact(artifact);
-            }
+            foreach (const SourceArtifactConstPtr &sa, addedSourceArtifacts)
+                createArtifact(resolvedProduct, sa, logger);
         }
     }
     doSanityChecks(internalProject, logger);

src/lib/corelib/buildgraph/buildgraphloader.cpp
@@ -577,7 +577,6 @@ void BuildGraphLoader::onProductFileListChanged(const ResolvedProductPtr &restor
 
     QBS_CHECK(newlyResolvedProduct->enabled);
 
-    QList<Artifact *> addedArtifacts;
     ArtifactSet artifactsToRemove;
     QHash<QString, SourceArtifactConstPtr> oldArtifacts, newArtifacts;
 
@@ -616,7 +615,6 @@ void BuildGraphLoader::onProductFileListChanged(const ResolvedProductPtr &restor
                                                       newArtifact);
                 }
             }
-            addedArtifacts += newArtifact;
         }
     }
 
@@ -648,13 +646,14 @@ void BuildGraphLoader::onProductFileListChanged(const ResolvedProductPtr &restor
             // handle added filetags
             foreach (const FileTag &addedFileTag, changedArtifact->fileTags - a->fileTags) {
                 artifact->fileTags += addedFileTag;
-                newlyResolvedProduct->registerAddedFileTag(addedFileTag, artifact);
+                artifact->product->buildData->artifactsByFileTag[addedFileTag].insert(artifact);
             }
 
             // handle removed filetags
             foreach (const FileTag &removedFileTag, a->fileTags - changedArtifact->fileTags) {
                 artifact->fileTags -= removedFileTag;
-                newlyResolvedProduct->registerRemovedFileTag(removedFileTag, artifact);
+                removeArtifactFromSetByFileTag(artifact, removedFileTag,
+                                               artifact->product->buildData->artifactsByFileTag);
             }
         }
 
@@ -667,7 +666,7 @@ void BuildGraphLoader::onProductFileListChanged(const ResolvedProductPtr &restor
             newlyResolvedProduct->topLevelProject()->buildData
                 ->removeArtifactAndExclusiveDependents(oldArtifact, m_logger, true,
                                                        &artifactsToRemove);
-            addedArtifacts += createArtifact(newlyResolvedProduct, changedArtifact, m_logger);
+            createArtifact(newlyResolvedProduct, changedArtifact, m_logger);
         }
     }
 
@@ -677,8 +676,6 @@ void BuildGraphLoader::onProductFileListChanged(const ResolvedProductPtr &restor
             m_artifactsRemovedFromDisk << artifact->filePath();
         m_objectsToDelete << artifact;
     }
-    foreach (Artifact * const artifact, addedArtifacts)
-        newlyResolvedProduct->registerAddedArtifact(artifact);
 }
 
 static SourceArtifactConstPtr findSourceArtifact(const ResolvedProductConstPtr &product,

src/lib/corelib/buildgraph/executor.cpp
@@ -861,10 +861,6 @@ void Executor::finish()
                 m_artifactsRemovedFromDisk << filePath;
             }
             product->buildData->rescuableArtifactData.clear();
-
-            // Similar logic applies for the artifacts scheduled for potential rule application.
-            product->buildData->addedArtifactsByFileTag.clear();
-            product->buildData->removedArtifactsByFileTag.clear();
         }
     }
 

src/lib/corelib/buildgraph/productbuilddata.cpp
@@ -78,8 +78,6 @@ void ProductBuildData::load(PersistentPool &pool)
         rescuableArtifactData.insert(filePath, elem);
     }
     loadArtifactSetByFileTag(pool, artifactsByFileTag);
-    loadArtifactSetByFileTag(pool, addedArtifactsByFileTag);
-    loadArtifactSetByFileTag(pool, removedArtifactsByFileTag);
 
     pool.stream() >> count;
     for (int i = 0; i < count; ++i) {
@@ -112,8 +110,6 @@ void ProductBuildData::store(PersistentPool &pool) const
         it.value().store(pool);
     }
     storeArtifactSetByFileTag(pool, artifactsByFileTag);
-    storeArtifactSetByFileTag(pool, addedArtifactsByFileTag);
-    storeArtifactSetByFileTag(pool, removedArtifactsByFileTag);
 
     pool.stream() << artifactsWithChangedInputsPerRule.count();
     for (ArtifactSetByRule::ConstIterator it = artifactsWithChangedInputsPerRule.constBegin();
@@ -129,14 +125,21 @@ void addArtifactToSet(Artifact *artifact, ProductBuildData::ArtifactSetByFileTag
         container[tag] += artifact;
 }
 
+void removeArtifactFromSetByFileTag(Artifact *artifact, const FileTag &fileTag,
+        ProductBuildData::ArtifactSetByFileTag &container)
+{
+    ProductBuildData::ArtifactSetByFileTag::iterator it = container.find(fileTag);
+    if (it == container.end())
+        return;
+    it->remove(artifact);
+    if (it->isEmpty())
+        container.erase(it);
+}
+
 void removeArtifactFromSet(Artifact *artifact, ProductBuildData::ArtifactSetByFileTag &container)
 {
-    foreach (const FileTag &t, artifact->fileTags) {
-        ArtifactSet &s = container[t];
-        s.remove(artifact);
-        if (s.isEmpty())
-            container.remove(t);
-    }
+    foreach (const FileTag &t, artifact->fileTags)
+        removeArtifactFromSetByFileTag(artifact, t, container);
 }
 
 } // namespace Internal

commit fd49db9b33c0f54255d92c0d882a4f18c20a5ef3

fix calculation of old inputs for dynamic rules
Instead of deducing the old inputs of a rule from the inputs of its
created transformers, we hold a dedicated list of old inputs, because
the former approach doesn't work for dynamic rules.

This fixes a speed regression introduced in 32c4d3d7.
The outputArtifacts script of dynamic rules was always run on
compatible inputs that were not input of transformers, e.g.
C++ sources that don't have a Q_OBJECT macro.

Change-Id: I2d43fa7c91ed9084bd27dc9b948c06152bfce726
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/lib/corelib/buildgraph/artifactset.cpp
@@ -47,6 +47,12 @@ ArtifactSet::ArtifactSet(const QSet<Artifact *> &other)
 {
 }
 
+ArtifactSet &ArtifactSet::unite(const ArtifactSet &other)
+{
+    QSet::unite(other);
+    return *this;
+}
+
 ArtifactSet ArtifactSet::fromNodeSet(const NodeSet &nodes)
 {
     ArtifactSet result;

src/lib/corelib/buildgraph/artifactset.h
@@ -44,6 +44,9 @@ public:
     ArtifactSet();
     ArtifactSet(const ArtifactSet &other);
     ArtifactSet(const QSet<Artifact *> &other);
+
+    ArtifactSet &unite(const ArtifactSet &other);
+
     static ArtifactSet fromNodeSet(const NodeSet &nodes);
     static ArtifactSet fromNodeList(const QList<Artifact *> &lst);
 };

src/lib/corelib/buildgraph/projectbuilddata.cpp
@@ -261,6 +261,49 @@ void ProjectBuildData::removeArtifactAndExclusiveDependents(Artifact *artifact,
     removeArtifact(artifact, logger, removeFromDisk, removeFromProduct);
 }
 
+template <class Func>
+void forEachRuleNode(BuildGraphNode *n, const Func &f)
+{
+    if (n->type() != BuildGraphNode::RuleNodeType)
+        return;
+    f(static_cast<RuleNode *>(n));
+    foreach (BuildGraphNode *c, n->children)
+        forEachRuleNode(c, f);
+}
+
+template <class Func>
+void forEachRuleNode(const TopLevelProject *project, const Func &f)
+{
+    foreach (const ResolvedProductPtr &product, project->allProducts()) {
+        if (!product->buildData)
+            continue;
+        foreach (BuildGraphNode *n, product->buildData->roots)
+            forEachRuleNode(n, f);
+    }
+}
+
+class RemoveOldInputArtifactFromRuleNode
+{
+public:
+    RemoveOldInputArtifactFromRuleNode(Artifact *artifact, const Logger &logger)
+        : m_artifact(artifact), m_logger(logger)
+    {
+    }
+
+    void operator()(RuleNode *ruleNode) const
+    {
+        if (m_logger.traceEnabled()) {
+            m_logger.qbsTrace() << "[BG] remove old input " << m_artifact->filePath()
+                                << " from rule " << ruleNode->rule()->toString();
+        }
+        ruleNode->removeOldInputArtifact(m_artifact);
+    }
+
+private:
+    Artifact * const m_artifact;
+    const Logger &m_logger;
+};
+
 void ProjectBuildData::removeArtifact(Artifact *artifact,
         const Logger &logger, bool removeFromDisk, bool removeFromProduct)
 {
@@ -275,6 +318,8 @@ void ProjectBuildData::removeArtifact(Artifact *artifact,
         artifact->product->buildData->roots.remove(artifact);
         removeArtifactFromSet(artifact, artifact->product->buildData->artifactsByFileTag);
     }
+    forEachRuleNode(artifact->product->topLevelProject(),
+                    RemoveOldInputArtifactFromRuleNode(artifact, logger));
 
     disconnectArtifact(artifact, logger);
     if (artifact->transformer) {

src/lib/corelib/buildgraph/rulenode.cpp
@@ -65,10 +65,9 @@ QString RuleNode::toString() const
 void RuleNode::apply(const Logger &logger, const ArtifactSet &changedInputs,
         ApplicationResult *result)
 {
-    const ArtifactSet oldInputs = oldInputArtifacts();
     ArtifactSet allCompatibleInputs = currentInputArtifacts();
-    const ArtifactSet addedInputs = allCompatibleInputs - oldInputs;
-    const ArtifactSet removedInputs = oldInputs - allCompatibleInputs;
+    const ArtifactSet addedInputs = allCompatibleInputs - m_oldInputArtifacts;
+    const ArtifactSet removedInputs = m_oldInputArtifacts - allCompatibleInputs;
     result->upToDate = changedInputs.isEmpty() && addedInputs.isEmpty() && removedInputs.isEmpty();
 
     ArtifactSet inputs = changedInputs;
@@ -108,6 +107,7 @@ void RuleNode::apply(const Logger &logger, const ArtifactSet &changedInputs,
     if (!inputs.isEmpty()) {
         RulesApplicator applicator(product, logger);
         result->createdNodes = applicator.applyRuleInEvaluationContext(m_rule, inputs);
+        m_oldInputArtifacts.unite(inputs);
     }
 }
 
@@ -115,34 +115,14 @@ void RuleNode::load(PersistentPool &pool)
 {
     BuildGraphNode::load(pool);
     m_rule = pool.idLoadS<Rule>();
+    pool.loadContainer(m_oldInputArtifacts);
 }
 
 void RuleNode::store(PersistentPool &pool) const
 {
     BuildGraphNode::store(pool);
     pool.store(m_rule);
-}
-
-QList<TransformerPtr> RuleNode::createdTransformers() const
-{
-    QList<TransformerPtr> lst;
-    foreach (BuildGraphNode *parent, parents) {
-        if (parent->type() != ArtifactNodeType)
-            continue;
-        Artifact *artifact = static_cast<Artifact *>(parent);
-        if (!artifact->transformer || artifact->transformer->rule != m_rule)
-            continue;
-        lst.append(artifact->transformer);
-    }
-    return lst;
-}
-
-ArtifactSet RuleNode::oldInputArtifacts() const
-{
-    ArtifactSet s;
-    foreach (const TransformerPtr &t, createdTransformers())
-        s += t->inputs;
-    return s;
+    pool.storeContainer(m_oldInputArtifacts);
 }
 
 ArtifactSet RuleNode::currentInputArtifacts() const

src/lib/corelib/buildgraph/rulenode.h
@@ -60,17 +60,17 @@ public:
     };
 
     void apply(const Logger &logger, const ArtifactSet &changedInputs, ApplicationResult *result);
+    void removeOldInputArtifact(Artifact *artifact) { m_oldInputArtifacts.remove(artifact); }
 
 protected:
     void load(PersistentPool &pool);
     void store(PersistentPool &pool) const;
 
 private:
-    QList<TransformerPtr> createdTransformers() const;
-    ArtifactSet oldInputArtifacts() const;
     ArtifactSet currentInputArtifacts() const;
 
     RuleConstPtr m_rule;
+    ArtifactSet m_oldInputArtifacts;
 };
 
 } // namespace Internal

src/lib/corelib/tools/persistence.cpp
@@ -40,7 +40,7 @@
 namespace qbs {
 namespace Internal {
 
-static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-73";
+static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-74";
 
 PersistentPool::PersistentPool(const Logger &logger) : m_logger(logger)
 {