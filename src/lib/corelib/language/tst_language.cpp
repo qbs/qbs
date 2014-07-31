c585823afd68fcee4e1bc61ba87adefd8c7395b1
Christian Kandeler <christian.kandeler@digia.com>
2014-07-31 14:17:46 +0200

    Add missing comma to error message.
    Change-Id: I90b1b4dd35c6b2c625d11137e0f7d76d92641f72
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

--- src/lib/corelib/language/tst_language.cpp
+++ src/lib/corelib/language/tst_language.cpp
@@ -376,7 +376,7 @@ void TestLanguage::erroneousFiles_data()
                "at .*duplicate_sources_wildcards.qbs:4:12 "
                "and .*duplicate_sources_wildcards.qbs:6:16.";
     QTest::newRow("oldQbsVersion")
-            << "The project requires at least qbs version \\d+\\.\\d+.\\d+ "
+            << "The project requires at least qbs version \\d+\\.\\d+.\\d+, "
                "but this is qbs version " QBS_VERSION ".";
     QTest::newRow("wrongQbsVersionFormat")
             << "The value of Project.minimumQbsVersion is not a valid version string.";
