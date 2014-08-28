qbs/qbs

commit 5877328c4dacb1283408083cb3538ea5bc46eae5

Add support for running Node.js using qbs run.
Change-Id: I98a38e49cbea57d44b787eec12c2cb5f2e5cd601
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

share/qbs/modules/nodejs/NodeJS.qbs
@@ -1,0 +1,56 @@
+import qbs
+import qbs.File
+import qbs.FileInfo
+import qbs.ModUtils
+
+Module {
+    // JavaScript files which have been "processed" - currently this simply means "copied to output
+    // directory" but might later include minification and obfuscation processing
+    additionalProductTypes: ["nodejs_processed_js"]
+
+    property path applicationFile
+    PropertyOptions {
+        name: "applicationFile"
+        description: "file whose corresponding output will be executed when running the Node.js app"
+    }
+
+    setupRunEnvironment: {
+        var v = new ModUtils.EnvironmentVariable("NODE_PATH", qbs.pathListSeparator, qbs.hostOS.contains("windows"));
+        // can't use product.buildDirectory here, but RunEnvironment always sets the working
+        // directory to the directory containing the target file so we can exploit this for now
+        v.prepend(".");
+        v.set();
+    }
+
+    FileTagger {
+        patterns: ["*.js"]
+        fileTags: ["js"]
+    }
+
+    Rule {
+        inputs: ["js"]
+
+        outputArtifacts: {
+            var tags = ["nodejs_processed_js"];
+            if (input.fileTags.contains("application_js") ||
+                product.moduleProperty("nodejs", "applicationFile") === input.filePath)
+                tags.push("application");
+
+            return [{
+                filePath: product.destinationDirectory + '/' + input.fileName,
+                fileTags: tags
+            }];
+        }
+
+        outputFileTags: ["nodejs_processed_js", "application"]
+
+        prepare: {
+            var cmd = new JavaScriptCommand();
+            cmd.description = "copying " + FileInfo.fileName(input.filePath);
+            cmd.sourceCode = function() {
+                File.copy(input.filePath, output.filePath);
+            };
+            return cmd;
+        }
+    }
+}

src/lib/corelib/api/runenvironment.cpp
@@ -129,6 +129,21 @@ int RunEnvironment::runShell()
     return system(command.toLocal8Bit().constData());
 }
 
+static QString findExecutable(const QStringList &fileNames)
+{
+    const QStringList path = QString::fromLocal8Bit(qgetenv("PATH"))
+            .split(HostOsInfo::pathListSeparator(), QString::SkipEmptyParts);
+
+    foreach (const QString &fileName, fileNames) {
+        foreach (const QString &ppath, path) {
+            const QString fullPath = ppath + QLatin1Char('/') + fileName;
+            if (QFileInfo(fullPath).exists())
+                return QDir::cleanPath(fullPath);
+        }
+    }
+    return QString();
+}
+
 int RunEnvironment::runTarget(const QString &targetBin, const QStringList &arguments)
 {
     const QStringList targetOS = PropertyFinder().propertyValue(
@@ -154,6 +169,15 @@ int RunEnvironment::runTarget(const QString &targetBin, const QStringList &argum
         }
     }
 
+    if (completeSuffix == QLatin1String("js")) {
+        // The Node.js binary is called nodejs on Debian/Ubuntu-family operating systems due to a
+        // conflict with another package containing a binary named node
+        targetExecutable = findExecutable(QStringList()
+                                          << QLatin1String("nodejs")
+                                          << QLatin1String("node"));
+        targetArguments.prepend(targetBin);
+    }
+
     // Only check if the target is executable if we're not running it through another
     // known application such as msiexec or wine, as we can't check in this case anyways
     if (targetBin == targetExecutable && !QFileInfo(targetExecutable).isExecutable()) {
@@ -166,6 +190,7 @@ int RunEnvironment::runTarget(const QString &targetBin, const QStringList &argum
 
     d->logger.qbsInfo() << Tr::tr("Starting target '%1'.").arg(QDir::toNativeSeparators(targetBin));
     QProcess process;
+    process.setWorkingDirectory(QFileInfo(targetBin).absolutePath());
     process.setProcessEnvironment(d->resolvedProduct->runEnvironment);
     process.setProcessChannelMode(QProcess::ForwardedChannels);
     process.start(targetExecutable, targetArguments);

doc/reference/modules/nodejs-module.qdoc
@@ -1,0 +1,54 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2014 Petroules Corporation.
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the Qt Build Suite.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
+
+/*!
+    \contentspage index.html
+    \page nodejs-module.html
+    \ingroup list-of-modules
+
+    \title Module nodejs
+    \brief Provides Node.js support.
+
+    The \c nodejs module contains support for running \l{http://nodejs.org}{Node.js}
+    applications from \QBS.
+
+    \section1 General Properties
+
+
+    \section2 applicationFile
+
+    \table
+    \row    \li \b{Type:}            \li \c{path}
+    \row    \li \b{Default:}         \li \c{undefined}
+    \endtable
+
+    The input JavaScript file whose corresponding output will be executed when running the Node.js
+    application. If this property is \c{undefined}, the product will not be runnable.
+*/

share/qbs/imports/qbs/base/NodeJSApplication.qbs
@@ -1,0 +1,3 @@
+Product {
+    Depends { name: "nodejs" }
+}

tests/auto/blackbox/testdata/nodejs/hello.js
@@ -1,0 +1,3 @@
+if (console) {
+    console.log("hello world");
+}

tests/auto/blackbox/testdata/nodejs/hello.qbs
@@ -1,0 +1,7 @@
+import qbs
+
+NodeJSApplication {
+    nodejs.applicationFile: "hello.js"
+    name: "hello"
+    files: "hello.js"
+}

tests/auto/blackbox/tst_blackbox.cpp
@@ -1980,4 +1980,44 @@ void TestBlackbox::testWiX()
     QVERIFY(QFile::exists(buildDir + "/qbs-setup-" + arch + ".exe"));
 }
 
+static QString findExecutable(const QStringList &fileNames)
+{
+    const QStringList path = QString::fromLocal8Bit(qgetenv("PATH"))
+            .split(HostOsInfo::pathListSeparator(), QString::SkipEmptyParts);
+
+    foreach (const QString &fileName, fileNames) {
+        foreach (const QString &ppath, path) {
+            const QString fullPath = ppath + QLatin1Char('/') + fileName;
+            if (QFileInfo(fullPath).exists())
+                return QDir::cleanPath(fullPath);
+        }
+    }
+    return QString();
+}
+
+static bool haveNodeJs()
+{
+    // The Node.js binary is called nodejs on Debian/Ubuntu-family operating systems due to a
+    // conflict with another package containing a binary named node
+    return !findExecutable(QStringList()
+                                      << QLatin1String("nodejs")
+                                      << QLatin1String("node")).isEmpty();
+}
+
+void TestBlackbox::testNodeJs()
+{
+    if (!haveNodeJs()) {
+        SKIP_TEST("Node.js is not installed");
+        return;
+    }
+
+    QDir::setCurrent(testDataDir + QLatin1String("/nodejs"));
+
+    QbsRunParameters params;
+    params.command = QLatin1String("run");
+    QCOMPARE(runQbs(params), 0);
+    QVERIFY((bool)m_qbsStdout.contains("hello world"));
+    QVERIFY(QFile::exists(buildDir + "/hello.js"));
+}
+
 QTEST_MAIN(TestBlackbox)

tests/auto/blackbox/tst_blackbox.h
@@ -169,6 +169,7 @@ private slots:
     void testNsis();
     void testEmbedInfoPlist();
     void testWiX();
+    void testNodeJs();
 
 private:
     QByteArray m_qbsStderr;

commit 58f3c809ec0c7e9a9ddc87a6baf926738513bbd0

Fix run environment setup.
Now Darwin platforms get the same DYLD_* environment variables set as
Xcode (plus Qt) so applications can be developed/tested without
embedding bundles. This brings equivalent behavior to what already
exists on Windows.

This also fixes a regression introduced in
5877328c4dacb1283408083cb3538ea5bc46eae5 with the Node.js module,
causing the working directory to be inappropriately changed by the run
command.

Change-Id: Ic2904f105378eb61d9236367329a2f9c8771a7d3
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

share/qbs/modules/cpp/DarwinGCC.qbs
@@ -41,6 +41,29 @@ UnixGCC {
         }
     }
 
+    setupRunEnvironment: {
+        var env;
+        var installRoot = getEnv("QBS_INSTALL_ROOT");
+
+        env = new ModUtils.EnvironmentVariable("DYLD_FRAMEWORK_PATH", qbs.pathListSeparator);
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix, "Library", "Frameworks"));
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix, "lib"));
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix));
+        env.set();
+
+        env = new ModUtils.EnvironmentVariable("DYLD_LIBRARY_PATH", qbs.pathListSeparator);
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix, "lib"));
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix, "Library", "Frameworks"));
+        env.append(FileInfo.joinPaths(installRoot, qbs.installPrefix));
+        env.set();
+
+        if (qbs.sysroot) {
+            env = new ModUtils.EnvironmentVariable("DYLD_ROOT_PATH", qbs.pathListSeparator);
+            env.append(qbs.sysroot);
+            env.set();
+        }
+    }
+
     // private properties
     readonly property var buildEnv: {
         var env = {

share/qbs/modules/nodejs/NodeJS.qbs
@@ -16,9 +16,7 @@ Module {
 
     setupRunEnvironment: {
         var v = new ModUtils.EnvironmentVariable("NODE_PATH", qbs.pathListSeparator, qbs.hostOS.contains("windows"));
-        // can't use product.buildDirectory here, but RunEnvironment always sets the working
-        // directory to the directory containing the target file so we can exploit this for now
-        v.prepend(".");
+        v.prepend(FileInfo.path(getEnv("QBS_RUN_FILE_PATH")));
         v.set();
     }
 

src/app/qbs/commandlinefrontend.cpp
@@ -385,6 +385,7 @@ int CommandLineFrontend::runShell()
     const QList<ProductData> &products = productMap.begin().value();
     Q_ASSERT(products.count() == 1);
     RunEnvironment runEnvironment = project.getRunEnvironment(products.first(),
+            m_parser.installOptions(),
             QProcessEnvironment::systemEnvironment(), m_settings);
     return runEnvironment.runShell();
 }
@@ -462,6 +463,7 @@ int CommandLineFrontend::runTarget()
                         .arg(productToRun.name()));
         }
         RunEnvironment runEnvironment = project.getRunEnvironment(productToRun,
+                m_parser.installOptions(),
                 QProcessEnvironment::systemEnvironment(), m_settings);
         return runEnvironment.runTarget(executableFilePath, m_parser.runArgs());
     } catch (const ErrorInfo &error) {

src/lib/corelib/api/project.cpp
@@ -829,11 +829,12 @@ QString Project::targetExecutable(const ProductData &product,
 }
 
 RunEnvironment Project::getRunEnvironment(const ProductData &product,
+        const InstallOptions &installOptions,
         const QProcessEnvironment &environment, Settings *settings) const
 {
     QBS_CHECK(product.isEnabled());
     const ResolvedProductPtr resolvedProduct = d->internalProduct(product);
-    return RunEnvironment(resolvedProduct, environment, settings, d->logger);
+    return RunEnvironment(resolvedProduct, installOptions, environment, settings, d->logger);
 }
 
 /*!

src/lib/corelib/api/project.h
@@ -86,6 +86,7 @@ public:
     QString targetExecutable(const ProductData &product,
                              const InstallOptions &installoptions) const;
     RunEnvironment getRunEnvironment(const ProductData &product,
+            const InstallOptions &installOptions,
             const QProcessEnvironment &environment, Settings *settings) const;
 
     BuildJob *buildAllProducts(const BuildOptions &options, QObject *jobOwner = 0) const;

src/lib/corelib/api/runenvironment.cpp
@@ -30,12 +30,14 @@
 #include "runenvironment.h"
 
 #include <api/projectdata.h>
+#include <buildgraph/productinstaller.h>
 #include <language/language.h>
 #include <language/scriptengine.h>
 #include <logging/logger.h>
 #include <logging/translator.h>
 #include <tools/error.h>
 #include <tools/hostosinfo.h>
+#include <tools/installoptions.h>
 #include <tools/preferences.h>
 #include <tools/propertyfinder.h>
 
@@ -54,10 +56,11 @@ using namespace Internal;
 class RunEnvironment::RunEnvironmentPrivate
 {
 public:
-    RunEnvironmentPrivate(const ResolvedProductPtr &product,
+    RunEnvironmentPrivate(const ResolvedProductPtr &product, const InstallOptions &installOptions,
             const QProcessEnvironment &environment, Settings *settings, const Logger &logger)
         : engine(logger)
         , resolvedProduct(product)
+        , installOptions(installOptions)
         , environment(environment)
         , settings(settings)
         , logger(logger)
@@ -66,14 +69,16 @@ public:
 
     ScriptEngine engine;
     const ResolvedProductPtr resolvedProduct;
+    InstallOptions installOptions;
     const QProcessEnvironment environment;
     Settings * const settings;
     Logger logger;
 };
 
 RunEnvironment::RunEnvironment(const ResolvedProductPtr &product,
+        const InstallOptions &installOptions,
         const QProcessEnvironment &environment, Settings *settings, const Logger &logger)
-    : d(new RunEnvironmentPrivate(product, environment, settings, logger))
+    : d(new RunEnvironmentPrivate(product, installOptions, environment, settings, logger))
 {
 }
 
@@ -186,11 +191,15 @@ int RunEnvironment::runTarget(const QString &targetBin, const QStringList &argum
         return EXIT_FAILURE;
     }
 
-    d->resolvedProduct->setupRunEnvironment(&d->engine, d->environment);
+    const QString installRoot = effectiveInstallRoot(d->installOptions,
+                                                     d->resolvedProduct->topLevelProject());
+    QProcessEnvironment env = d->environment;
+    env.insert(QLatin1String("QBS_INSTALL_ROOT"), installRoot);
+    env.insert(QLatin1String("QBS_RUN_FILE_PATH"), targetBin);
+    d->resolvedProduct->setupRunEnvironment(&d->engine, env);
 
     d->logger.qbsInfo() << Tr::tr("Starting target '%1'.").arg(QDir::toNativeSeparators(targetBin));
     QProcess process;
-    process.setWorkingDirectory(QFileInfo(targetBin).absolutePath());
     process.setProcessEnvironment(d->resolvedProduct->runEnvironment);
     process.setProcessChannelMode(QProcess::ForwardedChannels);
     process.start(targetExecutable, targetArguments);

src/lib/corelib/api/runenvironment.h
@@ -40,6 +40,7 @@ class QProcessEnvironment;
 QT_END_NAMESPACE
 
 namespace qbs {
+class InstallOptions;
 class Settings;
 
 namespace Internal {
@@ -62,6 +63,7 @@ public:
 
 private:
     RunEnvironment(const Internal::ResolvedProductPtr &product,
+                   const InstallOptions &installOptions,
                    const QProcessEnvironment &environment, Settings *settings,
                    const Internal::Logger &logger);
 

src/lib/corelib/buildgraph/productinstaller.cpp
@@ -123,13 +123,7 @@ void ProductInstaller::initInstallRoot(const TopLevelProject *project,
     if (!options.installRoot().isEmpty())
         return;
 
-    if (options.installIntoSysroot()) {
-        options.setInstallRoot(PropertyFinder().propertyValue(project->buildConfiguration(),
-                QLatin1String("qbs"), QLatin1String("sysroot")).toString());
-    } else {
-        options.setInstallRoot(project->buildDirectory + QLatin1Char('/') +
-                               InstallOptions::defaultInstallRoot());
-    }
+    options.setInstallRoot(effectiveInstallRoot(options, project));
 }
 
 void ProductInstaller::removeInstallRoot()

src/lib/corelib/tools/installoptions.cpp
@@ -27,6 +27,8 @@
 **
 ****************************************************************************/
 #include "installoptions.h"
+#include "propertyfinder.h"
+#include "language/language.h"
 
 #include <QDir>
 #include <QSharedData>
@@ -49,6 +51,20 @@ public:
     bool keepGoing;
     bool logElapsedTime;
 };
+
+QString effectiveInstallRoot(const InstallOptions &options, const TopLevelProject *project)
+{
+    const QString installRoot = options.installRoot();
+    if (!installRoot.isEmpty())
+        return installRoot;
+
+    if (options.installIntoSysroot()) {
+        return PropertyFinder().propertyValue(project->buildConfiguration(),
+            QLatin1String("qbs"), QLatin1String("sysroot")).toString();
+    }
+
+    return project->buildDirectory + QLatin1Char('/') + InstallOptions::defaultInstallRoot();
+}
 
 } // namespace Internal
 

src/lib/corelib/tools/installoptions.h
@@ -35,7 +35,12 @@
 #include <QString>
 
 namespace qbs {
-namespace Internal { class InstallOptionsPrivate; }
+class InstallOptions;
+namespace Internal {
+class InstallOptionsPrivate;
+class TopLevelProject;
+QString effectiveInstallRoot(const InstallOptions &options, const TopLevelProject *project);
+}
 
 class QBS_EXPORT InstallOptions
 {

src/lib/qtprofilesetup/templates/core.qbs
@@ -171,12 +171,19 @@ Module {
     }
 
     setupRunEnvironment: {
+        var env;
         if (qbs.targetOS.contains('windows')) {
-            var v = getEnv('PATH') || '';
-            if (v.length > 0 && v.charAt(0) != ';')
-                v = ';' + v;
-            v = FileInfo.toWindowsSeparators(binPath) + v;
-            putEnv('PATH', v);
+            env = new ModUtils.EnvironmentVariable("PATH", qbs.pathListSeparator, true);
+            env.append(binPath);
+            env.set();
+        } else if (qbs.targetOS.contains("darwin")) {
+            env = new ModUtils.EnvironmentVariable("DYLD_FRAMEWORK_PATH", qbs.pathListSeparator);
+            env.append(libPath);
+            env.set();
+
+            env = new ModUtils.EnvironmentVariable("DYLD_LIBRARY_PATH", qbs.pathListSeparator);
+            env.append(libPath);
+            env.set();
         }
     }
 