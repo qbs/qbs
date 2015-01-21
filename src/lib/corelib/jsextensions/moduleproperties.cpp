qbs/qbs

commit 0d477159e67ef4e0dd665b8c5ba8850b11145afe

fix product variable in Export items
Consider a module M that's exported by Product B.
Product A depends on product B and thus depends on M.
This patch fixes the product variable in M that was referring to the
exporting product B, not the importing product A.

Task-number: QBS-729
Change-Id: Ia30b7159aa5d5ec37fe6257300e6c16205141f86
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

src/lib/corelib/language/projectresolver.cpp
@@ -859,7 +859,8 @@ void ProjectResolver::resolveExport(Item *item, ProjectContext *projectContext)
     Q_UNUSED(projectContext);
     checkCancelation();
     const QString &productName = m_productContext->product->uniqueName();
-    m_exports[productName] = evaluateModuleValues(item);
+    m_exports[productName] = item;
+    m_exportItemModules[item] = evaluateModuleValues(item, false);
 }
 
 static void insertExportedConfig(const QString &usedProductName,
@@ -889,6 +890,56 @@ static void addUsedProducts(ModuleLoaderResult::ProductInfo *productInfo,
             productInfo->usedProducts  += usedProduct;
     }
     *productsAdded = (oldCount != productInfo->usedProducts.count());
+}
+
+typedef QHash<Item *, ValuePtr> ValuePerItem;
+
+static void replaceProductRecursive(Item *item, const ItemValuePtr &productItemValue,
+        ValuePerItem *seen)
+{
+    if (seen->contains(item))
+        return;
+    ValuePtr oldProductValue = item->property(QLatin1String("product"));
+    seen->insert(item, oldProductValue);
+    if (oldProductValue)
+        item->setProperty(QLatin1String("product"), productItemValue);
+    if (item->scope())
+        replaceProductRecursive(item->scope(), productItemValue, seen);
+    foreach (const Item::Module &module, item->modules())
+        replaceProductRecursive(module.item, productItemValue, seen);
+    foreach (Item *child, item->children())
+        replaceProductRecursive(child, productItemValue, seen);
+}
+
+static ValuePerItem replaceProduct(Item *item, const ItemValuePtr &productItemValue)
+{
+    ValuePerItem seen;
+    replaceProductRecursive(item, productItemValue, &seen);
+    return seen;
+}
+
+static void restoreOldProducts(const ValuePerItem &vip)
+{
+    for (ValuePerItem::const_iterator it = vip.constBegin(); it != vip.constEnd(); ++it) {
+        const ValuePtr &oldProductValue = it.value();
+        if (oldProductValue)
+            it.key()->setProperty(QLatin1String("product"), oldProductValue);
+    }
+}
+
+static void copyProperties(const QVariantMap &src, QVariantMap &dst)
+{
+    for (QVariantMap::const_iterator it = src.constBegin(); it != src.constEnd(); ++it) {
+        const QVariant &v = it.value();
+        if (v.type() == QVariant::Map) {
+            QVariant &dstValue = dst[it.key()];
+            QVariantMap dstMap = dstValue.toMap();
+            copyProperties(v.toMap(), dstMap);
+            dstValue = dstMap;
+        } else {
+            dst[it.key()] = v;
+        }
+    }
 }
 
 void ProjectResolver::resolveProductDependencies(ProjectContext *projectContext)
@@ -927,6 +978,7 @@ void ProjectResolver::resolveProductDependencies(ProjectContext *projectContext)
         if (!rproduct->enabled)
             continue;
         Item *productItem = m_productItemMap.value(rproduct);
+        ItemValuePtr productItemValue = ItemValue::create(productItem);
         foreach (const ModuleLoaderResult::ProductInfo::Dependency &dependency,
                  projectContext->loadResult->productInfos.value(productItem).usedProducts) {
             const QString &usedProductName = dependency.uniqueName();
@@ -936,8 +988,22 @@ void ProjectResolver::resolveProductDependencies(ProjectContext *projectContext)
                             productItem->location());
             rproduct->dependencies.insert(usedProduct);
 
+            // Evaluate properties of the Export item using the importing product.
+            Item *exportItem = m_exports.value(usedProductName);
+            ProductContext productContext;
+            productContext.product = usedProduct;
+            m_productContext = &productContext;
+            const ValuePerItem oldProducts = replaceProduct(exportItem, productItemValue);
+            m_evaluator->setCachingEnabled(false);
+            QVariantMap exportedConfig = evaluateModuleValues(exportItem);
+            m_evaluator->setCachingEnabled(true);
+            restoreOldProducts(oldProducts);
+
+            // Re-evaluate direct properties of the Export item using the exporting product.
+            copyProperties(m_exportItemModules.value(exportItem), exportedConfig);
+            m_productContext = 0;
+
             // insert the configuration of the Export item into the product's configuration
-            const QVariantMap exportedConfig = m_exports.value(usedProductName);
             if (exportedConfig.isEmpty())
                 continue;
 
@@ -983,16 +1049,17 @@ void ProjectResolver::applyFileTaggers(const SourceArtifactPtr &artifact,
     }
 }
 
-QVariantMap ProjectResolver::evaluateModuleValues(Item *item) const
+QVariantMap ProjectResolver::evaluateModuleValues(Item *item, bool lookupPrototype) const
 {
     QVariantMap modules;
-    evaluateModuleValues(item, &modules);
+    evaluateModuleValues(item, &modules, lookupPrototype);
     QVariantMap result;
     result[QLatin1String("modules")] = modules;
     return result;
 }
 
-void ProjectResolver::evaluateModuleValues(Item *item, QVariantMap *modulesMap) const
+void ProjectResolver::evaluateModuleValues(Item *item, QVariantMap *modulesMap,
+        bool lookupPrototype) const
 {
     checkCancelation();
     for (Item::Modules::const_iterator it = item->modules().constBegin();
@@ -1000,22 +1067,23 @@ void ProjectResolver::evaluateModuleValues(Item *item, QVariantMap *modulesMap) 
     {
         QVariantMap depmods;
         const Item::Module &module = *it;
-        evaluateModuleValues(module.item, &depmods);
-        QVariantMap dep = evaluateProperties(module.item);
+        evaluateModuleValues(module.item, &depmods, lookupPrototype);
+        QVariantMap dep = evaluateProperties(module.item, lookupPrototype);
         dep.insert(QLatin1String("modules"), depmods);
         modulesMap->insert(ModuleLoader::fullModuleName(module.name), dep);
     }
 }
 
-QVariantMap ProjectResolver::evaluateProperties(Item *item) const
+QVariantMap ProjectResolver::evaluateProperties(Item *item, bool lookupPrototype) const
 {
     const QVariantMap tmplt;
-    return evaluateProperties(item, item, tmplt);
+    return evaluateProperties(item, item, tmplt, lookupPrototype);
 }
 
 QVariantMap ProjectResolver::evaluateProperties(Item *item,
                                                 Item *propertiesContainer,
-                                                const QVariantMap &tmplt) const
+                                                const QVariantMap &tmplt,
+                                                bool lookupPrototype) const
 {
     QVariantMap result = tmplt;
     for (QMap<QString, ValuePtr>::const_iterator it = propertiesContainer->properties().begin();
@@ -1076,8 +1144,8 @@ QVariantMap ProjectResolver::evaluateProperties(Item *item,
             break;
         }
     }
-    return propertiesContainer->prototype()
-            ? evaluateProperties(item, propertiesContainer->prototype(), result)
+    return lookupPrototype && propertiesContainer->prototype()
+            ? evaluateProperties(item, propertiesContainer->prototype(), result, true)
             : result;
 }
 

src/lib/corelib/language/projectresolver.h
@@ -118,11 +118,12 @@ private:
     void resolveProductDependencies(ProjectContext *projectContext);
     void postProcess(const ResolvedProductPtr &product, ProjectContext *projectContext) const;
     void applyFileTaggers(const ResolvedProductPtr &product) const;
-    QVariantMap evaluateModuleValues(Item *item) const;
-    void evaluateModuleValues(Item *item, QVariantMap *modulesMap) const;
-    QVariantMap evaluateProperties(Item *item) const;
+    QVariantMap evaluateModuleValues(Item *item, bool lookupPrototype = true) const;
+    void evaluateModuleValues(Item *item, QVariantMap *modulesMap,
+            bool lookupPrototype = true) const;
+    QVariantMap evaluateProperties(Item *item, bool lookupPrototype = true) const;
     QVariantMap evaluateProperties(Item *item, Item *propertiesContainer,
-            const QVariantMap &tmplt) const;
+            const QVariantMap &tmplt, bool lookupPrototype = true) const;
     QVariantMap createProductConfig() const;
     QString convertPathProperty(const QString &path, const QString &dirPath) const;
     QStringList convertPathListProperty(const QStringList &paths, const QString &dirPath) const;
@@ -138,7 +139,8 @@ private:
     QMap<QString, ResolvedProductPtr> m_productsByName;
     QHash<ResolvedProductPtr, Item *> m_productItemMap;
     mutable QHash<FileContextConstPtr, ResolvedFileContextPtr> m_fileContextMap;
-    QMap<QString, QVariantMap> m_exports;
+    QMap<QString, Item *> m_exports;
+    QHash<Item *, QVariantMap> m_exportItemModules; // ### merge with ExportContext in 1.4.
     SetupProjectParameters m_setupParams;
 
     typedef void (ProjectResolver::*ItemFuncPtr)(Item *item, ProjectContext *projectContext);

src/lib/corelib/language/tst_language.cpp
@@ -420,7 +420,6 @@ void TestLanguage::exports()
         QVariant propertyValue = getConfigProperty(product->moduleProperties->value(), propertyName);
         QCOMPARE(propertyValue.toStringList(), QStringList() << "USE_MYLIB");
 
-        QEXPECT_FAIL(0, "QBS-729", Continue);
         QCOMPARE(PropertyFinder().propertyValue(product->moduleProperties->value(), "dummy",
                                                 "productName").toString(), QString("myapp"));
 

src/lib/corelib/language/evaluator.cpp
@@ -224,5 +224,10 @@ QScriptValue Evaluator::fileScope(const FileContextConstPtr &file)
     return result;
 }
 
+void Evaluator::setCachingEnabled(bool enabled)
+{
+    m_scriptClass->setValueCacheEnabled(enabled);
+}
+
 } // namespace Internal
 } // namespace qbs

src/lib/corelib/language/evaluator.h
@@ -66,6 +66,8 @@ public:
     QScriptValue scriptValue(const Item *item);
     QScriptValue fileScope(const FileContextConstPtr &file);
 
+    void setCachingEnabled(bool enabled);
+
 private:
     void onItemPropertyChanged(Item *item);
     void onItemDestroyed(Item *item);

src/lib/corelib/language/evaluatorscriptclass.cpp
@@ -395,11 +395,14 @@ QScriptValue EvaluatorScriptClass::property(const QScriptValue &object, const QS
     if (debugProperties)
         m_logger.qbsTrace() << "[SC] property " << name;
 
-    QScriptValue result = data->valueCache.value(name);
-    if (result.isValid()) {
-        if (debugProperties)
-            m_logger.qbsTrace() << "[SC] cache hit " << name << ": " << resultToString(result);
-        return result;
+    QScriptValue result;
+    if (m_valueCacheEnabled) {
+        result = data->valueCache.value(name);
+        if (result.isValid()) {
+            if (debugProperties)
+                m_logger.qbsTrace() << "[SC] cache hit " << name << ": " << resultToString(result);
+            return result;
+        }
     }
 
     SVConverter converter(this, &object, value, itemOfProperty);
@@ -413,8 +416,14 @@ QScriptValue EvaluatorScriptClass::property(const QScriptValue &object, const QS
 
     if (debugProperties)
         m_logger.qbsTrace() << "[SC] cache miss " << name << ": " << resultToString(result);
-    data->valueCache.insert(name, result);
+    if (m_valueCacheEnabled)
+        data->valueCache.insert(name, result);
     return result;
+}
+
+void EvaluatorScriptClass::setValueCacheEnabled(bool enabled)
+{
+    m_valueCacheEnabled = enabled;
 }
 
 QScriptValue EvaluatorScriptClass::scriptValueForBuiltin(BuiltinValue::Builtin builtin) const

src/lib/corelib/language/evaluatorscriptclass.h
@@ -57,6 +57,7 @@ public:
     QScriptValue property(const QScriptValue &object,
                           const QScriptString &name, uint id);
 
+    void setValueCacheEnabled(bool enabled);
     QScriptValue scriptValueForBuiltin(BuiltinValue::Builtin builtin) const;
 
 private:
@@ -87,6 +88,7 @@ private:
     };
     QueryResult m_queryResult;
     Logger m_logger;
+    bool m_valueCacheEnabled;
     QScriptValue m_getNativeSettingBuiltin;
     QScriptValue m_getEnvBuiltin;
     QScriptValue m_getHostOSBuiltin;

commit ff18f6944becdfb77f3294af80d5b7e9f8688fef

more fine-grained cache disabling
Disable caching only for properties that reference product in some way.
This fixes the performance regression introduced by 0d477159e6.
Additionally, the property cache in ModuleProperties must be disabled
too. Otherwise we might return wrong cached values.

Task-number: QBS-729
Task-number: QBS-732
Change-Id: I4c46e4c5ddf20053851d7ee5e9a51f6f3f082a6d
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

src/lib/corelib/jsextensions/moduleproperties.cpp
@@ -125,8 +125,10 @@ QScriptValue ModuleProperties::moduleProperties(QScriptContext *context, QScript
     const QString moduleName = internalModuleName(context->argument(0).toString());
     const QString propertyName = context->argument(1).toString();
 
-    QVariant value = qbsEngine->retrieveFromPropertyCache(moduleName, propertyName, oneValue,
-                                                          properties);
+    QVariant value;
+    if (qbsEngine->isPropertyCacheEnabled())
+        value = qbsEngine->retrieveFromPropertyCache(moduleName, propertyName, oneValue,
+                                                     properties);
     if (!value.isValid()) {
         if (oneValue)
             value = PropertyFinder().propertyValue(properties->value(), moduleName, propertyName);

src/lib/corelib/language/itemreaderastvisitor.cpp
@@ -454,15 +454,18 @@ bool ItemReaderASTVisitor::visitStatement(AST::Statement *statement)
     m_sourceValue->setLocation(statement->firstSourceLocation().startLine,
                                statement->firstSourceLocation().startColumn);
 
-    bool usesBase, usesOuter;
+    bool usesBase, usesOuter, usesProduct;
     IdentifierSearch idsearch;
     idsearch.add(QLatin1String("base"), &usesBase);
     idsearch.add(QLatin1String("outer"), &usesOuter);
+    idsearch.add(QLatin1String("product"), &usesProduct);
     idsearch.start(statement);
     if (usesBase)
         m_sourceValue->m_flags |= JSSourceValue::SourceUsesBase;
     if (usesOuter)
         m_sourceValue->m_flags |= JSSourceValue::SourceUsesOuter;
+    if (usesProduct)
+        m_sourceValue->m_flags |= JSSourceValue::SourceUsesProduct;
     return false;
 }
 

src/lib/corelib/language/projectresolver.cpp
@@ -73,6 +73,7 @@ ProjectResolver::ProjectResolver(ModuleLoader *ldr, const BuiltinDeclarations *b
     , m_logger(logger)
     , m_engine(m_evaluator->engine())
     , m_progressObserver(0)
+    , m_disableCachedEvaluation(false)
 {
 }
 
@@ -994,9 +995,9 @@ void ProjectResolver::resolveProductDependencies(ProjectContext *projectContext)
             productContext.product = usedProduct;
             m_productContext = &productContext;
             const ValuePerItem oldProducts = replaceProduct(exportItem, productItemValue);
-            m_evaluator->setCachingEnabled(false);
+            m_disableCachedEvaluation = true;
             QVariantMap exportedConfig = evaluateModuleValues(exportItem);
-            m_evaluator->setCachingEnabled(true);
+            m_disableCachedEvaluation = false;
             restoreOldProducts(oldProducts);
 
             // Re-evaluate direct properties of the Export item using the exporting product.
@@ -1112,7 +1113,21 @@ QVariantMap ProjectResolver::evaluateProperties(Item *item,
             {
                 break;
             }
+
+            bool cacheDisabled = false;
+            const JSSourceValuePtr srcValue = it.value().staticCast<JSSourceValue>();
+            if (m_disableCachedEvaluation && (pd.type() == PropertyDeclaration::Path
+                                || srcValue->sourceUsesProduct())) {
+                cacheDisabled = true;
+                m_evaluator->setCachingEnabled(false);
+                m_evaluator->engine()->setPropertyCacheEnabled(false);
+            }
+
             const QScriptValue scriptValue = m_evaluator->property(item, it.key());
+            if (cacheDisabled) {
+                m_evaluator->setCachingEnabled(true);
+                m_evaluator->engine()->setPropertyCacheEnabled(true);
+            }
             if (Q_UNLIKELY(m_evaluator->engine()->hasErrorOrException(scriptValue)))
                 throw ErrorInfo(scriptValue.toString(), it.value()->location());
 

src/lib/corelib/language/projectresolver.h
@@ -136,6 +136,7 @@ private:
     ProgressObserver *m_progressObserver;
     ProductContext *m_productContext;
     ModuleContext *m_moduleContext;
+    bool m_disableCachedEvaluation;
     QMap<QString, ResolvedProductPtr> m_productsByName;
     QHash<ResolvedProductPtr, Item *> m_productItemMap;
     mutable QHash<FileContextConstPtr, ResolvedFileContextPtr> m_fileContextMap;

src/lib/corelib/language/scriptengine.cpp
@@ -78,7 +78,7 @@ uint qHash(const ScriptEngine::PropertyCacheKey &k, uint seed = 0)
 }
 
 ScriptEngine::ScriptEngine(const Logger &logger, QObject *parent)
-    : QScriptEngine(parent), m_logger(logger)
+    : QScriptEngine(parent), m_propertyCacheEnabled(true), m_logger(logger)
 {
     setProcessEventsInterval(1000); // For the cancelation mechanism to work.
     m_cancelationError = currentContext()->throwValue(tr("Execution canceled"));

src/lib/corelib/language/scriptengine.h
@@ -77,6 +77,8 @@ public:
         return m_propertiesRequestedFromArtifact;
     }
 
+    void setPropertyCacheEnabled(bool enable) { m_propertyCacheEnabled = enable; }
+    bool isPropertyCacheEnabled() const { return m_propertyCacheEnabled; }
     void addToPropertyCache(const QString &moduleName, const QString &propertyName,
             bool oneValue, const PropertyMapConstPtr &propertyMap, const QVariant &value);
     QVariant retrieveFromPropertyCache(const QString &moduleName, const QString &propertyName,
@@ -154,6 +156,7 @@ private:
 
     ScriptValueCache m_scriptValueCache;
     QHash<QString, QScriptValue> m_jsImportCache;
+    bool m_propertyCacheEnabled;
     QHash<PropertyCacheKey, QVariant> m_propertyCache;
     PropertyList m_propertiesRequestedInScript;
     QHash<QString, PropertyList> m_propertiesRequestedFromArtifact;

src/lib/corelib/language/tst_language.cpp
@@ -411,7 +411,7 @@ void TestLanguage::exports()
         TopLevelProjectPtr project = loader->loadProject(defaultParameters);
         QVERIFY(project);
         QHash<QString, ResolvedProductPtr> products = productsFromProject(project);
-        QCOMPARE(products.count(), 8);
+        QCOMPARE(products.count(), 9);
         ResolvedProductPtr product;
         product = products.value("myapp");
         QVERIFY(product);
@@ -454,6 +454,14 @@ void TestLanguage::exports()
                                      << "modules" << "dummy" << "defines";
         propertyValue = getConfigProperty(product->moduleProperties->value(), propertyName);
         QCOMPARE(propertyValue.toStringList(), QStringList() << "ABC");
+        QCOMPARE(PropertyFinder().propertyValue(product->moduleProperties->value(), "dummy",
+                                                "productName").toString(), QString("myapp2"));
+
+        // Check whether we're returning incorrect cached values.
+        product = products.value("myapp3");
+        QVERIFY(product);
+        QCOMPARE(PropertyFinder().propertyValue(product->moduleProperties->value(), "dummy",
+                                                "productName").toString(), QString("myapp3"));
     }
     catch (const ErrorInfo &e) {
         exceptionCaught = true;

src/lib/corelib/language/value.h
@@ -83,7 +83,8 @@ class JSSourceValue : public Value
         NoFlags = 0x00,
         SourceUsesBase = 0x01,
         SourceUsesOuter = 0x02,
-        HasFunctionForm = 0x04
+        SourceUsesProduct = 0x04,
+        HasFunctionForm = 0x08
     };
     Q_DECLARE_FLAGS(Flags, Flag)
 
@@ -108,6 +109,7 @@ public:
 
     bool sourceUsesBase() const { return m_flags.testFlag(SourceUsesBase); }
     bool sourceUsesOuter() const { return m_flags.testFlag(SourceUsesOuter); }
+    bool sourceUsesProduct() const { return m_flags.testFlag(SourceUsesProduct); }
     bool hasFunctionForm() const { return m_flags.testFlag(HasFunctionForm); }
     void setHasFunctionForm(bool b);
 

src/lib/corelib/language/testdata/exports.qbs
@@ -46,4 +46,8 @@ Project {
             dummy.cxxFlags: ["-bar"]
         }
     }
+    Application {
+        name: "myapp3"
+        Depends { name: "productWithInheritedExportItem" }
+    }
 }