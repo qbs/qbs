qbs/qbs

commit a3b5c6e9444023ec5daad85d917336334f5dac43

ProcessCommandExecutor: Clean up process output handling.
- Get rid of code duplication.
- Simplify code flow.
- Fix potentially erroneous overwriting of error status.

Change-Id: Ic7e3babe707e6d9e5c8c478de0891a5adecc0875
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

src/lib/corelib/buildgraph/processcommandexecutor.cpp
@@ -196,6 +196,49 @@ QString ProcessCommandExecutor::filterProcessOutput(const QByteArray &_output,
     return filteredOutput.toString();
 }
 
+static QProcess::ProcessError saveToFile(const QString &filePath, const QByteArray &content)
+{
+    QBS_ASSERT(!filePath.isEmpty(), return QProcess::WriteError);
+
+    QFile f(filePath);
+    if (!f.open(QIODevice::WriteOnly))
+        return QProcess::WriteError;
+
+    if (f.write(content) != content.size())
+        return QProcess::WriteError;
+    f.close();
+    return f.error() == QFileDevice::NoError ? QProcess::UnknownError : QProcess::WriteError;
+}
+
+void ProcessCommandExecutor::getProcessOutput(bool stdOut, ProcessResult &result)
+{
+    QByteArray content;
+    QString filterFunction;
+    QString redirectPath;
+    QStringList *target;
+    if (stdOut) {
+        content = m_process.readAllStandardOutput();
+        filterFunction = processCommand()->stdoutFilterFunction();
+        redirectPath = processCommand()->stdoutFilePath();
+        target = &result.d->stdOut;
+    } else {
+        content = m_process.readAllStandardError();
+        filterFunction = processCommand()->stderrFilterFunction();
+        redirectPath = processCommand()->stderrFilePath();
+        target = &result.d->stdErr;
+    }
+    QString contentString = filterProcessOutput(content, filterFunction);
+    if (!redirectPath.isEmpty()) {
+        const QProcess::ProcessError error = saveToFile(redirectPath, contentString.toLocal8Bit());
+        if (result.error() == QProcess::UnknownError && error != QProcess::UnknownError)
+            result.d->error = error;
+    } else {
+        if (!contentString.isEmpty() && contentString.endsWith(QLatin1Char('\n')))
+            contentString.chop(1);
+        *target = contentString.split(QLatin1Char('\n'));
+    }
+}
+
 void ProcessCommandExecutor::sendProcessOutput()
 {
     ProcessResult result;
@@ -208,39 +251,9 @@ void ProcessCommandExecutor::sendProcessOutput()
     result.d->error = m_process.error();
     QString errorString = m_process.errorString();
 
-    QByteArray content = m_process.readAllStandardOutput();
-    QString tmp;
-    if (!processCommand()->stdoutFilterFunction().isEmpty())
-        tmp = filterProcessOutput(content, processCommand()->stdoutFilterFunction());
+    getProcessOutput(true, result);
+    getProcessOutput(false, result);
 
-    if (!processCommand()->stdoutFilePath().isEmpty()) {
-        result.d->error = processCommand()->saveStdout(tmp.isEmpty() ? content : tmp.toLocal8Bit());
-    } else {
-        if (tmp.isEmpty())
-            tmp = QString::fromLocal8Bit(content);
-        if (!tmp.isEmpty()) {
-            if (tmp.endsWith(QLatin1Char('\n')))
-                tmp.chop(1);
-            result.d->stdOut = tmp.split(QLatin1Char('\n'));
-        }
-    }
-
-    tmp.clear();
-    content = m_process.readAllStandardError();
-    if (!processCommand()->stderrFilterFunction().isEmpty())
-        tmp = filterProcessOutput(content, processCommand()->stderrFilterFunction());
-
-    if (!processCommand()->stderrFilePath().isEmpty()) {
-        result.d->error = processCommand()->saveStderr(tmp.isEmpty() ? content : tmp.toLocal8Bit());
-    } else {
-        if (tmp.isEmpty())
-            tmp = QString::fromLocal8Bit(content);
-        if (!tmp.isEmpty()) {
-            if (tmp.endsWith(QLatin1Char('\n')))
-                tmp.chop(1);
-            result.d->stdErr = tmp.split(QLatin1Char('\n'));
-        }
-    }
     const bool processError = result.error() != QProcess::UnknownError;
     const bool failureExit = quint32(m_process.exitCode())
             > quint32(processCommand()->maxExitCode());

src/lib/corelib/buildgraph/command.cpp
@@ -262,30 +262,6 @@ void ProcessCommand::fillFromScriptValue(const QScriptValue *scriptValue, const 
     applyCommandProperties(scriptValue);
 }
 
-static QProcess::ProcessError saveToFile(const QString &filePath, const QByteArray &content)
-{
-    QBS_ASSERT(!filePath.isEmpty(), return QProcess::WriteError);
-
-    QFile f(filePath);
-    if (!f.open(QIODevice::WriteOnly))
-        return QProcess::WriteError;
-
-    if (f.write(content) != content.size())
-        return QProcess::WriteError;
-    f.close();
-    return f.error() == QFileDevice::NoError ? QProcess::UnknownError : QProcess::WriteError;
-}
-
-QProcess::ProcessError ProcessCommand::saveStdout(const QByteArray &content) const
-{
-    return saveToFile(m_stdoutFilePath, content);
-}
-
-QProcess::ProcessError ProcessCommand::saveStderr(const QByteArray &content) const
-{
-    return saveToFile(m_stderrFilePath, content);
-}
-
 void ProcessCommand::load(PersistentPool &pool)
 {
     AbstractCommand::load(pool);

src/lib/corelib/buildgraph/command.h
@@ -113,9 +113,6 @@ public:
     QString stdoutFilePath() const { return m_stdoutFilePath; }
     QString stderrFilePath() const { return m_stderrFilePath; }
 
-    QProcess::ProcessError saveStdout(const QByteArray &content) const;
-    QProcess::ProcessError saveStderr(const QByteArray &content) const;
-
 private:
     ProcessCommand();
 

src/lib/corelib/buildgraph/processcommandexecutor.h
@@ -68,6 +68,8 @@ private:
 
     void startProcessCommand();
     QString filterProcessOutput(const QByteArray &output, const QString &filterFunctionSource);
+    void getProcessOutput(bool stdOut, ProcessResult &result);
+
     void sendProcessOutput();
     void removeResponseFile();
     const ProcessCommand *processCommand() const;

commit c9317cb83a5292ccc10fc72f134f58295623ec7e

Fix command lines being printed regardless of echo mode.
Bug was introduced by a3b5c6e944.

Change-Id: Ic7a0a9dfa7f009905880dd6613e37bae4b74a3d2
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

src/lib/corelib/buildgraph/processcommandexecutor.cpp
@@ -235,7 +235,7 @@ void ProcessCommandExecutor::getProcessOutput(bool stdOut, ProcessResult &result
     } else {
         if (!contentString.isEmpty() && contentString.endsWith(QLatin1Char('\n')))
             contentString.chop(1);
-        *target = contentString.split(QLatin1Char('\n'));
+        *target = contentString.split(QLatin1Char('\n'), QString::SkipEmptyParts);
     }
 }
 