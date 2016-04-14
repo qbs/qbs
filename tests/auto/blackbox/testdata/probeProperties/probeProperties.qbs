qbs/qbs

commit ea470c74e4e62831f45faaa898dac8cb06da899b

Overhaul Probe handling for node.js/TypeScript.
Change-Id: I61964212856f0ebd78bdca621ebc13f9f0654e8b
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

share/qbs/imports/qbs/Probes/path-probe.js
@@ -1,0 +1,74 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing
+**
+** This file is part of the Qt Build Suite.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms and
+** conditions see http://www.qt.io/terms-conditions. For further information
+** use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file.  Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, The Qt Company gives you certain additional
+** rights.  These rights are described in The Qt Company LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
+
+var File = loadExtension("qbs.File");
+var FileInfo = loadExtension("qbs.FileInfo");
+var ModUtils = loadExtension("qbs.ModUtils");
+
+function configure(names, nameSuffixes, nameFilter, pathPrefixes, pathSuffixes, platformPaths,
+                   environmentPaths, platformEnvironmentPaths, pathListSeparator) {
+    if (!names)
+        throw '"names" must be specified';
+    var _names = ModUtils.concatAll(names);
+    if (nameFilter)
+        _names = _names.map(nameFilter);
+    _names = ModUtils.concatAll.apply(undefined, _names.map(function(name) {
+        return (nameSuffixes || [""]).map(function(suffix) { return name + suffix; });
+    }));
+    // FIXME: Suggest how to obtain paths from system
+    var _paths = ModUtils.concatAll(pathPrefixes, platformPaths);
+    // FIXME: Add getenv support
+    var envs = ModUtils.concatAll(platformEnvironmentPaths, environmentPaths);
+    for (var i = 0; i < envs.length; ++i) {
+        var value = qbs.getEnv(envs[i]) || '';
+        if (value.length > 0)
+            _paths = _paths.concat(value.split(pathListSeparator));
+    }
+    var _suffixes = ModUtils.concatAll('', pathSuffixes);
+    _paths = _paths.map(FileInfo.fromNativeSeparators);
+    _suffixes = _suffixes.map(FileInfo.fromNativeSeparators);
+    for (i = 0; i < _names.length; ++i) {
+        for (var j = 0; j < _paths.length; ++j) {
+            for (var k = 0; k < _suffixes.length; ++k) {
+                var _filePath = FileInfo.joinPaths(_paths[j], _suffixes[k], _names[i]);
+                if (File.exists(_filePath)) {
+                    return {
+                        found: true,
+                        filePath: _filePath,
+                        fileName: FileInfo.fileName(_filePath),
+                        path: FileInfo.path(_filePath)
+                    }
+                }
+            }
+        }
+    }
+
+    return { found: false };
+}

tests/auto/blackbox/tst_blackbox.cpp
@@ -98,7 +98,8 @@ int TestBlackbox::runQbs(const QbsRunParameters &params)
     if (!params.command.isEmpty())
         args << params.command;
     if (supportsBuildDirectoryOption(params.command)) {
-        args.append(QStringList(QLatin1String("-d")) << QLatin1String("."));
+        args.append(QLatin1String("-d"));
+        args.append(params.buildDirectory.isEmpty() ? QLatin1String(".") : params.buildDirectory);
     }
     args << params.arguments;
     if (params.useProfile)
@@ -226,18 +227,54 @@ static QString findExecutable(const QStringList &fileNames)
 
 QMap<QString, QString> TestBlackbox::findJdkTools(int *status)
 {
-    QDir::setCurrent(testDataDir + "/find-java");
-    const int res = runQbs(QbsRunParameters(QStringList()
-                                            << "-f" << "find-jar.qbs"));
+    QTemporaryDir temp;
+    QDir::setCurrent(testDataDir + "/find");
+    QbsRunParameters params = QStringList() << "-f" << "find-jdk.qbs";
+    params.buildDirectory = temp.path();
+    const int res = runQbs(params);
     if (status)
         *status = res;
-    QFile file(relativeProductBuildDir("find-jar") + "/jdk.json");
+    QFile file(temp.path() + "/" + relativeProductBuildDir("find-jdk") + "/jdk.json");
     file.open(QIODevice::ReadOnly);
     const auto tools = QJsonDocument::fromJson(file.readAll()).toVariant().toMap();
     return QMap<QString, QString> {
         {"java", QDir::fromNativeSeparators(tools["java"].toString())},
         {"javac", QDir::fromNativeSeparators(tools["javac"].toString())},
         {"jar", QDir::fromNativeSeparators(tools["jar"].toString())}
+    };
+}
+
+QMap<QString, QString> TestBlackbox::findNodejs(int *status)
+{
+    QTemporaryDir temp;
+    QDir::setCurrent(testDataDir + "/find");
+    QbsRunParameters params = QStringList() << "-f" << "find-nodejs.qbs";
+    params.buildDirectory = temp.path();
+    const int res = runQbs(params);
+    if (status)
+        *status = res;
+    QFile file(temp.path() + "/" + relativeProductBuildDir("find-nodejs") + "/nodejs.json");
+    file.open(QIODevice::ReadOnly);
+    const auto tools = QJsonDocument::fromJson(file.readAll()).toVariant().toMap();
+    return QMap<QString, QString> {
+        {"node", QDir::fromNativeSeparators(tools["node"].toString())}
+    };
+}
+
+QMap<QString, QString> TestBlackbox::findTypeScript(int *status)
+{
+    QTemporaryDir temp;
+    QDir::setCurrent(testDataDir + "/find");
+    QbsRunParameters params = QStringList() << "-f" << "find-typescript.qbs";
+    params.buildDirectory = temp.path();
+    const int res = runQbs(params);
+    if (status)
+        *status = res;
+    QFile file(temp.path() + "/" + relativeProductBuildDir("find-typescript") + "/typescript.json");
+    file.open(QIODevice::ReadOnly);
+    const auto tools = QJsonDocument::fromJson(file.readAll()).toVariant().toMap();
+    return QMap<QString, QString> {
+        {"tsc", QDir::fromNativeSeparators(tools["tsc"].toString())}
     };
 }
 
@@ -2738,23 +2775,29 @@ void TestBlackbox::wix()
     }
 }
 
-static bool haveNodeJs()
-{
-    // The Node.js binary is called nodejs on Debian/Ubuntu-family operating systems due to a
-    // conflict with another package containing a binary named node
-    return !findExecutable(QStringList()
-                                      << QLatin1String("nodejs")
-                                      << QLatin1String("node")).isEmpty();
-}
-
 void TestBlackbox::nodejs()
 {
-    if (!haveNodeJs()) {
-        QSKIP("Node.js is not installed");
-        return;
-    }
+    Settings settings((QString()));
+    Profile p(profileName(), &settings);
+
+    int status;
+    findNodejs(&status);
+    QCOMPARE(status, 0);
 
     QDir::setCurrent(testDataDir + QLatin1String("/nodejs"));
+
+    status = runQbs();
+    if (p.value("nodejs.toolchainInstallPath").toString().isEmpty()
+            && status != 0 && m_qbsStderr.contains("toolchainInstallPath")) {
+        QSKIP("nodejs.toolchainInstallPath not set and automatic detection failed");
+    }
+
+    if (p.value("nodejs.packageManagerPrefixPath").toString().isEmpty()
+            && status != 0 && m_qbsStderr.contains("nodejs.packageManagerPrefixPath")) {
+        QSKIP("nodejs.packageManagerFilePath not set and automatic detection failed");
+    }
+
+    QCOMPARE(status, 0);
 
     QbsRunParameters params;
     params.command = QLatin1String("run");
@@ -2765,12 +2808,22 @@ void TestBlackbox::nodejs()
 
 void TestBlackbox::typescript()
 {
-    if (!haveNodeJs()) {
-        QSKIP("node.js is not installed");
-        return;
-    }
+    Settings settings((QString()));
+    Profile p(profileName(), &settings);
+
+    int status;
+    findTypeScript(&status);
+    QCOMPARE(status, 0);
 
     QDir::setCurrent(testDataDir + QLatin1String("/typescript"));
+
+    status = runQbs();
+    if (p.value("typescript.toolchainInstallPath").toString().isEmpty()
+            && status != 0 && m_qbsStderr.contains("toolchainInstallPath")) {
+        QSKIP("typescript.toolchainInstallPath not set and automatic detection failed");
+    }
+
+    QCOMPARE(status, 0);
 
     QbsRunParameters params;
     params.command = QLatin1String("run");

tests/auto/blackbox/tst_blackbox.h
@@ -67,6 +67,7 @@ public:
 
     QString command;
     QStringList arguments;
+    QString buildDirectory;
     QProcessEnvironment environment;
     bool expectFailure;
     bool useProfile;
@@ -197,6 +198,8 @@ private slots:
 
 private:
     QMap<QString, QString> findJdkTools(int *status);
+    QMap<QString, QString> findNodejs(int *status);
+    QMap<QString, QString> findTypeScript(int *status);
     QString findArchiver(const QString &fileName, int *status = nullptr);
 
     QByteArray m_qbsStderr;

share/qbs/imports/qbs/Probes/NodeJsProbe.qbs
@@ -32,9 +32,17 @@ import qbs
 import qbs.FileInfo
 
 BinaryProbe {
-    names: ["nodejs", "node" + (qbs.hostOS.contains("windows") ? ".exe" : "")]
-    platformPaths: base.concat(qbs.hostOS.contains("windows")
-                               ? [FileInfo.joinPaths(qbs.getEnv("PROGRAMFILES"), "nodejs"),
-                                  FileInfo.joinPaths(qbs.getEnv("PROGRAMFILES(X86)"), "nodejs")]
-                               : [])
+    names: ["nodejs", "node"]
+    platformPaths: {
+        var paths = base;
+        if (qbs.hostOS.contains("windows")) {
+            var env32 = qbs.getEnv("PROGRAMFILES(X86)");
+            var env64 = qbs.getEnv("PROGRAMFILES");
+            if (env64 === env32 && env64.endsWith(" (x86)"))
+                env64 = env64.slice(0, -(" (x86)".length)); // QTBUG-3845
+            paths.push(FileInfo.joinPaths(env64, "nodejs"));
+            paths.push(FileInfo.joinPaths(env32, "nodejs"));
+        }
+        return paths;
+    }
 }

share/qbs/imports/qbs/Probes/NpmProbe.qbs
@@ -28,10 +28,32 @@
 **
 ****************************************************************************/
 
-import qbs 1.0
+import qbs
+import "path-probe.js" as PathProbeConfigure
+import "../../../modules/nodejs/nodejs.js" as NodeJs
 
-PathProbe {
-    nameSuffixes: qbs.hostOS.contains("windows") ? [".com", ".exe", ".bat", ".cmd"] : undefined
-    platformPaths: undefined
-    platformEnvironmentPaths: [ "PATH" ]
+NodeJsProbe {
+    names: ["npm"]
+
+    // Outputs
+    property path npmBin
+    property path npmRoot
+    property path npmPrefix
+
+    configure: {
+        var result = PathProbeConfigure.configure(names, nameSuffixes, nameFilter, pathPrefixes,
+                                                  pathSuffixes, platformPaths, environmentPaths,
+                                                  platformEnvironmentPaths, qbs.pathListSeparator);
+        result.npmBin = result.found ? NodeJs.findLocation(result.filePath, "bin") : undefined;
+        result.npmRoot = result.found ? NodeJs.findLocation(result.filePath, "root") : undefined;
+        result.npmPrefix = result.found ? NodeJs.findLocation(result.filePath, "prefix") : undefined;
+
+        found = result.found;
+        path = result.path;
+        filePath = result.filePath;
+        fileName = result.fileName;
+        npmBin = result.npmBin;
+        npmRoot = result.npmRoot;
+        npmPrefix = result.npmPrefix;
+    }
 }

share/qbs/imports/qbs/Probes/PathProbe.qbs
@@ -29,13 +29,12 @@
 ****************************************************************************/
 
 import qbs 1.0
-import qbs.File
-import qbs.FileInfo
-import qbs.ModUtils
+import "path-probe.js" as PathProbeConfigure
 
 Probe {
     // Inputs
     property stringList names
+    property stringList nameSuffixes
     property var nameFilter
     property pathList pathPrefixes
     property stringList pathSuffixes
@@ -49,38 +48,12 @@ Probe {
     property string fileName
 
     configure: {
-        if (!names)
-            throw '"names" must be specified';
-        var _names = ModUtils.concatAll(names);
-        if (nameFilter)
-            _names = _names.map(nameFilter);
-        // FIXME: Suggest how to obtain paths from system
-        var _paths = ModUtils.concatAll(pathPrefixes, platformPaths);
-        // FIXME: Add getenv support
-        var envs = ModUtils.concatAll(platformEnvironmentPaths, environmentPaths);
-        for (var i = 0; i < envs.length; ++i) {
-            var value = qbs.getEnv(envs[i]) || '';
-            if (value.length > 0)
-                _paths = _paths.concat(value.split(qbs.pathListSeparator));
-        }
-        var _suffixes = ModUtils.concatAll('', pathSuffixes);
-        for (i = 0; i < _names.length; ++i) {
-            for (var j = 0; j < _paths.length; ++j) {
-                for (var k = 0; k < _suffixes.length; ++k) {
-                    var _filePath = FileInfo.joinPaths(_paths[j], _suffixes[k], _names[i]);
-                    if (File.exists(_filePath)) {
-                        found = true;
-                        path = FileInfo.joinPaths(_paths[j], _suffixes[k]);
-                        filePath = _filePath;
-                        fileName = _names[i];
-                        return;
-                    }
-                }
-            }
-        }
-        found = false;
-        path = undefined;
-        filePath = undefined;
-        fileName = undefined;
+        var result = PathProbeConfigure.configure(names, nameSuffixes, nameFilter, pathPrefixes,
+                                                  pathSuffixes, platformPaths, environmentPaths,
+                                                  platformEnvironmentPaths, qbs.pathListSeparator);
+        found = result.found;
+        path = result.path;
+        filePath = result.filePath;
+        fileName = result.fileName;
     }
 }

share/qbs/imports/qbs/Probes/TypeScriptProbe.qbs
@@ -32,57 +32,44 @@ import qbs
 import qbs.File
 import qbs.FileInfo
 import qbs.ModUtils
+import "path-probe.js" as PathProbeConfigure
 import "../../../modules/typescript/typescript.js" as TypeScript
 
 BinaryProbe {
     id: tsc
     names: ["tsc"]
+    pathPrefixes: [packageManagerBinPath]
 
     // Inputs
-    property path nodejsToolchainInstallPath
+    property path packageManagerBinPath
+    property path packageManagerRootPath
 
     // Outputs
     property var version
 
     configure: {
-        if (!nodejsToolchainInstallPath)
-            throw '"nodejsToolchainInstallPath" must be specified';
-        // HACK: Duplicated from PathProbe.qbs
-        if (!names)
-            throw '"names" must be specified';
-        var _names = ModUtils.concatAll(names);
-        if (nameFilter)
-            _names = _names.map(nameFilter);
-        // FIXME: Suggest how to obtain paths from system
-        var _paths = ModUtils.concatAll(pathPrefixes, platformPaths);
-        // FIXME: Add getenv support
-        var envs = ModUtils.concatAll(platformEnvironmentPaths, environmentPaths);
-        for (var i = 0; i < envs.length; ++i) {
-            var value = qbs.getEnv(envs[i]) || '';
-            if (value.length > 0)
-                _paths = _paths.concat(value.split(qbs.pathListSeparator));
+        if (!condition)
+            return;
+        if (!packageManagerBinPath)
+            throw '"packageManagerBinPath" must be specified';
+        if (!packageManagerRootPath)
+            throw '"packageManagerRootPath" must be specified';
+
+        var result = PathProbeConfigure.configure(names, nameSuffixes, nameFilter, pathPrefixes,
+                                                  pathSuffixes, platformPaths, environmentPaths,
+                                                  platformEnvironmentPaths, qbs.pathListSeparator);
+        result.version = result.found
+                ? TypeScript.findTscVersion(result.filePath, packageManagerBinPath)
+                : undefined;
+        if (FileInfo.fromNativeSeparators(packageManagerBinPath) !== result.path ||
+                !File.exists(FileInfo.fromNativeSeparators(packageManagerRootPath, "typescript"))) {
+            result = { found: false };
         }
-        var _suffixes = ModUtils.concatAll('', pathSuffixes);
-        for (i = 0; i < _names.length; ++i) {
-            for (var j = 0; j < _paths.length; ++j) {
-                for (var k = 0; k < _suffixes.length; ++k) {
-                    var _filePath = FileInfo.joinPaths(_paths[j], _suffixes[k], _names[i]);
-                    if (File.exists(_filePath)) {
-                        found = true;
-                        filePath = File.canonicalFilePath(_filePath);
-                        fileName = FileInfo.fileName(filePath);
-                        path = FileInfo.path(filePath);
-                        version = TypeScript.findTscVersion(filePath, nodejsToolchainInstallPath);
-                        return;
-                    }
-                }
-            }
-        }
-        found = false;
-        path = undefined;
-        filePath = undefined;
-        fileName = undefined;
-        version = undefined;
-        // HACK: Duplicated from PathProbe.qbs
+
+        found = result.found;
+        path = result.path;
+        filePath = result.filePath;
+        fileName = result.fileName;
+        version = result.version;
     }
 }

share/qbs/modules/nodejs/NodeJS.qbs
@@ -45,16 +45,33 @@ Module {
         pathPrefixes: [toolchainInstallPath]
     }
 
+    Probes.NpmProbe {
+        id: npm
+        pathPrefixes: [toolchainInstallPath]
+    }
+
     property path applicationFile
     PropertyOptions {
         name: "applicationFile"
         description: "file whose corresponding output will be executed when running the Node.js app"
     }
 
-    property path toolchainInstallPath: nodejs.path
+    property path toolchainInstallPath: {
+        if (nodejs.path && npm.path && nodejs.path !== npm.path)
+            throw("node and npm binaries do not belong to the same installation ("
+                  + nodejs.path + " vs " + npm.path + ")");
+        return nodejs.path || npm.path;
+    }
 
     property path interpreterFileName: nodejs.fileName
     property path interpreterFilePath: nodejs.filePath
+
+    property path packageManagerFileName: npm.fileName
+    property path packageManagerFilePath: npm.filePath
+
+    property path packageManagerBinPath: npm.npmBin
+    property path packageManagerRootPath: npm.npmRoot
+    property path packageManagerPrefixPath: npm.npmPrefix
 
     // private properties
     readonly property path compiledIntermediateDir: FileInfo.joinPaths(product.buildDirectory,
@@ -82,6 +99,11 @@ Module {
         validator.setRequiredProperty("toolchainInstallPath", toolchainInstallPath);
         validator.setRequiredProperty("interpreterFileName", interpreterFileName);
         validator.setRequiredProperty("interpreterFilePath", interpreterFilePath);
+        validator.setRequiredProperty("packageManagerFileName", packageManagerFileName);
+        validator.setRequiredProperty("packageManagerFilePath", packageManagerFilePath);
+        validator.setRequiredProperty("packageManagerBinPath", packageManagerBinPath);
+        validator.setRequiredProperty("packageManagerRootPath", packageManagerRootPath);
+        validator.setRequiredProperty("packageManagerPrefixPath", packageManagerPrefixPath);
         validator.validate();
     }
 

commit 9ec50632e8a9a57c1a02b629d322db27142cdcd0

Restore original behavior of PathProbe fileName/path properties.
This is a regression caused by ea470c74e4e62831f45faaa898dac8cb06da899b.
Add autotests to verify.

Task-number: QBS-948
Change-Id: I012312de60e9d64107f5621a7fd87acab98190e8
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

share/qbs/imports/qbs/Probes/path-probe.js
@@ -63,8 +63,15 @@ function configure(names, nameSuffixes, nameFilter, pathPrefixes, pathSuffixes, 
                     return {
                         found: true,
                         filePath: _filePath,
-                        fileName: FileInfo.fileName(_filePath),
-                        path: FileInfo.path(_filePath)
+
+                        // Manually specify the path components that constitute _filePath rather
+                        // than using the FileInfo.path and FileInfo.fileName functions because we
+                        // want to break _filePath into its constituent parts based on the input
+                        // originally given by the user. For example, the FileInfo functions would
+                        // produce a different result if any of the items in the names property
+                        // contained more than a single path component.
+                        fileName: _names[i],
+                        path: FileInfo.joinPaths(_paths[j], _suffixes[k]),
                     }
                 }
             }

tests/auto/blackbox/testdata/probeProperties/main.c
@@ -1,0 +1,1 @@
+int main() { return 0; }

tests/auto/blackbox/testdata/probeProperties/probeProperties.qbs
@@ -1,0 +1,30 @@
+import qbs
+import qbs.Probes
+
+CppApplication {
+    Probes.PathProbe {
+        id: probe1
+        names: ["bin/tool"]
+        platformPaths: [product.sourceDirectory]
+    }
+
+    Probes.PathProbe {
+        id: probe2
+        names: ["tool"]
+        platformPaths: [product.sourceDirectory + "/bin"]
+    }
+
+    targetName: {
+        console.info("probe1.fileName=" + probe1.fileName);
+        console.info("probe1.path=" + probe1.path);
+        console.info("probe1.filePath=" + probe1.filePath);
+
+        console.info("probe2.fileName=" + probe2.fileName);
+        console.info("probe2.path=" + probe2.path);
+        console.info("probe2.filePath=" + probe2.filePath);
+
+        return name;
+    }
+
+    files: ["main.c"]
+}

tests/auto/blackbox/tst_blackbox.cpp
@@ -1691,6 +1691,19 @@ void TestBlackbox::overrideProjectProperties()
     QCOMPARE(runQbs(params), 0);
 }
 
+void TestBlackbox::probeProperties()
+{
+    QDir::setCurrent(testDataDir + "/probeProperties");
+    const QByteArray dir = QDir::cleanPath(testDataDir).toLatin1() + "/probeProperties";
+    QCOMPARE(runQbs(), 0);
+    QVERIFY2(m_qbsStdout.contains("probe1.fileName=bin/tool"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe1.path=" + dir), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe1.filePath=" + dir + "/bin/tool"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe2.fileName=tool"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe2.path=" + dir + "/bin"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe2.filePath=" + dir + "/bin/tool"), m_qbsStdout.constData());
+}
+
 void TestBlackbox::probeInExportedModule()
 {
     QDir::setCurrent(testDataDir + "/probe-in-exported-module");

tests/auto/blackbox/tst_blackbox.h
@@ -160,6 +160,7 @@ private slots:
     void objcArc();
     void outputArtifactAutoTagging();
     void overrideProjectProperties();
+    void probeProperties();
     void probeInExportedModule();
     void probesInNestedModules();
     void productDependenciesByType();