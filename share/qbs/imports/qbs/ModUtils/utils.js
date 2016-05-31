a9f70a366fdc47d58057116ae70c2d20dfa9c5f3
Jake Petroules <jake.petroules@qt.io>
2016-05-31 09:17:46 -0700

    Fix version validation for limits of 1,1.
    It seems that the {0,} specifier is equivalent to {1,}, so work around
    this.
    
    Change-Id: I06b6b6a7eb9561f948c0b0ba6e83a66800234e74
    Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

--- share/qbs/imports/qbs/ModUtils/utils.js
+++ share/qbs/imports/qbs/ModUtils/utils.js
@@ -322,7 +322,10 @@ var PropertyValidator = (function () {
         this.addCustomValidator(propertyName, propertyValue, function (value) {
             if (typeof value !== "string")
                 return false;
-            return value && value.match("^[0-9]+(\\.[0-9]+){" + ((minComponents - 1) || 0) + "," + ((maxComponents - 1) || "") + "}" + (!allowSuffixes ? "$" : "")) !== null;
+            var comps = "";
+            if ((maxComponents - minComponents) > 0)
+                comps = "(\\.[0-9]+){" + ((minComponents - 1) || 0) + "," + ((maxComponents - 1) || "") + "}";
+            return value && value.match("^[0-9]+" + comps + (!allowSuffixes ? "$" : "")) !== null;
         }, "must be a version number with " + (minComponents === maxComponents
                 ? minComponents : (minComponents + " to " + maxComponents))
                   + (minComponents === maxComponents && minComponents === 1
