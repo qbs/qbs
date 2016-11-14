qbs/qbs

commit d0516e847335e7f432f137bf4774a05b0c007569

Executor: Fix dynamic rules with generated inputs
This has never worked, as the condition that was supposed to collect
such inputs always evaluated to false.

Task-number: QBS-1029
Change-Id: Idf3f86f12fa050a3d151f4551821bf4395f715d8
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/lib/corelib/buildgraph/executor.cpp
@@ -449,9 +449,15 @@ void Executor::executeRuleNode(RuleNode *ruleNode)
         for (Artifact *artifact : filterByType<Artifact>(ruleNode->product->buildData->nodes)) {
             if (artifact->artifactType == Artifact::SourceFile)
                 continue;
-            if (artifact->timestampRetrieved && !isUpToDate(artifact)
-                    && ruleNode->rule()->acceptsAsInput(artifact)) {
-                changedInputArtifacts += artifact;
+            if (ruleNode->rule()->acceptsAsInput(artifact)) {
+                for (const Artifact * const parent : artifact->parentArtifacts()) {
+                    if (parent->transformer->rule != ruleNode->rule())
+                        continue;
+                    if (parent->timestamp() < artifact->timestamp()) {
+                        changedInputArtifacts += artifact;
+                        break;
+                    }
+                }
             }
         }
     }

tests/auto/blackbox/testdata/generated-artifact-as-input-to-dynamic-rule/p.qbs
@@ -1,0 +1,50 @@
+import qbs
+import qbs.File
+import qbs.TextFile
+
+Product {
+    type: ["mytype.final"]
+
+    Group {
+        files: ["input.txt"]
+        fileTags: ["mytype.in"]
+    }
+
+    Rule {
+        inputs: ["mytype.in"]
+        Artifact {
+            filePath: "output.txt"
+            fileTags: ["mytype.out"]
+        }
+        prepare: {
+            var cmd = new JavaScriptCommand();
+            cmd.description = "generating " + output.fileName;
+            cmd.sourceCode = function() { File.copy(input.filePath, output.filePath); };
+            return [cmd];
+        }
+    }
+
+    Rule {
+        inputs: ["mytype.out"]
+        outputFileTags: ["mytype.final"]
+        outputArtifacts: {
+            var file;
+            var inFile = new TextFile(input.filePath, TextFile.ReadOnly);
+            try {
+                file = inFile.readLine();
+                if (!file)
+                    throw "no file name found";
+            } finally {
+                inFile.close();
+           }
+            return [{ filePath: file, fileTags: ["mytype.final"] }];
+        }
+        prepare: {
+            var cmd = new JavaScriptCommand();
+            cmd.description = "generating " + output.fileName;
+            cmd.sourceCode = function() { File.copy(input.filePath, output.filePath); };
+            return [cmd];
+        }
+    }
+}
+

tests/auto/blackbox/tst_blackbox.cpp
@@ -4042,6 +4042,24 @@ void TestBlackbox::frameworkStructure()
     QVERIFY(!directoryExists(relativeProductBuildDir("Widget") + "/Widget.framework/PrivateHeaders"));
 }
 
+void TestBlackbox::generatedArtifactAsInputToDynamicRule()
+{
+    QDir::setCurrent(testDataDir + "/generated-artifact-as-input-to-dynamic-rule");
+    QCOMPARE(runQbs(), 0);
+    const QString oldFile = relativeProductBuildDir("p") + "/old.txt";
+    QVERIFY2(regularFileExists(oldFile), qPrintable(oldFile));
+    WAIT_FOR_NEW_TIMESTAMP();
+    QFile inputFile("input.txt");
+    QVERIFY2(inputFile.open(QIODevice::WriteOnly), qPrintable(inputFile.errorString()));
+    inputFile.resize(0);
+    inputFile.write("new.txt");
+    inputFile.close();
+    QCOMPARE(runQbs(), 0);
+    QVERIFY2(!regularFileExists(oldFile), qPrintable(oldFile));
+    const QString newFile = relativeProductBuildDir("p") + "/new.txt";
+    QVERIFY2(regularFileExists(newFile), qPrintable(oldFile));
+}
+
 static bool haveWiX(const Profile &profile)
 {
     if (profile.value("wix.toolchainInstallPath").isValid() &&

tests/auto/blackbox/tst_blackbox.h
@@ -134,6 +134,7 @@ private slots:
     void exportToOutsideSearchPath();
     void fileDependencies();
     void frameworkStructure();
+    void generatedArtifactAsInputToDynamicRule();
     void groupsInModules();
     void iconset();
     void iconsetApp();

commit 0649b77b020b02436a843ee72a5941f2f875c846

Fix bug that causes re-linking on every build on Linux
Introduced by d0516e8473.

Change-Id: I5bf9e9acab9485ba47c4f5b02e7b0c4ae2493266
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/lib/corelib/buildgraph/executor.cpp
@@ -453,6 +453,8 @@ void Executor::executeRuleNode(RuleNode *ruleNode)
                 for (const Artifact * const parent : artifact->parentArtifacts()) {
                     if (parent->transformer->rule != ruleNode->rule())
                         continue;
+                    if (!parent->alwaysUpdated)
+                        continue;
                     if (parent->timestamp() < artifact->timestamp()) {
                         changedInputArtifacts += artifact;
                         break;

tests/auto/blackbox/testdata/generated-artifact-as-input-to-dynamic-rule/p.qbs
@@ -26,7 +26,7 @@ Product {
 
     Rule {
         inputs: ["mytype.out"]
-        outputFileTags: ["mytype.final"]
+        outputFileTags: ["mytype.final", "dummy"]
         outputArtifacts: {
             var file;
             var inFile = new TextFile(input.filePath, TextFile.ReadOnly);
@@ -37,11 +37,16 @@ Product {
             } finally {
                 inFile.close();
            }
-            return [{ filePath: file, fileTags: ["mytype.final"] }];
+            return [
+                    { filePath: file, fileTags: ["mytype.final"] },
+                    { filePath: "dummy", fileTags: ["dummy"], alwaysUpdated: false }
+            ];
         }
         prepare: {
             var cmd = new JavaScriptCommand();
+            var output = outputs["mytype.final"][0];
             cmd.description = "generating " + output.fileName;
+            cmd.output = output;
             cmd.sourceCode = function() { File.copy(input.filePath, output.filePath); };
             return [cmd];
         }

tests/auto/blackbox/tst_blackbox.cpp
@@ -4058,6 +4058,9 @@ void TestBlackbox::generatedArtifactAsInputToDynamicRule()
     QVERIFY2(!regularFileExists(oldFile), qPrintable(oldFile));
     const QString newFile = relativeProductBuildDir("p") + "/new.txt";
     QVERIFY2(regularFileExists(newFile), qPrintable(oldFile));
+    QVERIFY2(m_qbsStdout.contains("generating"), m_qbsStdout.constData());
+    QCOMPARE(runQbs(), 0);
+    QVERIFY2(!m_qbsStdout.contains("generating"), m_qbsStdout.constData());
 }
 
 static bool haveWiX(const Profile &profile)