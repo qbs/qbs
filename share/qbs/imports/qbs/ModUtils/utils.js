b17e94f932fd8adab4f1a123c3221bbe110c93d1
Joerg Bornemann <joerg.bornemann@qt.io>
2017-01-18 22:27:42 +0100

    Remove ancient debugging functions
    Remove debug output helpers that are not used by anybody.
    There's no need for a cheap re-implementation of JSON.stringify.
    
    Change-Id: I37abd396c305ea186172b6f696b419dc17dd16e6
    Reviewed-by: Jake Petroules <jake.petroules@qt.io>
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- share/qbs/imports/qbs/ModUtils/utils.js
+++ share/qbs/imports/qbs/ModUtils/utils.js
@@ -207,38 +207,6 @@ function modulePropertyFromArtifacts(product, artifacts, moduleName, propertyNam
     return values[0];
 }
 
-function dumpProperty(key, value, level) {
-    var indent = "";
-    for (var k = 0; k < level; ++k)
-        indent += "  ";
-    console.info(indent + key + ": " + value);
-}
-
-function traverseObject(obj, func, level) {
-    if (!level)
-        level = 0;
-    var i, children = {};
-    for (i in obj) {
-        if (typeof(obj[i]) === "object" && !(obj[i] instanceof Array))
-            children[i] = obj[i];
-        else
-            func.apply(this, [i, obj[i], level]);
-    }
-    level++;
-    for (i in children) {
-        func.apply(this, [i, children[i], level - 1]);
-        traverseObject(children[i], func, level);
-    }
-    level--;
-}
-
-function dumpObject(obj, description) {
-    if (!description)
-        description = "object dump";
-    console.info("+++++++++ " + description + " +++++++++");
-    traverseObject(obj, dumpProperty);
-}
-
 function concatAll() {
     var result = [];
     for (var i = 0; i < arguments.length; ++i) {
