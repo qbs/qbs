qbs/qbs

commit 49d4249db09aabaec6c7095b8fc258dbd0a332b8

Use File.directoryEntries() in BlackboxTracker
Instead of a platform-specific external process.

Change-Id: I6c3064c2b755ef587a48558c0a07729253c071f1
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

share/qbs/imports/qbs/ModUtils/utils.js
@@ -485,23 +485,12 @@ var BlackboxOutputArtifactTracker = (function () {
             fileTags: this.defaultFileTags
         };
     };
-    // TODO: Use File.directoryEntries
     BlackboxOutputArtifactTracker.prototype.findFiles = function (dir) {
-        var proc;
-        try {
-            proc = new Process();
-            if (this.hostOS && this.hostOS.contains("windows"))
-                proc.exec(this.shellPath, ["/C", "dir", FileInfo.toWindowsSeparators(dir),
-                                           "/B", "/S", "/A:-D"], true);
-            else
-                proc.exec("find", [dir, "-type", "f"], true);
-            return proc.readStdOut().trim().split(/\r?\n/).map(
-                        function(p) { return FileInfo.fromWindowsSeparators(p); });
-        }
-        finally {
-            if (proc)
-                proc.close();
-        }
+        var fileList = File.directoryEntries(dir, File.Files);
+        var dirList = File.directoryEntries(dir, File.Dirs | File.NoDotDot);
+        for (var i = 0; i < dirList.length; ++i)
+            fileList = fileList.concat(this.findFiles(dirList[i]));
+        return fileList;
     };
     BlackboxOutputArtifactTracker.prototype.fixArtifactPaths = function (artifacts, realBasePath, fakeBasePath) {
         for (var i = 0; i < artifacts.length; ++i)

commit df307637267d2a5f4649811429c592598eb94305

Return absolute paths from BlackboxOutputArtifactTracker
This fixes a regression introduced in 49d4249d which caused the returned
paths to become relative.

Change-Id: Ic4285167994b53838f26b54d21700018928eb783
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/imports/qbs/ModUtils/utils.js
@@ -454,8 +454,10 @@ var BlackboxOutputArtifactTracker = (function () {
         };
     };
     BlackboxOutputArtifactTracker.prototype.findFiles = function (dir) {
-        var fileList = File.directoryEntries(dir, File.Files);
-        var dirList = File.directoryEntries(dir, File.Dirs | File.NoDotAndDotDot);
+        var fileList = File.directoryEntries(dir, File.Files).map(function (p) {
+            return FileInfo.joinPaths(dir, p); });
+        var dirList = File.directoryEntries(dir, File.Dirs | File.NoDotAndDotDot).map(function (p) {
+            return FileInfo.joinPaths(dir, p); });
         for (var i = 0; i < dirList.length; ++i)
             fileList = fileList.concat(this.findFiles(dirList[i]));
         return fileList;