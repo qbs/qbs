qbs/qbs

commit e9fb1cc49edc9d50be5cff2f08599a9056627581

Attach the bundle.content file tag to files that are part of a bundle
This simplifies installation for end users, as they only ever have to
use this one tag to install an entire bundle, rather than having to
remember every possible file tag attached to the output artifacts of
rules which install their output to a bundle.

Refactor associated rules to handle this new internal workflow. This
paves the way for other new functionality like bundle embedding.

Change-Id: I76369383ae08d1ff5433b73d517a657dbd329b78
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/modules/bundle/BundleModule.qbs
@@ -94,7 +94,7 @@ Module {
         }
     }
 
-    additionalProductTypes: ["bundle"]
+    additionalProductTypes: ["bundle.content"]
 
     property bool isBundle: !product.consoleApplication && qbs.targetOS.contains("darwin") &&
         product.type.containsAny(["application", "dynamiclibrary", "loadablemodule"])
@@ -265,17 +265,20 @@ Module {
         multiplex: true
         inputs: ["qbs", "infoplist", "partial_infoplist"]
 
-        outputFileTags: ["aggregate_infoplist"]
+        outputFileTags: ["bundle.input", "aggregate_infoplist"]
         outputArtifacts: {
             var artifacts = [];
             var embed = ModUtils.moduleProperty(product, "embedInfoPlist");
             if (ModUtils.moduleProperty(product, "isBundle") || embed) {
                 artifacts.push({
                     filePath: FileInfo.joinPaths(
-                                  product.destinationDirectory, embed
-                                      ? product.name + "-Info.plist"
-                                      : ModUtils.moduleProperty(product, "infoPlistPath")),
-                    fileTags: ["aggregate_infoplist"]
+                                  product.destinationDirectory, product.name + "-Info.plist"),
+                    fileTags: ["aggregate_infoplist"].concat(!embed ? ["bundle.input"] : []),
+                    bundle: {
+                        _bundleFilePath: FileInfo.joinPaths(
+                                             product.destinationDirectory,
+                                             ModUtils.moduleProperty(product, "infoPlistPath")),
+                    }
                 });
             }
             return artifacts;
@@ -448,13 +451,14 @@ Module {
         multiplex: true
         inputs: ["aggregate_infoplist"]
 
-        outputFileTags: ["pkginfo"]
+        outputFileTags: ["bundle.input", "pkginfo"]
         outputArtifacts: {
             var artifacts = [];
             if (ModUtils.moduleProperty(product, "isBundle") && ModUtils.moduleProperty(product, "generatePackageInfo")) {
                 artifacts.push({
-                    filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "pkgInfoPath")),
-                    fileTags: ["pkginfo"]
+                    filePath: FileInfo.joinPaths(product.destinationDirectory, "PkgInfo"),
+                    fileTags: ["bundle.input", "pkginfo"],
+                    bundle: { _bundleFilePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "pkgInfoPath")) }
                 });
             }
             return artifacts;
@@ -486,12 +490,14 @@ Module {
     Rule {
         condition: qbs.targetOS.contains("darwin")
         multiplex: true
-        inputs: ["aggregate_infoplist", "pkginfo", "hpp",
+        inputs: ["bundle.input",
+                 "aggregate_infoplist", "pkginfo", "hpp",
                  "icns", "xcent",
                  "compiled_ibdoc", "compiled_assetcatalog",
                  "xcode.provisioningprofile.main"]
 
-        outputFileTags: ["bundle",
+        outputFileTags: [
+            "bundle.content",
             "bundle.symlink.headers", "bundle.symlink.private-headers",
             "bundle.symlink.resources", "bundle.symlink.executable",
             "bundle.symlink.version", "bundle.hpp", "bundle.resource",
@@ -499,10 +505,15 @@ Module {
         outputArtifacts: {
             var i, artifacts = [];
             if (ModUtils.moduleProperty(product, "isBundle")) {
-                artifacts.push({
-                    filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "bundleName")),
-                    fileTags: ["bundle"]
-                });
+                for (i in inputs["bundle.input"]) {
+                    var fp = inputs["bundle.input"][i].moduleProperty("bundle", "_bundleFilePath");
+                    if (!fp)
+                        throw("Artifact " + inputs["bundle.input"][i].filePath + " has no associated bundle file path");
+                    artifacts.push({
+                        filePath: fp,
+                        fileTags: ["bundle.content", "bundle.content.copied"]
+                    });
+                }
 
                 for (i in inputs["xcode.provisioningprofile.main"]) {
                     var ext = inputs["xcode.provisioningprofile.main"][i].fileName.split('.')[1];
@@ -511,7 +522,7 @@ Module {
                                                      ModUtils.moduleProperty(product,
                                                                              "contentsFolderPath"),
                                                      "embedded." + ext),
-                        fileTags: ["bundle.provisioningprofile"]
+                        fileTags: ["bundle.provisioningprofile", "bundle.content"]
                     });
                 }
 
@@ -522,7 +533,7 @@ Module {
                     if (publicHeaders && publicHeaders.length) {
                         artifacts.push({
                             filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "bundleName"), "Headers"),
-                            fileTags: ["bundle.symlink.headers"]
+                            fileTags: ["bundle.symlink.headers", "bundle.content"]
                         });
                     }
 
@@ -530,23 +541,23 @@ Module {
                     if (privateHeaders && privateHeaders.length) {
                         artifacts.push({
                             filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "bundleName"), "PrivateHeaders"),
-                            fileTags: ["bundle.symlink.private-headers"]
+                            fileTags: ["bundle.symlink.private-headers", "bundle.content"]
                         });
                     }
 
                     artifacts.push({
                         filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "bundleName"), "Resources"),
-                        fileTags: ["bundle.symlink.resources"]
+                        fileTags: ["bundle.symlink.resources", "bundle.content"]
                     });
 
                     artifacts.push({
                         filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "bundleName"), product.targetName),
-                        fileTags: ["bundle.symlink.executable"]
+                        fileTags: ["bundle.symlink.executable", "bundle.content"]
                     });
 
                     artifacts.push({
                         filePath: FileInfo.joinPaths(product.destinationDirectory, ModUtils.moduleProperty(product, "versionsFolderPath"), "Current"),
-                        fileTags: ["bundle.symlink.version"]
+                        fileTags: ["bundle.symlink.version", "bundle.content"]
                     });
                 }
 
@@ -557,7 +568,7 @@ Module {
                     for (i in sources) {
                         artifacts.push({
                             filePath: FileInfo.joinPaths(destination, FileInfo.fileName(sources[i])),
-                            fileTags: ["bundle.hpp"]
+                            fileTags: ["bundle.hpp", "bundle.content"]
                         });
                     }
                 }
@@ -567,9 +578,15 @@ Module {
                     destination = BundleTools.destinationDirectoryForResource(product, {baseDir: FileInfo.path(sources[i]), fileName: FileInfo.fileName(sources[i])});
                     artifacts.push({
                         filePath: FileInfo.joinPaths(destination, FileInfo.fileName(sources[i])),
-                        fileTags: ["bundle.resource"]
+                        fileTags: ["bundle.resource", "bundle.content"]
                     });
                 }
+
+                var wrapperPath = FileInfo.joinPaths(
+                            product.destinationDirectory,
+                            ModUtils.moduleProperty(product, "bundleName"));
+                for (var i = 0; i < artifacts.length; ++i)
+                    artifacts[i].bundle = { wrapperPath: wrapperPath };
             }
             return artifacts;
         }
@@ -640,6 +657,30 @@ Module {
                 cmd = new Command("ln", ["-sf", FileInfo.joinPaths("Versions", "Current", product.targetName),
                                          executables[i].filePath]);
                 cmd.silent = true;
+                commands.push(cmd);
+            }
+
+            function sortedArtifactList(list, func) {
+                if (list) {
+                    return list.sort(func || (function (a, b) {
+                        return a.filePath.localeCompare(b.filePath);
+                    }));
+                }
+            }
+
+            var bundleInputs = sortedArtifactList(inputs["bundle.input"], function (a, b) {
+                return a.moduleProperty("bundle", "_bundleFilePath").localeCompare(
+                            b.moduleProperty("bundle", "_bundleFilePath"));
+            });
+            var bundleContents = sortedArtifactList(outputs["bundle.content.copied"]);
+            for (i in bundleContents) {
+                cmd = new JavaScriptCommand();
+                cmd.silent = true;
+                cmd.source = bundleInputs[i].filePath;
+                cmd.destination = bundleContents[i].filePath;
+                cmd.sourceCode = function() {
+                    File.copy(source, destination);
+                };
                 commands.push(cmd);
             }
 

share/qbs/modules/cpp/GenericGCC.qbs
@@ -285,13 +285,20 @@ CppModule {
         inputsFromDependencies: ["dynamiclibrary_copy", "staticlibrary"]
 
         outputFileTags: [
+            "bundle.input",
             "dynamiclibrary", "dynamiclibrary_symlink", "dynamiclibrary_copy", "debuginfo_dll"
         ]
         outputArtifacts: {
+            if (product.type.contains("bundle") && !product.type.contains("dynamiclibrary"))
+                return [];
             var lib = {
                 filePath: product.destinationDirectory + "/"
                           + PathTools.dynamicLibraryFilePath(product),
-                fileTags: ["dynamiclibrary"]
+                fileTags: ["bundle.input", "dynamiclibrary"],
+                bundle: {
+                    _bundleFilePath: product.destinationDirectory + "/"
+                                     + PathTools.bundleExecutableFilePath(product)
+                }
             };
             var libCopy = {
                 // List of libfoo's public symbols for smart re-linking.
@@ -307,7 +314,7 @@ CppModule {
                 for (var i = 0; i < maxVersionParts; ++i) {
                     var symlink = {
                         filePath: product.destinationDirectory + "/"
-                                  + PathTools.dynamicLibraryFileName(product, undefined, i),
+                                  + PathTools.dynamicLibraryFilePath(product, undefined, i),
                         fileTags: ["dynamiclibrary_symlink"]
                     };
                     if (i > 0 && artifacts[i-1].filePath == symlink.filePath)
@@ -329,9 +336,11 @@ CppModule {
         inputs: ["obj", "linkerscript"]
         inputsFromDependencies: ["dynamiclibrary", "staticlibrary"]
 
-        outputFileTags: ["staticlibrary", "c_staticlibrary", "cpp_staticlibrary"]
+        outputFileTags: ["bundle.input", "staticlibrary", "c_staticlibrary", "cpp_staticlibrary"]
         outputArtifacts: {
-            var tags = ["staticlibrary"];
+            if (product.type.contains("bundle") && !product.type.contains("staticlibrary"))
+                return [];
+            var tags = ["bundle.input", "staticlibrary"];
             for (var i = 0; i < inputs["obj"].length; ++i) {
                 var ft = inputs["obj"][i].fileTags;
                 if (ft.contains("c_obj"))
@@ -364,7 +373,11 @@ CppModule {
                 filePath: FileInfo.joinPaths(product.destinationDirectory,
                                              PathTools.staticLibraryFilePath(product)),
                 fileTags: tags,
-                cpp: { "staticLibraries": staticLibraries, "dynamicLibraries": dynamicLibraries }
+                cpp: { "staticLibraries": staticLibraries, "dynamicLibraries": dynamicLibraries },
+                bundle: {
+                    _bundleFilePath: FileInfo.joinPaths(product.destinationDirectory,
+                                                        PathTools.bundleExecutableFilePath(product))
+                }
             }];
         }
 
@@ -393,12 +406,18 @@ CppModule {
         }
         inputsFromDependencies: ["dynamiclibrary_copy", "staticlibrary"]
 
-        outputFileTags: ["loadablemodule", "debuginfo_loadablemodule"]
+        outputFileTags: ["bundle.input", "loadablemodule", "debuginfo_loadablemodule"]
         outputArtifacts: {
+            if (product.type.contains("bundle") && !product.type.contains("loadablemodule"))
+                return [];
             var app = {
                 filePath: FileInfo.joinPaths(product.destinationDirectory,
                                              PathTools.loadableModuleFilePath(product)),
-                fileTags: ["loadablemodule"]
+                fileTags: ["bundle.input", "loadablemodule"],
+                bundle: {
+                    _bundleFilePath: FileInfo.joinPaths(product.destinationDirectory,
+                                                        PathTools.bundleExecutableFilePath(product))
+                }
             }
             return [app].concat(Gcc.debugInfoArtifacts(product, "loadablemodule"));
         }
@@ -421,12 +440,18 @@ CppModule {
         }
         inputsFromDependencies: ["dynamiclibrary_copy", "staticlibrary"]
 
-        outputFileTags: ["application", "debuginfo_app"]
+        outputFileTags: ["bundle.input", "application", "debuginfo_app"]
         outputArtifacts: {
+            if (product.type.contains("bundle") && !product.type.contains("application"))
+                return [];
             var app = {
                 filePath: FileInfo.joinPaths(product.destinationDirectory,
                                              PathTools.applicationFilePath(product)),
-                fileTags: ["application"]
+                fileTags: ["bundle.input", "application"],
+                bundle: {
+                    _bundleFilePath: FileInfo.joinPaths(product.destinationDirectory,
+                                                        PathTools.bundleExecutableFilePath(product))
+                }
             }
             return [app].concat(Gcc.debugInfoArtifacts(product, "app"));
         }

doc/reference/modules/bundle-module.qdoc
@@ -293,4 +293,19 @@
         \li Path of the bundle's Versions subdirectory.
             This is only relevant for (non-shallow) framework bundles.
     \endtable
+
+    \section1 Relevant File Tags
+
+    \table
+    \header
+        \li Tag
+        \li Auto-tagged File Names
+        \li Since
+        \li Description
+    \row
+        \li \c{"bundle.content"}
+        \li -
+        \li 1.8
+        \li This tag is attached to the output artifacts of the rule that produces the bundle.
+    \endtable
 */

examples/install-bundle/Storyboard.storyboard
@@ -1,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.Cocoa.Storyboard.XIB" version="3.0" toolsVersion="8152.3" systemVersion="14E46" targetRuntime="MacOSX.Cocoa" propertyAccessControl="none" useAutolayout="YES">
+    <dependencies>
+        <deployment version="0.0" identifier="macosx"/>
+        <development version="5100" identifier="xcode"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.CocoaPlugin" version="8152.3"/>
+    </dependencies>
+    <scenes>
+        <!--View Controller-->
+        <scene sceneID="hyC-et-BQD">
+            <objects>
+                <viewController storyboardIdentifier="Main" id="1os-k8-h10" sceneMemberID="viewController">
+                    <view key="view" id="qKA-a5-eUe">
+                        <rect key="frame" x="0.0" y="0.0" width="450" height="300"/>
+                        <autoresizingMask key="autoresizingMask"/>
+                    </view>
+                    <connections>
+                        <segue destination="Yem-rc-72E" kind="show" id="smB-wf-uRu"/>
+                        <segue destination="Iqk-Fi-Vhk" kind="show" id="mam-9U-Q1A"/>
+                    </connections>
+                </viewController>
+                <customObject id="2Oa-eW-Ce9" userLabel="First Responder" customClass="NSResponder" sceneMemberID="firstResponder"/>
+            </objects>
+            <point key="canvasLocation" x="479" y="205"/>
+        </scene>
+        <!--View Controller-->
+        <scene sceneID="ivR-Sv-tI6">
+            <objects>
+                <viewController id="Iqk-Fi-Vhk" sceneMemberID="viewController">
+                    <view key="view" id="HRv-3O-Qxh">
+                        <rect key="frame" x="0.0" y="0.0" width="450" height="300"/>
+                        <autoresizingMask key="autoresizingMask"/>
+                    </view>
+                </viewController>
+                <customObject id="Lyt-Nt-lfk" userLabel="First Responder" customClass="NSResponder" sceneMemberID="firstResponder"/>
+            </objects>
+            <point key="canvasLocation" x="946" y="206"/>
+        </scene>
+        <!--View Controller-->
+        <scene sceneID="YQV-Q9-5te">
+            <objects>
+                <viewController id="Yem-rc-72E" sceneMemberID="viewController">
+                    <view key="view" id="ODp-aO-Dmf">
+                        <rect key="frame" x="0.0" y="0.0" width="450" height="300"/>
+                        <autoresizingMask key="autoresizingMask"/>
+                    </view>
+                </viewController>
+                <customObject id="W89-TH-tp2" userLabel="First Responder" customClass="NSResponder" sceneMemberID="firstResponder"/>
+            </objects>
+            <point key="canvasLocation" x="728" y="553"/>
+        </scene>
+    </scenes>
+</document>

commit e093bda8b4a894d0f6824adfcc5556b958e7bb59

Make sure the bundle rule only accepts inputs from compatible rules
This fixes a regression introduced in e9fb1cc which broke all bundle
products on Apple platforms.

Change-Id: Ice17bb1d6d6b535d8c654ae6c717f32d9b2ea41e
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/modules/bundle/BundleModule.qbs
@@ -69,6 +69,7 @@ Module {
 
         // Outputs
         property var xcodeSettings: ({})
+        property var productTypeIdentifierChain: []
 
         configure: {
             var specsPath = path;
@@ -84,11 +85,14 @@ Module {
                                                           additionalSettings,
                                                           !qbs.targetOS.contains("macos"));
             var settings = reader.expandedSettings(_productTypeIdentifier);
-            if (settings) {
+            var chain = reader.productTypeIdentifierChain(_productTypeIdentifier);
+            if (settings && chain) {
                 xcodeSettings = settings;
+                productTypeIdentifierChain = chain;
                 found = true;
             } else {
                 xcodeSettings = {};
+                productTypeIdentifierChain = [];
                 found = false;
             }
         }
@@ -180,6 +184,7 @@ Module {
 
     // private properties
     property string _productTypeIdentifier: Bundle.productTypeIdentifier(product.type)
+    property stringList _productTypeIdentifierChain: bundleSettingsProbe.productTypeIdentifierChain
 
     property bool _useXcodeBuildSpecs: true // false to use ONLY the qbs build specs
 
@@ -495,6 +500,10 @@ Module {
                  "icns", "xcent",
                  "compiled_ibdoc", "compiled_assetcatalog",
                  "xcode.provisioningprofile.main"]
+
+        // Make sure the inputs of this rule are only those rules which produce outputs compatible
+        // with the type of the bundle being produced.
+        excludedAuxiliaryInputs: Bundle.excludedAuxiliaryInputs(project, product)
 
         outputFileTags: [
             "bundle.content",

share/qbs/modules/bundle/bundle.js
@@ -29,6 +29,7 @@
 ****************************************************************************/
 
 var DarwinTools = require("qbs.DarwinTools");
+var ModUtils = require("qbs.ModUtils");
 var Process = require("qbs.Process");
 
 // HACK: Workaround until the PropertyList extension is supported cross-platform
@@ -85,6 +86,37 @@ function productTypeIdentifier(productType) {
     return "com.apple.package-type.wrapper";
 }
 
+function excludedAuxiliaryInputs(project, product) {
+    var chain = product.moduleProperty("bundle", "_productTypeIdentifierChain");
+    var bestPossibleType;
+    for (var i = chain.length - 1; i >= 0; --i) {
+        switch (chain[i]) {
+        case "com.apple.product-type.bundle":
+            bestPossibleType = "loadablemodule";
+            break;
+        case "com.apple.product-type.framework":
+            bestPossibleType = "dynamiclibrary";
+            break;
+        case "com.apple.product-type.framework.static":
+            bestPossibleType = "staticlibrary";
+            break;
+        case "com.apple.product-type.application":
+        case "com.apple.product-type.xpc-service":
+            bestPossibleType = "application";
+            break;
+        }
+    }
+
+    var excluded = [];
+    var possibleTypes = ["application", "dynamiclibrary", "staticlibrary", "loadablemodule"];
+    for (i = 0; i < possibleTypes.length; ++i) {
+        if (possibleTypes[i] !== bestPossibleType)
+            excluded.push(possibleTypes[i]);
+    }
+
+    return excluded;
+}
+
 var XcodeBuildSpecsReader = (function () {
     function XcodeBuildSpecsReader(specsPath, separator, additionalSettings, useShallowBundles) {
         this._additionalSettings = additionalSettings;
@@ -107,6 +139,14 @@ var XcodeBuildSpecsReader = (function () {
             plist2.clear();
         }
     }
+    XcodeBuildSpecsReader.prototype.productTypeIdentifierChain = function (typeIdentifier) {
+        var ids = [typeIdentifier];
+        var obj = this._types[typeIdentifier];
+        var parentId = obj && obj["BasedOn"];
+        if (parentId)
+            return ids.concat(this.productTypeIdentifierChain(parentId));
+        return ids;
+    };
     XcodeBuildSpecsReader.prototype.settings = function (typeIdentifier, recursive, skipPackageTypes) {
         // Silently use shallow bundles when preferred since it seems to be some sort of automatic
         // shadowing mechanism. For example, this matches Xcode behavior where static frameworks

share/qbs/modules/cpp/GenericGCC.qbs
@@ -289,8 +289,6 @@ CppModule {
             "dynamiclibrary", "dynamiclibrary_symlink", "dynamiclibrary_copy", "debuginfo_dll"
         ]
         outputArtifacts: {
-            if (product.type.contains("bundle") && !product.type.contains("dynamiclibrary"))
-                return [];
             var lib = {
                 filePath: product.destinationDirectory + "/"
                           + PathTools.dynamicLibraryFilePath(product),
@@ -338,8 +336,6 @@ CppModule {
 
         outputFileTags: ["bundle.input", "staticlibrary", "c_staticlibrary", "cpp_staticlibrary"]
         outputArtifacts: {
-            if (product.type.contains("bundle") && !product.type.contains("staticlibrary"))
-                return [];
             var tags = ["bundle.input", "staticlibrary"];
             for (var i = 0; i < inputs["obj"].length; ++i) {
                 var ft = inputs["obj"][i].fileTags;
@@ -408,8 +404,6 @@ CppModule {
 
         outputFileTags: ["bundle.input", "loadablemodule", "debuginfo_loadablemodule"]
         outputArtifacts: {
-            if (product.type.contains("bundle") && !product.type.contains("loadablemodule"))
-                return [];
             var app = {
                 filePath: FileInfo.joinPaths(product.destinationDirectory,
                                              PathTools.loadableModuleFilePath(product)),
@@ -442,8 +436,6 @@ CppModule {
 
         outputFileTags: ["bundle.input", "application", "debuginfo_app"]
         outputArtifacts: {
-            if (product.type.contains("bundle") && !product.type.contains("application"))
-                return [];
             var app = {
                 filePath: FileInfo.joinPaths(product.destinationDirectory,
                                              PathTools.applicationFilePath(product)),