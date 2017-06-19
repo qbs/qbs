b2677d0789d85a3136e41fba75c82782fefb7009
Joerg Bornemann <joerg.bornemann@qt.io>
2017-06-19 11:43:21 +0200

    Fix error messages in console functions
    Change-Id: I8bd20c7ca4cae81193bf4def946846fd393d170e
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/lib/corelib/language/scriptengine.cpp
+++ src/lib/corelib/language/scriptengine.cpp
@@ -580,7 +580,7 @@ static QScriptValue js_consoleError(QScriptContext *context, QScriptEngine *engi
 {
     if (Q_UNLIKELY(context->argumentCount() != 1))
         return context->throwError(QScriptContext::SyntaxError,
-                                   QLatin1String("error expects 1 argument"));
+                                   QLatin1String("console.error() expects 1 argument"));
     logger->qbsLog(LoggerError) << context->argument(0).toString();
     return engine->undefinedValue();
 }
@@ -589,7 +589,7 @@ static QScriptValue js_consoleWarn(QScriptContext *context, QScriptEngine *engin
 {
     if (Q_UNLIKELY(context->argumentCount() != 1))
         return context->throwError(QScriptContext::SyntaxError,
-                                   QLatin1String("error expects 1 argument"));
+                                   QLatin1String("console.warn() expects 1 argument"));
     logger->qbsWarning() << context->argument(0).toString();
     return engine->undefinedValue();
 }
@@ -598,7 +598,7 @@ static QScriptValue js_consoleInfo(QScriptContext *context, QScriptEngine *engin
 {
     if (Q_UNLIKELY(context->argumentCount() != 1))
         return context->throwError(QScriptContext::SyntaxError,
-                                   QLatin1String("error expects 1 argument"));
+                                   QLatin1String("console.info() expects 1 argument"));
     logger->qbsInfo() << context->argument(0).toString();
     return engine->undefinedValue();
 }
@@ -607,7 +607,7 @@ static QScriptValue js_consoleDebug(QScriptContext *context, QScriptEngine *engi
 {
     if (Q_UNLIKELY(context->argumentCount() != 1))
         return context->throwError(QScriptContext::SyntaxError,
-                                   QLatin1String("error expects 1 argument"));
+                                   QLatin1String("console.debug() expects 1 argument"));
     logger->qbsDebug() << context->argument(0).toString();
     return engine->undefinedValue();
 }
