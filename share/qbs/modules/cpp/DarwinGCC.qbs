qbs/qbs

commit 4551d11102e6a82bc787ccfc79a8d8bc4e9eda77

Don't set minimum deployment targets for iOS and macOS when using libc++
This causes linker errors with the latest versions of Xcode because the
versions set are too old.

Change-Id: If125667ae3b0c410d59f51b1fb86f77ef50e6e79
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/modules/cpp/ios-gcc.qbs
@@ -40,7 +40,6 @@ DarwinGCC {
 
     targetSystem: "ios" + (minimumIosVersion || "")
 
-    minimumIosVersion: cxxStandardLibrary === "libc++" ? "5.0" : undefined
     minimumDarwinVersion: minimumIosVersion
     minimumDarwinVersionCompilerFlag: qbs.targetOS.contains("ios-simulator")
                                       ? "-mios-simulator-version-min"

share/qbs/modules/cpp/macos-gcc.qbs
@@ -37,7 +37,6 @@ DarwinGCC {
 
     targetSystem: "macosx" + (minimumMacosVersion || "")
 
-    minimumOsxVersion: cxxStandardLibrary === "libc++" ? "10.7" : undefined
     minimumDarwinVersion: minimumMacosVersion
     minimumDarwinVersionCompilerFlag: "-mmacosx-version-min"
     minimumDarwinVersionLinkerFlag: "-macosx_version_min"

commit 1913b32bd828c53f6845ed00dae99512e8d81321

Fix tests with older versions of Apple Clang
Amends and fixes a regression introduced in 4551d11.

Change-Id: I8014c8bfb82c389d4ae741f868be45e420e327ca
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/modules/cpp/DarwinGCC.qbs
@@ -50,8 +50,7 @@ UnixGCC {
     imageFormat: "macho"
 
     compilerDefines: ["__GNUC__=4", "__APPLE__"]
-    cxxStandardLibrary: qbs.toolchain.contains("clang") && cxxLanguageVersion !== "c++98"
-                        ? "libc++" : base
+    cxxStandardLibrary: libcxxAvailable ? "libc++" : base
     loadableModulePrefix: ""
     loadableModuleSuffix: ".bundle"
     dynamicLibrarySuffix: ".dylib"
@@ -178,6 +177,8 @@ UnixGCC {
     property string minimumDarwinVersion
     property string minimumDarwinVersionCompilerFlag
     property string minimumDarwinVersionLinkerFlag
+
+    property bool libcxxAvailable: qbs.toolchain.contains("clang") && cxxLanguageVersion !== "c++98"
 
     Rule {
         condition: product.aggregate

share/qbs/modules/cpp/ios-gcc.qbs
@@ -33,6 +33,7 @@ import qbs.DarwinTools
 import qbs.File
 import qbs.FileInfo
 import qbs.ModUtils
+import qbs.Utilities
 
 DarwinGCC {
     condition: qbs.targetOS.contains('ios') &&
@@ -47,6 +48,10 @@ DarwinGCC {
     minimumDarwinVersionLinkerFlag: qbs.targetOS.contains("ios-simulator")
                                     ? "-ios_simulator_version_min"
                                     : "-iphoneos_version_min"
+
+    libcxxAvailable: base
+                     && minimumDarwinVersion
+                     && Utilities.versionCompare(minimumDarwinVersion, "5") >= 0
 
     platformObjcFlags: base.concat(simulatorObjcFlags)
     platformObjcxxFlags: base.concat(simulatorObjcFlags)

share/qbs/modules/cpp/macos-gcc.qbs
@@ -30,6 +30,7 @@
 
 import qbs 1.0
 import qbs.ModUtils
+import qbs.Utilities
 
 DarwinGCC {
     condition: qbs.targetOS.contains('macos') &&
@@ -40,4 +41,8 @@ DarwinGCC {
     minimumDarwinVersion: minimumMacosVersion
     minimumDarwinVersionCompilerFlag: "-mmacosx-version-min"
     minimumDarwinVersionLinkerFlag: "-macosx_version_min"
+
+    libcxxAvailable: base
+                     && minimumDarwinVersion
+                     && Utilities.versionCompare(minimumDarwinVersion, "10.7") >= 0
 }

tests/auto/blackbox/testdata-apple/apple-multiconfig/apple-multiconfig.qbs
@@ -11,6 +11,7 @@ Project {
         targetName: "singleapp"
         files: ["app.c"]
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path/../../../"] : []
+        cpp.minimumMacosVersion: "10.5"
 
         // Turn off multiplexing
         aggregate: false
@@ -30,6 +31,7 @@ Project {
         targetName: "singleapp_agg"
         files: ["app.c"]
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path/../../../"] : []
+        cpp.minimumMacosVersion: "10.5"
 
         // Force aggregation when not needed
         aggregate: true
@@ -70,6 +72,7 @@ Project {
         targetName: "multiapp"
         files: ["app.c"]
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path/../../../"] : []
+        cpp.minimumMacosVersion: "10.5"
 
         Group {
             fileTagsFilter: ["bundle.content"]
@@ -85,6 +88,7 @@ Project {
         targetName: "fatmultiapp"
         files: ["app.c"]
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path/../../../"] : []
+        cpp.minimumMacosVersion: "10.5"
         qbs.architectures: ["x86", "x86_64"]
 
         Group {

tests/auto/blackbox/testdata-apple/embedInfoPlist/embedInfoPlist.qbs
@@ -9,6 +9,7 @@ Project {
         files: ["main.m"]
         cpp.frameworks: ["Foundation"]
         cpp.rpaths: ["@loader_path"]
+        cpp.minimumMacosVersion: "10.5"
         bundle.infoPlist: ({
             "QBS": "org.qt-project.qbs.testdata.embedInfoPlist"
         })

tests/auto/blackbox/testdata/lexyacc/one-grammar/one-grammar.qbs
@@ -6,6 +6,7 @@ CppApplication {
     lex_yacc.yaccFlags: ["-l"]
     cpp.includePaths: ["."]
     cpp.cxxLanguageVersion: "c++11"
+    cpp.minimumMacosVersion: "10.7"
     consoleApplication: true
     files: [
         "lexer.l",

tests/auto/blackbox/testdata/symbolLinkMode/symbolLinkMode.qbs
@@ -28,6 +28,7 @@ Project {
             return ["WEAK_IMPORT=", installLib];
         }
         cpp.cxxLanguageVersion: "c++11"
+        cpp.minimumMacosVersion: "10.5"
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path/../lib"] : ["$ORIGIN/../lib"]
 
         Group {
@@ -46,6 +47,7 @@ Project {
         name: "functions"
         files: ["lib.cpp"]
         cpp.cxxLanguageVersion: "c++11"
+        cpp.minimumMacosVersion: "10.5"
         cpp.rpaths: qbs.targetOS.contains("darwin") ? ["@loader_path"] : ["$ORIGIN"]
 
         Properties {
@@ -85,6 +87,7 @@ Project {
         name: "indirect"
         files: ["indirect.cpp"]
         cpp.cxxLanguageVersion: "c++11"
+        cpp.minimumMacosVersion: "10.5"
 
         Properties {
             condition: qbs.targetOS.contains("darwin")