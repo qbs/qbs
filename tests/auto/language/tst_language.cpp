qbs/qbs

commit b6bf17cfd1cad7a673082e80896edec889809446

Make use of more data from the build graph when rebuilding
Assume we are building a project this way:
$ qbs -f src/myproject.qbs modules.cpp.warningLevel:none

Upon rebuilding, we had to list the project file and the property value
again, even though both were stored in the build graph:
$ qbs # Failure, no project file given
$ qbs -f src/myproject.qbs # cpp.warningLevel:<default>

With this patch, project file and property values as well as the
environment are taken from the build graph, unless the user
requests otherwise by using the resolve command:
$ qbs # ok, uses the original values
$ qbs -f someotherproject.qbs # error, project file has changed
$ qbs resolve -f someotherproject.qbs # ok, using new project file
$ qbs # ok, builds using the new project file
$ qbs cpp.warningLevel:none # ok, no changes
$ qbs cpp.warningLevel:all # error, conflicting properties
$ qbs resolve cpp.warningLevel:all # ok, using new set of properties
$ qbs # ok, build using new set of properties

[ChangeLog] When rebuilding a project, the environment, project file and
property values are taken from the existing build graph.

Change-Id: I3c691920c3fc8ced2abba13258a6d384a9ed4eaa
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/app/qbs/parser/commandlineparser.cpp
@@ -343,9 +343,9 @@ void CommandLineParser::CommandLineParserPrivate::doParse()
     if (command->type() == BuildCommandType && optionPool.versionOption()->enabled())
         return;
 
-    setupProjectFile();
     setupBuildDirectory();
     setupBuildConfigurations();
+    setupProjectFile();
     setupProgress();
     setupLogLevel();
     setupBuildOptions();
@@ -429,6 +429,28 @@ void CommandLineParser::CommandLineParserPrivate::setupProjectFile()
 {
     projectFilePath = optionPool.fileOption()->projectFilePath();
     if (projectFilePath.isEmpty()) {
+        bool allBuildGraphsExist = true;
+        foreach (const QVariantMap &buildConfig, buildConfigurations) {
+            const QString configName = buildConfig.value(QLatin1String("qbs.configurationName"))
+                    .toString();
+            const QString profile = buildConfig.value(QLatin1String("qbs.profile")).toString();
+            QString buildDir = projectBuildDirectory;
+            Settings settings(settingsDir());
+            if (buildDir.isEmpty()) {
+                buildDir = Preferences(&settings, profile).defaultBuildDirectory();
+                if (buildDir.isEmpty())
+                    buildDir = QDir::currentPath();
+            }
+            if (!QFile::exists(buildDir + QLatin1Char('/') + configName + QLatin1Char('/')
+                               + configName + QLatin1String(".bg"))) {
+                allBuildGraphsExist = false;
+                break;
+            }
+        }
+        if (allBuildGraphsExist) {
+            qbsDebug() << "No project file given; using the one from the build graph.";
+            return;
+        }
         qbsDebug() << "No project file given; looking in current directory.";
         projectFilePath = QDir::currentPath();
     }

src/lib/corelib/buildgraph/buildgraphloader.cpp
@@ -106,7 +106,8 @@ BuildGraphLoadResult BuildGraphLoader::load(const TopLevelProjectPtr &existingPr
     if (existingProject) {
         QBS_CHECK(existingProject->buildData);
         existingProject->buildData->evaluationContext = evalContext;
-        checkBuildGraphCompatibility(existingProject);
+        if (!checkBuildGraphCompatibility(existingProject))
+            return m_result;
         m_result.loadedProject = existingProject;
     } else {
         loadBuildGraphFromDisk();
@@ -149,8 +150,10 @@ void BuildGraphLoader::loadBuildGraphFromDisk()
     try {
         pool.load(buildGraphFilePath);
     } catch (const ErrorInfo &loadError) {
-        if (m_parameters.restoreBehavior() == SetupProjectParameters::RestoreOnly)
+        if (m_parameters.restoreBehavior() == SetupProjectParameters::RestoreOnly
+                || m_parameters.projectFilePath().isEmpty()) {
             throw;
+        }
         m_logger.qbsInfo() << loadError.toString();
         return;
     }
@@ -164,7 +167,8 @@ void BuildGraphLoader::loadBuildGraphFromDisk()
     project->buildData->evaluationContext = m_evalContext;
     project->setBuildConfiguration(pool.headData().projectConfig);
     project->buildDirectory = buildDir;
-    checkBuildGraphCompatibility(project);
+    if (!checkBuildGraphCompatibility(project))
+        return;
     restoreBackPointers(project);
     project->location = CodeLocation(m_parameters.projectFilePath(), project->location.line(),
                                      project->location.column());
@@ -173,23 +177,24 @@ void BuildGraphLoader::loadBuildGraphFromDisk()
     doSanityChecks(project, m_logger);
 }
 
-void BuildGraphLoader::checkBuildGraphCompatibility(const TopLevelProjectConstPtr &project)
+bool BuildGraphLoader::checkBuildGraphCompatibility(const TopLevelProjectConstPtr &project)
 {
-    if (QFileInfo(project->location.filePath()) != QFileInfo(m_parameters.projectFilePath())) {
-        QString errorMessage = Tr::tr("Stored build graph at '%1' is for project file '%2', but "
-                                      "input file is '%3'. ")
-                .arg(QDir::toNativeSeparators(project->buildGraphFilePath()),
-                     QDir::toNativeSeparators(project->location.filePath()),
-                     QDir::toNativeSeparators(m_parameters.projectFilePath()));
-        if (!m_parameters.ignoreDifferentProjectFilePath()) {
-            errorMessage += Tr::tr("Aborting.");
-            throw ErrorInfo(errorMessage);
-        }
-
-        // Okay, let's assume it's the same project anyway (the source dir might have moved).
-        errorMessage += Tr::tr("Ignoring.");
-        m_logger.qbsWarning() << errorMessage;
+    if (m_parameters.projectFilePath().isEmpty())
+        m_parameters.setProjectFilePath(project->location.filePath());
+    if (QFileInfo(project->location.filePath()) == QFileInfo(m_parameters.projectFilePath()))
+        return true;
+    QString message = Tr::tr("Stored build graph at '%1' is for project file '%2', but "
+                             "input file is '%3'.")
+            .arg(QDir::toNativeSeparators(project->buildGraphFilePath()),
+                 QDir::toNativeSeparators(project->location.filePath()),
+                 QDir::toNativeSeparators(m_parameters.projectFilePath()));
+    if (m_parameters.overrideBuildGraphData()) {
+        m_logger.qbsInfo() << message;
+        return false;
     }
+    message.append(QLatin1Char('\n')).append(Tr::tr("Use the 'resolve' command to enforce "
+                                                    "using a different project file."));
+    throw ErrorInfo(message);
 }
 
 static bool checkProductForChangedDependency(QList<ResolvedProductPtr> &changedProducts,
@@ -230,7 +235,7 @@ void BuildGraphLoader::trackProjectChanges()
     QList<ResolvedProductPtr> allRestoredProducts = restoredProject->allProducts();
     QList<ResolvedProductPtr> changedProducts;
     bool reResolvingNecessary = false;
-    if (!isConfigCompatible())
+    if (!checkConfigCompatibility())
         reResolvingNecessary = true;
     if (hasProductFileChanged(allRestoredProducts, restoredProject->lastResolveTime,
                               buildSystemFiles, changedProducts)) {
@@ -258,6 +263,8 @@ void BuildGraphLoader::trackProjectChanges()
     }
 
     restoredProject->buildData->isDirty = true;
+    if (!m_parameters.overrideBuildGraphData())
+        m_parameters.setEnvironment(restoredProject->environment);
     Loader ldr(m_evalContext->engine(), m_logger);
     ldr.setSearchPaths(m_parameters.searchPaths());
     ldr.setProgressObserver(m_evalContext->observer());
@@ -388,6 +395,8 @@ bool BuildGraphLoader::probeExecutionForced(
 
 bool BuildGraphLoader::hasEnvironmentChanged(const TopLevelProjectConstPtr &restoredProject) const
 {
+    if (!m_parameters.overrideBuildGraphData())
+        return false;
     QProcessEnvironment oldEnv = restoredProject->environment;
     QProcessEnvironment newEnv = m_parameters.adjustedEnvironment();
 
@@ -833,19 +842,52 @@ void BuildGraphLoader::replaceFileDependencyWithArtifact(const ResolvedProductPt
     m_objectsToDelete << filedep;
 }
 
-bool BuildGraphLoader::isConfigCompatible()
+bool BuildGraphLoader::checkConfigCompatibility()
 {
     const TopLevelProjectConstPtr restoredProject = m_result.loadedProject;
-    if (m_parameters.finalBuildConfigurationTree() != restoredProject->buildConfiguration())
-        return false;
+    if (!m_parameters.overrideBuildGraphData()) {
+        if (!m_parameters.overriddenValues().isEmpty()
+                && m_parameters.overriddenValues() != restoredProject->overriddenValues) {
+            throw ErrorInfo(Tr::tr("Property values set on the command line differ from the "
+                                   "ones used for the previous build. Use the 'resolve' command if "
+                                   "you really want to rebuild with the new properties."));
+            }
+        m_parameters.setOverriddenValues(restoredProject->overriddenValues);
+        if (!m_parameters.topLevelProfile().isEmpty()
+                && m_parameters.topLevelProfile() != restoredProject->profile()) {
+            throw ErrorInfo(Tr::tr("The current profile is '%1', but profile '%2' was used "
+                                   "when last building for configuration '%3'. Use  the "
+                                   "'resolve' command if you really want to rebuild with a "
+                                   "different profile.")
+                            .arg(m_parameters.topLevelProfile(), restoredProject->profile(),
+                                 m_parameters.configurationName()));
+        }
+        m_parameters.setTopLevelProfile(restoredProject->profile());
+        m_parameters.expandBuildConfiguration();
+    }
+    if (m_parameters.finalBuildConfigurationTree() != restoredProject->buildConfiguration()) {
+        if (m_parameters.overrideBuildGraphData())
+            return false;
+        throw ErrorInfo(Tr::tr("The current set of properties for configuration '%1' differs "
+                               "from the one used in the last build. Use the 'resolve' "
+                               "command if you really want to rebuild with the new properties.")
+                        .arg(m_parameters.configurationName()));
+    }
     for (QVariantMap::ConstIterator it = restoredProject->profileConfigs.constBegin();
          it != restoredProject->profileConfigs.constEnd(); ++it) {
         const QVariantMap buildConfig = SetupProjectParameters::expandedBuildConfiguration(
                     m_parameters.settingsDirectory(), it.key(), m_parameters.configurationName());
         const QVariantMap newConfig = SetupProjectParameters::finalBuildConfigurationTree(
                     buildConfig, m_parameters.overriddenValues(), m_parameters.buildRoot());
-        if (newConfig != it.value())
-            return false;
+        if (newConfig != it.value()) {
+            if (m_parameters.overrideBuildGraphData())
+                return false;
+            throw ErrorInfo(Tr::tr("The current set of properties for configuration '%1' differs "
+                                   "in profile '%2' from the one used for the last build. "
+                                   "Use the 'resolve' command if you really want to rebuild "
+                                   "with the new properties.")
+                            .arg(m_parameters.configurationName(), it.key()));
+        }
     }
     return true;
 }

src/lib/corelib/language/loader.cpp
@@ -47,8 +47,10 @@
 
 #include <logging/translator.h>
 #include <tools/fileinfo.h>
+#include <tools/profile.h>
 #include <tools/progressobserver.h>
 #include <tools/qbsassert.h>
+#include <tools/settings.h>
 #include <tools/setupprojectparameters.h>
 
 #include <QtCore/qdir.h>
@@ -96,10 +98,22 @@ void Loader::setOldProductProbes(const QHash<QString, QList<ProbeConstPtr>> &old
     m_oldProductProbes = oldProbes;
 }
 
-TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &parameters)
+TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &_parameters)
 {
-    QBS_CHECK(QFileInfo(parameters.projectFilePath()).isAbsolute());
+    QBS_CHECK(QFileInfo(_parameters.projectFilePath()).isAbsolute());
 
+    SetupProjectParameters parameters = _parameters;
+    if (parameters.topLevelProfile().isEmpty()) {
+        Settings settings(parameters.settingsDirectory());
+        QString profileName = settings.defaultProfile();
+        if (profileName.isEmpty()) {
+            m_logger.qbsDebug() << Tr::tr("No profile specified and no default profile exists. "
+                                          "Using default property values.");
+            profileName = Profile::fallbackName();
+        }
+        parameters.setTopLevelProfile(profileName);
+        parameters.expandBuildConfiguration();
+    }
     m_engine->setEnvironment(parameters.adjustedEnvironment());
     m_engine->clearExceptions();
     m_engine->clearImportsCache();

src/lib/corelib/tools/persistence.cpp
@@ -50,7 +50,7 @@
 namespace qbs {
 namespace Internal {
 
-static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-99";
+static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-100";
 
 PersistentPool::PersistentPool(Logger &logger) : m_logger(logger)
 {

commit 77e407ef6b63253154ed692f69a2fac53d8c3fdf

Fix behavior with corrupt or outdated build graphs
Commit b6bf17cfd1 introduced a glitch in that a rebuild using a build
graph whose magic string does not match would throw an error at the
first attempt and silently succeed at the second one. The new behavior
is the same as for attempts to rebuild with property changes: A plain
"build" fails, a "resolve" does the trick. Some more code had to move
from the command line parser to the loader to achieve this.

Change-Id: I906748cc1686c0c5d413d9451ec9e2aa26ea2035
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/app/qbs/parser/commandlineparser.cpp
@@ -428,66 +428,6 @@ QString CommandLineParser::CommandLineParserPrivate::generalHelp() const
 void CommandLineParser::CommandLineParserPrivate::setupProjectFile()
 {
     projectFilePath = optionPool.fileOption()->projectFilePath();
-    if (projectFilePath.isEmpty()) {
-        bool allBuildGraphsExist = true;
-        foreach (const QVariantMap &buildConfig, buildConfigurations) {
-            const QString configName = buildConfig.value(QLatin1String("qbs.configurationName"))
-                    .toString();
-            const QString profile = buildConfig.value(QLatin1String("qbs.profile")).toString();
-            QString buildDir = projectBuildDirectory;
-            Settings settings(settingsDir());
-            if (buildDir.isEmpty()) {
-                buildDir = Preferences(&settings, profile).defaultBuildDirectory();
-                if (buildDir.isEmpty())
-                    buildDir = QDir::currentPath();
-            }
-            if (!QFile::exists(buildDir + QLatin1Char('/') + configName + QLatin1Char('/')
-                               + configName + QLatin1String(".bg"))) {
-                allBuildGraphsExist = false;
-                break;
-            }
-        }
-        if (allBuildGraphsExist) {
-            qbsDebug() << "No project file given; using the one from the build graph.";
-            return;
-        }
-        qbsDebug() << "No project file given; looking in current directory.";
-        projectFilePath = QDir::currentPath();
-    }
-
-    const QFileInfo projectFileInfo(projectFilePath);
-    if (!projectFileInfo.exists())
-        throw ErrorInfo(Tr::tr("Project file '%1' cannot be found.").arg(projectFilePath));
-    if (projectFileInfo.isRelative())
-        projectFilePath = projectFileInfo.absoluteFilePath();
-    if (projectFileInfo.isFile())
-        return;
-    if (!projectFileInfo.isDir())
-        throw ErrorInfo(Tr::tr("Project file '%1' has invalid type.").arg(projectFilePath));
-
-    const QStringList namePatterns = QStringList()
-            << QLatin1String("*.qbs");
-
-    const QStringList &actualFileNames
-            = QDir(projectFilePath).entryList(namePatterns, QDir::Files);
-    if (actualFileNames.isEmpty()) {
-        QString error;
-        if (optionPool.fileOption()->projectFilePath().isEmpty())
-            error = Tr::tr("No project file given and none found in current directory.\n");
-        else
-            error = Tr::tr("No project file found in directory '%1'.").arg(projectFilePath);
-        throw ErrorInfo(error);
-    }
-    if (actualFileNames.count() > 1) {
-        throw ErrorInfo(Tr::tr("More than one project file found in directory '%1'.")
-                .arg(projectFilePath));
-    }
-    projectFilePath.append(QLatin1Char('/')).append(actualFileNames.first());
-
-    projectFilePath = QDir::current().filePath(projectFilePath);
-    projectFilePath = QDir::cleanPath(projectFilePath);
-
-    qbsDebug() << "Using project file '" << QDir::toNativeSeparators(projectFilePath) << "'.";
 }
 
 void CommandLineParser::CommandLineParserPrivate::setupBuildDirectory()

src/lib/corelib/buildgraph/buildgraphloader.cpp
@@ -166,12 +166,18 @@ void BuildGraphLoader::loadBuildGraphFromDisk()
     m_logger.qbsDebug() << "[BG] trying to load: " << buildGraphFilePath;
     try {
         pool.load(buildGraphFilePath);
-    } catch (const ErrorInfo &loadError) {
-        if (m_parameters.restoreBehavior() == SetupProjectParameters::RestoreOnly
-                || m_parameters.projectFilePath().isEmpty()) {
+    } catch (const NoBuildGraphError &e) {
+        if (m_parameters.restoreBehavior() == SetupProjectParameters::RestoreOnly)
             throw;
+        m_logger.qbsInfo() << e.toString();
+        return;
+    } catch (const ErrorInfo &loadError) {
+        if (!m_parameters.overrideBuildGraphData()) {
+            ErrorInfo fullError = loadError;
+            fullError.append(Tr::tr("Use the 'resolve' command to set up a new build graph."));
+            throw fullError;
         }
-        m_logger.qbsInfo() << loadError.toString();
+        m_logger.qbsWarning() << loadError.toString();
         return;
     }
 
@@ -198,6 +204,8 @@ bool BuildGraphLoader::checkBuildGraphCompatibility(const TopLevelProjectConstPt
 {
     if (m_parameters.projectFilePath().isEmpty())
         m_parameters.setProjectFilePath(project->location.filePath());
+    else
+        Loader::setupProjectFilePath(m_parameters);
     if (QFileInfo(project->location.filePath()) == QFileInfo(m_parameters.projectFilePath()))
         return true;
     QString message = Tr::tr("Stored build graph at '%1' is for project file '%2', but "

src/lib/corelib/language/loader.cpp
@@ -105,9 +105,8 @@ void Loader::setStoredProfiles(const QVariantMap &profiles)
 
 TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &_parameters)
 {
-    QBS_CHECK(QFileInfo(_parameters.projectFilePath()).isAbsolute());
-
     SetupProjectParameters parameters = _parameters;
+
     if (parameters.topLevelProfile().isEmpty()) {
         Settings settings(parameters.settingsDirectory());
         QString profileName = settings.defaultProfile();
@@ -119,6 +118,12 @@ TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &_parameters
         parameters.setTopLevelProfile(profileName);
         parameters.expandBuildConfiguration();
     }
+
+    setupProjectFilePath(parameters);
+    QBS_CHECK(QFileInfo(parameters.projectFilePath()).isAbsolute());
+    m_logger.qbsDebug() << "Using project file '"
+                        << QDir::toNativeSeparators(parameters.projectFilePath()) << "'.";
+
     m_engine->setEnvironment(parameters.adjustedEnvironment());
     m_engine->clearExceptions();
     m_engine->clearImportsCache();
@@ -164,6 +169,46 @@ TopLevelProjectPtr Loader::loadProject(const SetupProjectParameters &_parameters
         m_progressObserver->setFinished();
 
     return project;
+}
+
+void Loader::setupProjectFilePath(SetupProjectParameters &parameters)
+{
+    QString projectFilePath = parameters.projectFilePath();
+    if (projectFilePath.isEmpty())
+        projectFilePath = QDir::currentPath();
+    const QFileInfo projectFileInfo(projectFilePath);
+    if (!projectFileInfo.exists())
+        throw ErrorInfo(Tr::tr("Project file '%1' cannot be found.").arg(projectFilePath));
+    if (projectFileInfo.isRelative())
+        projectFilePath = projectFileInfo.absoluteFilePath();
+    if (projectFileInfo.isFile()) {
+        parameters.setProjectFilePath(projectFilePath);
+        return;
+    }
+    if (!projectFileInfo.isDir())
+        throw ErrorInfo(Tr::tr("Project file '%1' has invalid type.").arg(projectFilePath));
+
+    const QStringList namePatterns = QStringList()
+            << QLatin1String("*.qbs");
+    const QStringList &actualFileNames
+            = QDir(projectFilePath).entryList(namePatterns, QDir::Files);
+    if (actualFileNames.isEmpty()) {
+        QString error;
+        if (parameters.projectFilePath().isEmpty())
+            error = Tr::tr("No project file given and none found in current directory.\n");
+        else
+            error = Tr::tr("No project file found in directory '%1'.").arg(projectFilePath);
+        throw ErrorInfo(error);
+    }
+    if (actualFileNames.count() > 1) {
+        throw ErrorInfo(Tr::tr("More than one project file found in directory '%1'.")
+                .arg(projectFilePath));
+    }
+    projectFilePath.append(QLatin1Char('/')).append(actualFileNames.first());
+
+    projectFilePath = QDir::current().filePath(projectFilePath);
+    projectFilePath = QDir::cleanPath(projectFilePath);
+    parameters.setProjectFilePath(projectFilePath);
 }
 
 } // namespace Internal

src/lib/corelib/language/loader.h
@@ -64,6 +64,8 @@ public:
     void setStoredProfiles(const QVariantMap &profiles);
     TopLevelProjectPtr loadProject(const SetupProjectParameters &parameters);
 
+    static void setupProjectFilePath(SetupProjectParameters &parameters);
+
 private:
     Logger m_logger;
     ProgressObserver *m_progressObserver;

src/lib/corelib/tools/persistence.cpp
@@ -52,6 +52,11 @@ namespace Internal {
 
 static const char QBS_PERSISTENCE_MAGIC[] = "QBSPERSISTENCE-103";
 
+NoBuildGraphError::NoBuildGraphError()
+    : ErrorInfo(Tr::tr("No build graph exists yet for this configuration."))
+{
+}
+
 PersistentPool::PersistentPool(Logger &logger) : m_logger(logger)
 {
     Q_UNUSED(m_logger);
@@ -67,7 +72,7 @@ void PersistentPool::load(const QString &filePath)
 {
     QScopedPointer<QFile> file(new QFile(filePath));
     if (!file->exists())
-        throw ErrorInfo(Tr::tr("No build graph exists yet for this configuration."));
+        throw NoBuildGraphError();
     if (!file->open(QFile::ReadOnly)) {
         throw ErrorInfo(Tr::tr("Could not open open build graph file '%1': %2")
                     .arg(filePath, file->errorString()));
@@ -77,8 +82,6 @@ void PersistentPool::load(const QString &filePath)
     QByteArray magic;
     m_stream >> magic;
     if (magic != QBS_PERSISTENCE_MAGIC) {
-        file->close();
-        file->remove();
         m_stream.setDevice(0);
         throw ErrorInfo(Tr::tr("Cannot use stored build graph at '%1': Incompatible file format. "
                            "Expected magic token '%2', got '%3'.")

src/lib/corelib/tools/persistence.h
@@ -40,6 +40,7 @@
 #ifndef QBS_PERSISTENCE
 #define QBS_PERSISTENCE
 
+#include "error.h"
 #include "persistentobject.h"
 #include <logging/logger.h>
 
@@ -55,6 +56,12 @@
 
 namespace qbs {
 namespace Internal {
+
+class NoBuildGraphError : public ErrorInfo
+{
+public:
+    NoBuildGraphError();
+};
 
 class PersistentPool
 {

tests/auto/blackbox/testdata/build-graph-versions/build-graph-versions.qbs
@@ -1,0 +1,5 @@
+import qbs
+
+CppApplication {
+    files: ["main.cpp"]
+}

tests/auto/blackbox/testdata/build-graph-versions/main.cpp
@@ -1,0 +1,1 @@
+int main() { }

tests/auto/blackbox/tst_blackbox.cpp
@@ -485,6 +485,36 @@ void TestBlackbox::buildEnvChange()
     QVERIFY(runQbs(params) != 0);
 }
 
+void TestBlackbox::buildGraphVersions()
+{
+    QDir::setCurrent(testDataDir + "/build-graph-versions");
+    QCOMPARE(runQbs(), 0);
+    QVERIFY2(m_qbsStdout.contains("compiling main.cpp"), m_qbsStdout.constData());
+    QFile bgFile(relativeBuildGraphFilePath());
+    QVERIFY2(bgFile.open(QIODevice::ReadWrite), qPrintable(bgFile.errorString()));
+    bgFile.write("blubb");
+    bgFile.close();
+
+    // The first attempt at simple rebuilding as well as subsequent ones must fail.
+    QbsRunParameters params;
+    params.expectFailure = true;
+    QVERIFY(runQbs(params) != 0);
+    QVERIFY2(m_qbsStderr.contains("Cannot use stored build graph"), m_qbsStderr.constData());
+    QVERIFY2(m_qbsStderr.contains("Use the 'resolve' command"), m_qbsStderr.constData());
+    QVERIFY(runQbs(params) != 0);
+    QVERIFY2(m_qbsStderr.contains("Cannot use stored build graph"), m_qbsStderr.constData());
+    QVERIFY2(m_qbsStderr.contains("Use the 'resolve' command"), m_qbsStderr.constData());
+
+    // On re-resolving, the error turns into a warning and a new build graph is created.
+    QCOMPARE(runQbs(QbsRunParameters("resolve")), 0);
+    QVERIFY2(m_qbsStderr.contains("Cannot use stored build graph"), m_qbsStderr.constData());
+    QVERIFY2(!m_qbsStderr.contains("Use the 'resolve' command"), m_qbsStderr.constData());
+
+    QCOMPARE(runQbs(), 0);
+    QVERIFY2(!m_qbsStderr.contains("Cannot use stored build graph"), m_qbsStderr.constData());
+    QVERIFY2(m_qbsStdout.contains("compiling main.cpp"), m_qbsStdout.constData());
+}
+
 void TestBlackbox::changedFiles_data()
 {
     QTest::addColumn<bool>("useChangedFilesForInitialBuild");

tests/auto/blackbox/tst_blackbox.h
@@ -47,6 +47,7 @@ private slots:
     void badInterpreter();
     void buildDirectories();
     void buildEnvChange();
+    void buildGraphVersions();
     void changedFiles_data();
     void changedFiles();
     void changeInDisabledProduct();

tests/auto/cmdlineparser/tst_cmdlineparser.cpp
@@ -165,25 +165,6 @@ private slots:
         QVERIFY(!parser.parseCommandLine(QStringList() << fileArgs << "-123")); // Unknown numeric argument.
     }
 
-    void testProjectFileLookup()
-    {
-        const QString srcDir = QLatin1String(SRCDIR);
-        const QString noProjectsDir = srcDir + QLatin1String("/data/dirwithnoprojects");
-        const QString oneProjectDir = srcDir + QLatin1String("/data/dirwithoneproject");
-        const QString multiProjectsDir = srcDir + QLatin1String("/data/dirwithmultipleprojects");
-        QVERIFY(QDir(noProjectsDir).exists() && QDir(oneProjectDir).exists()
-                && QDir(multiProjectsDir).exists());
-        CommandLineParser parser;
-        const QStringList args(QLatin1String("-f"));
-        QString projectFilePath = multiProjectsDir + QLatin1String("/project.qbs");
-        QVERIFY(parser.parseCommandLine(args + QStringList(projectFilePath)));
-        QCOMPARE(projectFilePath, parser.projectFilePath());
-        projectFilePath = oneProjectDir + QLatin1String("/project.qbs");
-        QVERIFY(parser.parseCommandLine(args + QStringList(oneProjectDir)));
-        QCOMPARE(projectFilePath, parser.projectFilePath());
-        QVERIFY(!parser.parseCommandLine(args + QStringList(noProjectsDir)));
-        QVERIFY(!parser.parseCommandLine(args + QStringList(multiProjectsDir)));
-    }
 };
 
 QTEST_MAIN(TestCmdLineParser)

tests/auto/language/tst_language.cpp
@@ -1797,6 +1797,42 @@ void TestLanguage::profileValuesAndOverriddenValues()
     QCOMPARE(exceptionCaught, false);
 }
 
+void TestLanguage::projectFileLookup()
+{
+    QFETCH(QString, projectFileInput);
+    QFETCH(QString, projectFileOutput);
+    QFETCH(bool, failureExpected);
+
+    try {
+        SetupProjectParameters params;
+        params.setProjectFilePath(projectFileInput);
+        Loader::setupProjectFilePath(params);
+        QVERIFY(!failureExpected);
+        QCOMPARE(params.projectFilePath(), projectFileOutput);
+    } catch (const ErrorInfo &) {
+        QVERIFY(failureExpected);
+    }
+}
+
+void TestLanguage::projectFileLookup_data()
+{
+    QTest::addColumn<QString>("projectFileInput");
+    QTest::addColumn<QString>("projectFileOutput");
+    QTest::addColumn<bool>("failureExpected");
+
+    const QString baseDir = QLatin1String(SRCDIR) + "/testdata";
+    const QString multiProjectsDir = baseDir + "/dirwithmultipleprojects";
+    const QString noProjectsDir = baseDir + "/dirwithnoprojects";
+    const QString oneProjectDir = baseDir + "/dirwithoneproject";
+    QVERIFY(QDir(noProjectsDir).exists() && QDir(oneProjectDir).exists()
+            && QDir(multiProjectsDir).exists());
+    const QString fullFilePath = multiProjectsDir + "/project.qbs";
+    QTest::newRow("full file path") << fullFilePath << fullFilePath << false;
+    QTest::newRow("base dir ") << oneProjectDir << (oneProjectDir + "/project.qbs") << false;
+    QTest::newRow("empty dir") << noProjectsDir << QString() << true;
+    QTest::newRow("ambiguous dir") << multiProjectsDir << QString() << true;
+}
+
 void TestLanguage::productConditions()
 {
     bool exceptionCaught = false;

tests/auto/language/tst_language.h
@@ -126,6 +126,8 @@ private slots:
     void productConditions();
     void productDirectories();
     void profileValuesAndOverriddenValues();
+    void projectFileLookup();
+    void projectFileLookup_data();
     void propertiesBlocks_data();
     void propertiesBlocks();
     void propertiesBlockInGroup();