qbs/qbs

commit f489dde33403e432286bddced04d2077aced02e0

Export equality operator for qbs::PropertyMap
This was forgotten when the operator was introduced.

Change-Id: Ib1a869e0b42f61bcb845077124c89478a0064783
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/lib/corelib/api/projectdata.h
@@ -63,8 +63,8 @@ class PropertyMapPrivate;
 
 class PropertyMap;
 
-bool operator==(const PropertyMap &pm1, const PropertyMap &pm2);
-bool operator!=(const PropertyMap &pm1, const PropertyMap &pm2);
+QBS_EXPORT bool operator==(const PropertyMap &pm1, const PropertyMap &pm2);
+QBS_EXPORT bool operator!=(const PropertyMap &pm1, const PropertyMap &pm2);
 
 class QBS_EXPORT PropertyMap
 {

commit b7e413bfb18e81db1eb49d4584a81855e81a1daf

MSVC: Fix 'inconsistent dll linkage' warnings
Fixes warnings introduced in f489dde33403e43228:

api/projectdata.h(72): warning C4273: 'qbs::operator ==': inconsistent dll linkage
api/projectdata.h(66): note: see previous definition of '=='
api/projectdata.h(73): warning C4273: 'qbs::operator !=': inconsistent dll linkage
api/projectdata.h(67): note: see previous definition of '!='

Change-Id: Iafcb75dc29db1fa5100781ec0a802936c874c621
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/lib/corelib/api/projectdata.h
@@ -69,8 +69,8 @@ QBS_EXPORT bool operator!=(const PropertyMap &pm1, const PropertyMap &pm2);
 class QBS_EXPORT PropertyMap
 {
     friend class Internal::ProjectPrivate;
-    friend bool operator==(const PropertyMap &, const PropertyMap &);
-    friend bool operator!=(const PropertyMap &, const PropertyMap &);
+    friend QBS_EXPORT bool operator==(const PropertyMap &, const PropertyMap &);
+    friend QBS_EXPORT bool operator!=(const PropertyMap &, const PropertyMap &);
 
 public:
     PropertyMap();