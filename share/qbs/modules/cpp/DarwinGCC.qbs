qbs/qbs

commit a7247ff1cde96864243eba3bf5d36d6b5bded2d6

Follow the "undefined means default" methodology for Apple OS targets.
This avoids the need for several special cases. The resulting behavior
is the same since the tools happen to default to the SDK version if no
deployment target is specified.

Change-Id: I7396b39c5bdbe65be5c82f9061d58a43e9444765
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

share/qbs/modules/cpp/DarwinGCC.qbs
@@ -103,12 +103,16 @@ UnixGCC {
             "LANG": "en_US.US-ASCII",
             "PRODUCT_NAME": product.name
         }
-        if (qbs.targetOS.contains("ios") && minimumIosVersion)
-            env["IPHONEOS_DEPLOYMENT_TARGET"] = minimumIosVersion;
-        if (qbs.targetOS.contains("osx") && minimumOsxVersion)
-            env["MACOSX_DEPLOYMENT_TARGET"] = minimumOsxVersion;
-        if (qbs.targetOS.contains("watchos") && minimumWatchosVersion)
-            env["WATCHOS_DEPLOYMENT_TARGET"] = minimumWatchosVersion;
+
+        // Set the corresponding environment variable even if the minimum OS version is undefined,
+        // because this indicates the default deployment target for that OS
+        if (qbs.targetOS.contains("ios"))
+            env["IPHONEOS_DEPLOYMENT_TARGET"] = minimumIosVersion || "";
+        if (qbs.targetOS.contains("osx"))
+            env["MACOSX_DEPLOYMENT_TARGET"] = minimumOsxVersion || "";
+        if (qbs.targetOS.contains("watchos"))
+            env["WATCHOS_DEPLOYMENT_TARGET"] = minimumWatchosVersion || "";
+
         if (xcode.present)
             env["TARGETED_DEVICE_FAMILY"] = DarwinTools.targetedDeviceFamily(xcode.targetDevices);
         return env;

share/qbs/modules/cpp/ios-gcc.qbs
@@ -41,9 +41,6 @@ DarwinGCC {
 
     targetSystem: "ios" + (minimumIosVersion || "")
 
-    // Setting a minimum is especially important for Simulator or CC/LD thinks the target is OS X
-    minimumIosVersion: xcode.sdkVersion || (cxxStandardLibrary === "libc++" ? "5.0" : undefined)
-
     platformObjcFlags: base.concat(simulatorObjcFlags)
     platformObjcxxFlags: base.concat(simulatorObjcFlags)
 

share/qbs/modules/cpp/osx-gcc.qbs
@@ -37,6 +37,4 @@ DarwinGCC {
                qbs.toolchain && qbs.toolchain.contains('gcc')
 
     targetSystem: "macosx" + (minimumOsxVersion || "")
-
-    minimumOsxVersion: xcode.sdkVersion || (cxxStandardLibrary === "libc++" ? "10.7" : undefined)
 }

share/qbs/modules/cpp/watchos-gcc.qbs
@@ -37,6 +37,4 @@ DarwinGCC {
                qbs.toolchain && qbs.toolchain.contains('gcc')
 
     targetSystem: "watchos" + (minimumWatchosVersion || "")
-
-    minimumWatchosVersion: xcode.sdkVersion || (cxxStandardLibrary === "libc++" ? "2.0" : undefined)
 }

commit adc28c434853cf0fa17e95cc207e5a1c6eb761de

Don't set the *_DEPLOYMENT_TARGET environment variables to empty string
This is a partial revert of a7247ff1cde96864243eba3bf5d36d6b5bded2d6,
which caused Qbs to set the *_DEPLOYMENT_TARGET environment variables to
the empty string if no value was provided for the deployment target.

The empty string causes problems for some tools like `strip`, which
passes the empty string verbatim to the -*_version_min flag of an
internal invocation of `ld`, which is not allowed.

Presumably the original intent was to influence the target platform that
the compiler would use, but this does not work (at least with current
toolchains) if the value is the empty string. It also shouldn't matter
since we use -target now, which explicitly states the platform. In that
case, the compiler will choose a minimum version internally if one is
not explicitly given.

Change-Id: I7d9f12bb52bf3289c0c5e3353a928c8cb0be9531
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qbs/modules/cpp/DarwinGCC.qbs
@@ -182,14 +182,14 @@ UnixGCC {
 
         // Set the corresponding environment variable even if the minimum OS version is undefined,
         // because this indicates the default deployment target for that OS
-        if (qbs.targetOS.contains("ios"))
-            env["IPHONEOS_DEPLOYMENT_TARGET"] = minimumIosVersion || "";
-        if (qbs.targetOS.contains("macos"))
-            env["MACOSX_DEPLOYMENT_TARGET"] = minimumMacosVersion || "";
-        if (qbs.targetOS.contains("watchos"))
-            env["WATCHOS_DEPLOYMENT_TARGET"] = minimumWatchosVersion || "";
-        if (qbs.targetOS.contains("tvos"))
-            env["TVOS_DEPLOYMENT_TARGET"] = minimumTvosVersion || "";
+        if (qbs.targetOS.contains("ios") && minimumIosVersion)
+            env["IPHONEOS_DEPLOYMENT_TARGET"] = minimumIosVersion;
+        if (qbs.targetOS.contains("macos") && minimumMacosVersion)
+            env["MACOSX_DEPLOYMENT_TARGET"] = minimumMacosVersion;
+        if (qbs.targetOS.contains("watchos") && minimumWatchosVersion)
+            env["WATCHOS_DEPLOYMENT_TARGET"] = minimumWatchosVersion;
+        if (qbs.targetOS.contains("tvos") && minimumTvosVersion)
+            env["TVOS_DEPLOYMENT_TARGET"] = minimumTvosVersion;
 
         if (xcode.present)
             env["TARGETED_DEVICE_FAMILY"] = DarwinTools.targetedDeviceFamily(xcode.targetDevices);