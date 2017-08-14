b25a9153de8995b733248ab401e6a951f86e4210
Jake Petroules <jake.petroules@qt.io>
2017-08-14 16:04:29 -0700

    Simplify qbs.hostOS
    We remove several values that are obsolete, as well as values for
    operating systems that have been discontinued.
    
    bsd4 and qnx6 are removed because there is a separate qbs.hostOSVersion
    property for the version of the host OS. These were removed from Qt
    Creator in 2014.
    
    osx is removed as it is a long-deprecated synonym for macos. 5 minor
    releases since the introduction of 'macos' in Qbs 1.5.2 is long enough.
    
    android, ios, lynx, nacl, qnx, integrity, and vxworks are removed
    because it is either not possible or not practical to run Qbs on these
    platforms. Command line tools can't be run on iOS or Android without
    jailbreaking or rooting the device, QNX has long since removed the
    QNX-native QCC compilers, and it seems unlikely that there have ever
    been or would be compilers available for LynxOS, INTEGRITY or VxWorks
    so those are easily out. NaCl is a deprecated technology that is being
    replaced by WebAssembly (nor would you run a build process on either of
    them anyways).
    
    ultrix and reliant have not seen a release since 1995. dgux not since
    2001. bsdi not since 2003. irix not since 2006. osf not since 2010.
    dynix... unclear, but no later than 2002. symbian needs no mention.
    All considered obsolete, all gone.
    
    sco and unixware are effectively obsolete. Remove them until someone
    expresses a real need.
    
    haiku was added because Qt has it.
    
    Change-Id: I5cc23ae165a3069c63ee13388655d0093e5ccabc
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/lib/corelib/language/moduleloader.cpp
+++ src/lib/corelib/language/moduleloader.cpp
@@ -2593,122 +2593,44 @@ Item::Module ModuleLoader::loadBaseModule(ProductContext *productContext, Item *
     return baseModuleDesc;
 }
 
-static QStringList hostOS()
+static std::vector<std::string> hostOS()
 {
-    QStringList hostSystem;
-
-#if defined(Q_OS_AIX)
-    hostSystem << QLatin1String("aix");
+#if defined(__APPLE__)
+    return {"macos", "darwin", "bsd", "unix"};
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    return {"windows"};
+#elif defined(_AIX)
+    return {"aix", "unix"};
+#elif defined(hpux) || defined(__hpux)
+    return {"hpux", "unix"};
+#elif defined(__sun) || defined(sun)
+    return {"solaris", "unix"};
+#elif defined(__linux__) || defined(__linux)
+    return {"linux", "unix"};
+#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__)
+    return {"freebsd", "bsd", "unix"};
+#elif defined(__NetBSD__)
+    return {"netbsd", "bsd", "unix"};
+#elif defined(__OpenBSD__)
+    return {"openbsd", "bsd", "unix"};
+#elif defined(__GNU__)
+    return {"hurd", "unix"};
+#elif defined(__HAIKU__)
+    return {"haiku"};
+#else
+    #warning "Qbs has not been ported to this OS - see http://qbs.io/"
+    return {};
 #endif
-#if defined(Q_OS_ANDROID)
-    hostSystem << QLatin1String("android");
-#endif
-#if defined(Q_OS_BSD4)
-    hostSystem << QLatin1String("bsd") << QLatin1String("bsd4");
-#endif
-#if defined(Q_OS_BSDI)
-    hostSystem << QLatin1String("bsdi");
-#endif
-#if defined(Q_OS_CYGWIN)
-    hostSystem << QLatin1String("cygwin");
-#endif
-#if defined(Q_OS_DARWIN)
-    hostSystem << QLatin1String("darwin");
-#endif
-#if defined(Q_OS_DGUX)
-    hostSystem << QLatin1String("dgux");
-#endif
-#if defined(Q_OS_DYNIX)
-    hostSystem << QLatin1String("dynix");
-#endif
-#if defined(Q_OS_FREEBSD)
-    hostSystem << QLatin1String("freebsd");
-#endif
-#if defined(Q_OS_HPUX)
-    hostSystem << QLatin1String("hpux");
-#endif
-#if defined(Q_OS_HURD)
-    hostSystem << QLatin1String("hurd");
-#endif
-#if defined(Q_OS_INTEGRITY)
-    hostSystem << QLatin1String("integrity");
-#endif
-#if defined(Q_OS_IOS)
-    hostSystem << QLatin1String("ios");
-#endif
-#if defined(Q_OS_IRIX)
-    hostSystem << QLatin1String("irix");
-#endif
-#if defined(Q_OS_LINUX)
-    hostSystem << QLatin1String("linux");
-#endif
-#if defined(Q_OS_LYNX)
-    hostSystem << QLatin1String("lynx");
-#endif
-#if defined(Q_OS_MACOS) || defined(Q_OS_OSX)
-    hostSystem << QLatin1String("macos") << QLatin1String("osx");
-#endif
-#if defined(Q_OS_MSDOS)
-    hostSystem << QLatin1String("msdos");
-#endif
-#if defined(Q_OS_NACL)
-    hostSystem << QLatin1String("nacl");
-#endif
-#if defined(Q_OS_NETBSD)
-    hostSystem << QLatin1String("netbsd");
-#endif
-#if defined(Q_OS_OPENBSD)
-    hostSystem << QLatin1String("openbsd");
-#endif
-#if defined(Q_OS_OS2)
-    hostSystem << QLatin1String("os2");
-#endif
-#if defined(Q_OS_OS2EMX)
-    hostSystem << QLatin1String("os2emx");
-#endif
-#if defined(Q_OS_OSF)
-    hostSystem << QLatin1String("osf");
-#endif
-#if defined(Q_OS_QNX)
-    hostSystem << QLatin1String("qnx");
-#endif
-#if defined(Q_OS_ONX6)
-    hostSystem << QLatin1String("qnx6");
-#endif
-#if defined(Q_OS_RELIANT)
-    hostSystem << QLatin1String("reliant");
-#endif
-#if defined(Q_OS_SCO)
-    hostSystem << QLatin1String("sco");
-#endif
-#if defined(Q_OS_SOLARIS)
-    hostSystem << QLatin1String("solaris");
-#endif
-#if defined(Q_OS_SYMBIAN)
-    hostSystem << QLatin1String("symbian");
-#endif
-#if defined(Q_OS_ULTRIX)
-    hostSystem << QLatin1String("ultrix");
-#endif
-#if defined(Q_OS_UNIX)
-    hostSystem << QLatin1String("unix");
-#endif
-#if defined(Q_OS_UNIXWARE)
-    hostSystem << QLatin1String("unixware");
-#endif
-#if defined(Q_OS_VXWORKS)
-    hostSystem << QLatin1String("vxworks");
-#endif
-#if defined(Q_OS_WIN)
-    hostSystem << QLatin1String("windows");
-#endif
-
-    return hostSystem;
 }
 
 void ModuleLoader::setupBaseModulePrototype(Item *prototype)
 {
-    prototype->setProperty(QLatin1String("hostOS"), VariantValue::create(hostOS()));
+    prototype->setProperty(QLatin1String("hostOS"), [] {
+        QStringList list;
+        for (const auto &s : hostOS())
+            list.push_back(QString::fromStdString(s));
+        return VariantValue::create(list);
+    }());
     prototype->setProperty(QLatin1String("libexecPath"),
                            VariantValue::create(m_parameters.libexecPath()));
 
