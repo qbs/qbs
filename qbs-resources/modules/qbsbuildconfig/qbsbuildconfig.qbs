qbs/qbs

commit 5a1bb1db8e10d3964d72d7a15855bd8796d97ec7

Rewrite rpath handling for binaries that are a part of Qbs itself
Exporting a list of rpaths from a target is completely bogus as-is,
because a target cannot know a dependee's install location relative to
itself. This patch instead has each target category (executables in bin,
libraries in lib, libexec binaries in libexec/qbs, and plugins in
lib/qbs/plugins) set its relative installation directory at the product
level, and then an appropriate rpath is created for the target by
calculating a relative path from the target to the lib directory in the
context of an installed build. This also has the advantage that rpaths
are now calculated dynamically at build time rather than hardcoded, so
if a user changes the directory depth of pluginsInstallDir, for example,
everything will be adjusted automatically. Each target also therefore
now has an rpath pointing exactly to its immediate dependencies, instead
of relying on the parent process to inherit the rpath entries. Among
simply being correct, this also has the advantage that any target can
now be loaded correctly by another process which itself sets no rpath
happening to point to the qbs lib directory.

Change-Id: I8fdf24b74ef60426cea0930b7941479919226fd2
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

qbs-resources/modules/qbsbuildconfig/qbsbuildconfig.qbs
@@ -19,16 +19,35 @@ Module {
     property string relativeLibexecPath: "../" + libexecInstallDir
     property string relativePluginsPath: "../" + libDirName
     property string relativeSearchPath: ".."
+    property string rpathOrigin: {
+        // qbs < 1.11 compatibility for cpp.rpathOrigin
+        if (qbs.targetOS.contains("darwin"))
+            return "@loader_path";
+        if (qbs.targetOS.contains("unix"))
+            return "$ORIGIN";
+    }
     property stringList libRPaths: {
-        if (!enableRPath)
-            return undefined;
-        if (qbs.targetOS.contains("linux"))
-            return ["$ORIGIN/../" + libDirName];
-        if (qbs.targetOS.contains("macos"))
-            return ["@loader_path/../" + libDirName]
+        if (enableRPath && rpathOrigin && product.targetInstallDir) {
+            if (!FileInfo.cleanPath) {
+                // qbs < 1.10 compatibility
+                FileInfo.cleanPath = function (a) {
+                    if (a.endsWith("/."))
+                        return a.slice(0, -2);
+                    if (a.endsWith("/"))
+                        return a.slice(0, -1);
+                    return a;
+                }
+            }
+            return [FileInfo.cleanPath(FileInfo.joinPaths(rpathOrigin, FileInfo.relativePath(
+                                              FileInfo.resolvePath(qbs.installRoot,
+                                                                   product.targetInstallDir),
+                                              FileInfo.resolvePath(qbs.installRoot,
+                                                                   libDirName))))];
+        }
+        return [];
     }
     property string resourcesInstallDir: ""
-    property string pluginsInstallDir: libDirName
+    property string pluginsInstallDir: libDirName + "/qbs/plugins"
     property string qmlTypeDescriptionsInstallDir: FileInfo.joinPaths(resourcesInstallDir,
                                                                   "share/qbs/qml-type-descriptions")
 }

qbs-resources/imports/QbsApp.qbs
@@ -17,9 +17,10 @@ QbsProduct {
         fileTagsFilter: product.type
             .concat(qbs.buildVariant === "debug" ? ["debuginfo_app"] : [])
         qbs.install: true
-        qbs.installDir: qbsbuildconfig.appInstallDir
         qbs.installSourceBase: destinationDirectory
+        qbs.installDir: targetInstallDir
     }
+    targetInstallDir: qbsbuildconfig.appInstallDir
     Group {
         name: "logging"
         prefix: FileInfo.joinPaths(product.sourceDirectory, "../shared/logging") + '/'

qbs-resources/imports/QbsLibrary.qbs
@@ -22,9 +22,10 @@ QbsProduct {
         fileTagsFilter: product.type.concat("dynamiclibrary_symlink")
             .concat(qbs.buildVariant === "debug" ? ["debuginfo_dll"] : [])
         qbs.install: install
-        qbs.installDir: qbsbuildconfig.libInstallDir
         qbs.installSourceBase: destinationDirectory
+        qbs.installDir: targetInstallDir
     }
+    targetInstallDir: qbsbuildconfig.libInstallDir
     Group {
         fileTagsFilter: ["dynamiclibrary_import"]
         qbs.install: install
@@ -42,7 +43,6 @@ QbsProduct {
         Depends { name: "Qt"; submodules: ["core"] }
         Depends { name: "qbsbuildconfig" }
 
-        cpp.rpaths: qbsbuildconfig.libRPaths
         cpp.includePaths: [product.sourceDirectory]
         cpp.defines: product.visibilityType === "static" ? ["QBS_STATIC_LIB"] : []
     }

qbs-resources/imports/QbsProduct.qbs
@@ -6,6 +6,7 @@ Product {
     Depends { name: "Qt.core"; versionAtLeast: minimumQtVersion }
     property string minimumQtVersion: "5.6.0"
     property bool install: true
+    property string targetInstallDir
     cpp.defines: {
         var res = ["QT_NO_CAST_FROM_ASCII", "QT_NO_PROCESS_COMBINED_ARGUMENT_START"];
         if (qbs.toolchain.contains("msvc"))
@@ -15,4 +16,5 @@ Product {
         return res;
     }
     cpp.enableExceptions: true
+    cpp.rpaths: qbsbuildconfig.libRPaths
 }

src/libexec/qbs_processlauncher/qbs_processlauncher.qbs
@@ -10,11 +10,6 @@ QbsProduct {
 
     Depends { name: "Qt.network" }
 
-    Properties {
-        condition: qbs.targetOS.contains("macos") && qbsbuildconfig.enableRPath
-        cpp.rpaths: ["@loader_path/../../" + qbsbuildconfig.libDirName]
-    }
-
     cpp.cxxLanguageVersion: "c++11"
     cpp.includePaths: base.concat(pathToProtocolSources)
 
@@ -40,7 +35,8 @@ QbsProduct {
         fileTagsFilter: product.type
             .concat(qbs.buildVariant === "debug" ? ["debuginfo_app"] : [])
         qbs.install: true
-        qbs.installDir: qbsbuildconfig.libexecInstallDir
         qbs.installSourceBase: destinationDirectory
+        qbs.installDir: targetInstallDir
     }
+    targetInstallDir: qbsbuildconfig.libexecInstallDir
 }

src/plugins/qbsplugin.qbs
@@ -25,9 +25,10 @@ QbsProduct {
             .concat(qbs.buildVariant === "debug"
         ? [isForDarwin ? "debuginfo_loadablemodule" : "debuginfo_dll"] : [])
         qbs.install: true
-        qbs.installDir: qbsbuildconfig.pluginsInstallDir + "/qbs/plugins"
         qbs.installSourceBase: destinationDirectory
+        qbs.installDir: targetInstallDir
     }
+    targetInstallDir: qbsbuildconfig.pluginsInstallDir
     Properties {
         condition: isForDarwin
         bundle.isBundle: false

commit 33a0c11a5fee3fb70163c055ba2d1cc64b5130e1

Fix qbs build
Was broken by 5a1bb1db8e.
We must not use FileInfo::cleanPath() on the string "$ORIGIN/../lib", as
that will simply strip away the first two components.
Also, use joinPaths() rather than resolvePath() to construct the paths
(as we always want to concatenate, even if the second argument looks
like an absolute path), and don't read qbs.installRoot unnecessarily.

Change-Id: Idf919c664a2e0ef1ea9a4052d1d0e591a52a234c
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

qbs-resources/modules/qbsbuildconfig/qbsbuildconfig.qbs
@@ -38,11 +38,9 @@ Module {
                     return a;
                 }
             }
-            return [FileInfo.cleanPath(FileInfo.joinPaths(rpathOrigin, FileInfo.relativePath(
-                                              FileInfo.resolvePath(qbs.installRoot,
-                                                                   product.targetInstallDir),
-                                              FileInfo.resolvePath(qbs.installRoot,
-                                                                   libDirName))))];
+            return [FileInfo.joinPaths(rpathOrigin, FileInfo.relativePath(
+                                           FileInfo.joinPaths('/', product.targetInstallDir),
+                                           FileInfo.joinPaths('/', libDirName)))];
         }
         return [];
     }