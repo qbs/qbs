qbs/qbs

commit aa1c500411bf81743d0588c14c777c98d4ea1157

Centralize path property resolution
We had two places where path properties were resolved. Centralize the
code in the EvaluatorScriptClass.

Change-Id: Ia8a34af912738f381fbe6016f8af1c05c3c746ed
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/lib/corelib/language/evaluatorscriptclass.cpp
@@ -429,10 +429,10 @@ void EvaluatorScriptClass::collectValuesFromNextChain(const EvaluationData *data
     }
 }
 
-static QString overriddenSourceDirectory(const Item *item)
+static QString overriddenSourceDirectory(const Item *item, const QString &defaultValue)
 {
     const VariantValuePtr v = item->variantProperty(QLatin1String("_qbs_sourceDir"));
-    return v ? v->value().toString() : QString();
+    return v ? v->value().toString() : defaultValue;
 }
 
 static void makeTypeError(const ErrorInfo &error, QScriptValue &v)
@@ -449,7 +449,8 @@ static void makeTypeError(const PropertyDeclaration &decl, const CodeLocation &l
     makeTypeError(error, v);
 }
 
-static void convertToPropertyType(const Item *item, const PropertyDeclaration& decl,
+static void convertToPropertyType(const QString &pathPropertiesBaseDir, const Item *item,
+                                  const PropertyDeclaration& decl,
                                   const Value *value, QScriptValue &v)
 {
     if (value->type() == Value::VariantValueType && v.isUndefined() && !decl.isScalar()) {
@@ -479,9 +480,10 @@ static void convertToPropertyType(const Item *item, const PropertyDeclaration& d
             makeTypeError(decl, location, v);
             break;
         }
-        const QString srcDir = overriddenSourceDirectory(item);
+        const QString srcDir = overriddenSourceDirectory(item, pathPropertiesBaseDir);
         if (!srcDir.isEmpty())
-            v = v.engine()->toScriptValue(FileInfo::resolvePath(srcDir, v.toString()));
+            v = v.engine()->toScriptValue(QDir::cleanPath(
+                                              FileInfo::resolvePath(srcDir, v.toString())));
         break;
     }
     case PropertyDeclaration::String:
@@ -489,7 +491,7 @@ static void convertToPropertyType(const Item *item, const PropertyDeclaration& d
             makeTypeError(decl, location, v);
         break;
     case PropertyDeclaration::PathList:
-        srcDir = overriddenSourceDirectory(item);
+        srcDir = overriddenSourceDirectory(item, pathPropertiesBaseDir);
         // Fall-through.
     case PropertyDeclaration::StringList:
     {
@@ -521,7 +523,8 @@ static void convertToPropertyType(const Item *item, const PropertyDeclaration& d
             }
             if (srcDir.isEmpty())
                 continue;
-            elem = v.engine()->toScriptValue(FileInfo::resolvePath(srcDir, elem.toString()));
+            elem = v.engine()->toScriptValue(
+                        QDir::cleanPath(FileInfo::resolvePath(srcDir, elem.toString())));
             v.setProperty(i, elem);
         }
         break;
@@ -614,7 +617,7 @@ QScriptValue EvaluatorScriptClass::property(const QScriptValue &object, const QS
         converter.start();
 
         const PropertyDeclaration decl = data->item->propertyDeclaration(name.toString());
-        convertToPropertyType(data->item, decl, value.get(), result);
+        convertToPropertyType(m_pathPropertiesBaseDir, data->item, decl, value.get(), result);
     }
 
     if (debugProperties)

src/lib/corelib/language/evaluator.cpp
@@ -190,6 +190,16 @@ void Evaluator::handleEvaluationError(const Item *item, const QString &name,
     throw ErrorInfo(message, CodeLocation(filePath, line, -1, false));
 }
 
+void Evaluator::setPathPropertiesBaseDir(const QString &dirPath)
+{
+    m_scriptClass->setPathPropertiesBaseDir(dirPath);
+}
+
+void Evaluator::clearPathPropertiesBaseDir()
+{
+    m_scriptClass->clearPathPropertiesBaseDir();
+}
+
 bool Evaluator::evaluateProperty(QScriptValue *result, const Item *item, const QString &name,
         bool *propertyWasSet)
 {

src/lib/corelib/language/evaluator.h
@@ -91,6 +91,10 @@ public:
 
     void handleEvaluationError(const Item *item, const QString &name,
             const QScriptValue &scriptValue);
+
+    void setPathPropertiesBaseDir(const QString &dirPath);
+    void clearPathPropertiesBaseDir();
+
 private:
     void onItemPropertyChanged(Item *item);
     bool evaluateProperty(QScriptValue *result, const Item *item, const QString &name,

src/lib/corelib/language/evaluatorscriptclass.h
@@ -77,6 +77,9 @@ public:
     PropertyDependencies propertyDependencies() const { return m_propertyDependencies; }
     void clearPropertyDependencies() { m_propertyDependencies.clear(); }
 
+    void setPathPropertiesBaseDir(const QString &dirPath) { m_pathPropertiesBaseDir = dirPath; }
+    void clearPathPropertiesBaseDir() { m_pathPropertiesBaseDir.clear(); }
+
 private:
     QueryFlags queryItemProperty(const EvaluationData *data,
                                  const QString &name,
@@ -115,6 +118,7 @@ private:
     Set<Value *> m_currentNextChain;
     PropertyDependencies m_propertyDependencies;
     std::stack<QualifiedId> m_requestedProperties;
+    QString m_pathPropertiesBaseDir;
 };
 
 } // namespace Internal

src/lib/corelib/language/projectresolver.cpp
@@ -600,6 +600,7 @@ QVariantMap ProjectResolver::resolveAdditionalModuleProperties(const Item *group
         propsPerModule[moduleName] << fullPropName.last();
     }
     EvalCacheEnabler cachingEnabler(m_evaluator);
+    m_evaluator->setPathPropertiesBaseDir(m_productContext->product->sourceDirectory);
     for (const Item::Module &module : group->modules()) {
         const QString &fullModName = module.name.toString();
         const QStringList propsForModule = propsPerModule.take(fullModName);
@@ -611,6 +612,7 @@ QVariantMap ProjectResolver::resolveAdditionalModuleProperties(const Item *group
         modulesMap.insert(fullModName,
                           evaluateProperties(module.item, module.item, reusableValues, true));
     }
+    m_evaluator->clearPathPropertiesBaseDir();
     QVariantMap newValues = currentValues;
     newValues.insert(QLatin1String("modules"), modulesMap);
     return newValues;
@@ -1263,14 +1265,12 @@ QVariantMap ProjectResolver::evaluateProperties(const Item *item, const Item *pr
             // NOTE: Loses type information if scriptValue.isUndefined == true,
             //       as such QScriptValues become invalid QVariants.
             QVariant v = scriptValue.toVariant();
-            if (pd.type() == PropertyDeclaration::Path && v.isValid())
-                v = convertPathProperty(v.toString(),
-                                        m_productContext->product->sourceDirectory);
-            else if (pd.type() == PropertyDeclaration::PathList)
-                v = convertPathListProperty(v.toStringList(),
-                                            m_productContext->product->sourceDirectory);
-            else if (pd.type() == PropertyDeclaration::StringList)
+            if (pd.type() == PropertyDeclaration::Path && v.isValid()) {
+                v = v.toString();
+            } else if (pd.type() == PropertyDeclaration::PathList
+                       || pd.type() == PropertyDeclaration::StringList) {
                 v = v.toStringList();
+            }
             result[it.key()] = v;
             break;
         }
@@ -1297,23 +1297,11 @@ QVariantMap ProjectResolver::evaluateProperties(const Item *item, const Item *pr
 QVariantMap ProjectResolver::createProductConfig()
 {
     EvalCacheEnabler cachingEnabler(m_evaluator);
+    m_evaluator->setPathPropertiesBaseDir(m_productContext->product->sourceDirectory);
     QVariantMap cfg = evaluateModuleValues(m_productContext->item);
     cfg = evaluateProperties(m_productContext->item, m_productContext->item, cfg);
+    m_evaluator->clearPathPropertiesBaseDir();
     return cfg;
-}
-
-QString ProjectResolver::convertPathProperty(const QString &path, const QString &dirPath) const
-{
-    return path.isEmpty() ? path : QDir::cleanPath(FileInfo::resolvePath(dirPath, path));
-}
-
-QStringList ProjectResolver::convertPathListProperty(const QStringList &paths,
-                                                     const QString &dirPath) const
-{
-    QStringList result;
-    for (const QString &path : paths)
-        result += convertPathProperty(path, dirPath);
-    return result;
 }
 
 void ProjectResolver::callItemFunction(const ItemFuncMap &mappings, Item *item,

src/lib/corelib/language/projectresolver.h
@@ -116,8 +116,6 @@ private:
     QVariantMap evaluateProperties(const Item *item, const Item *propertiesContainer,
                                    const QVariantMap &tmplt, bool lookupPrototype = true);
     QVariantMap createProductConfig();
-    QString convertPathProperty(const QString &path, const QString &dirPath) const;
-    QStringList convertPathListProperty(const QStringList &paths, const QString &dirPath) const;
     ProjectContext createProjectContext(ProjectContext *parentProjectContext) const;
 
     struct ProductDependencyInfo

commit 73f5d22f936570cfb741d5b00d48cf7540c8f816

Fix regression resolving exported paths defined in the exporting product
Commit aa1c500411 inadvertently changed the behavior for cases like
this:
DynamicLibrary {
    property path inclPath: "."
    Export { cpp.includePaths: [product.inclPath] }
}
Before that patch, the base dir for resolving the include path was the
exporting product, afterwards it was the importing one.
This patch restores the original behavior, which is the intended one.

Change-Id: I8a56cff1163e7fc87361b5ab5e9cbd527b53b79b
Reviewed-by: Jake Petroules <jake.petroules@qt.io>

src/lib/corelib/language/evaluatorscriptclass.cpp
@@ -480,6 +480,12 @@ static void convertToPropertyType(const QString &pathPropertiesBaseDir, const It
     if (v.isUndefined() || v.isError())
         return;
     QString srcDir;
+    QString actualBaseDir;
+    if (!pathPropertiesBaseDir.isEmpty()) {
+        const VariantValueConstPtr itemSourceDir
+                = item->variantProperty(QLatin1String("sourceDirectory"));
+        actualBaseDir = itemSourceDir ? itemSourceDir->value().toString() : pathPropertiesBaseDir;
+    }
     const CodeLocation &location = value->location();
     switch (decl.type()) {
     case PropertyDeclaration::UnknownType:
@@ -499,7 +505,7 @@ static void convertToPropertyType(const QString &pathPropertiesBaseDir, const It
             makeTypeError(decl, location, v);
             break;
         }
-        const QString srcDir = overriddenSourceDirectory(item, pathPropertiesBaseDir);
+        const QString srcDir = overriddenSourceDirectory(item, actualBaseDir);
         if (!srcDir.isEmpty())
             v = v.engine()->toScriptValue(QDir::cleanPath(
                                               FileInfo::resolvePath(srcDir, v.toString())));
@@ -510,7 +516,7 @@ static void convertToPropertyType(const QString &pathPropertiesBaseDir, const It
             makeTypeError(decl, location, v);
         break;
     case PropertyDeclaration::PathList:
-        srcDir = overriddenSourceDirectory(item, pathPropertiesBaseDir);
+        srcDir = overriddenSourceDirectory(item, actualBaseDir);
         // Fall-through.
     case PropertyDeclaration::StringList:
     {

tests/auto/language/testdata/modules/dummy/dummy.qbs
@@ -14,6 +14,7 @@ DummyBase {
     property string upperCaseProductName: productName.toUpperCase()
     property string zort: "zort in dummy"
     property pathList includePaths
+    property path somePath
     property stringList listProp: product.type.contains("blubb") ? ["123"] : ["456"]
 
     property bool controllingProp: false

tests/auto/language/testdata/subdir/exports-mylib.qbs
@@ -5,10 +5,13 @@ StaticLibrary {
     Depends { name: "dummy" }
     dummy.defines: ["BUILD_" + product.name.toUpperCase()]
     property string definePrefix: "USE_"
+    property path aPath: "."
+    dummy.somePath: aPath
     Export {
         Depends { name: "dummy" }
         Depends { name: "mylib2" }
         dummy.defines: [product.definePrefix + product.name.toUpperCase()]
         dummy.includePaths: ["./lib"]
+        dummy.somePath: product.aPath
     }
 }

tests/auto/language/tst_language.cpp
@@ -833,12 +833,16 @@ void TestLanguage::exports()
 
         QCOMPARE(product->moduleProperties->moduleProperty("dummy", "productName").toString(),
                  QString("myapp"));
+        QVERIFY(product->moduleProperties->moduleProperty("dummy", "somePath").toString()
+                .endsWith("/subdir"));
 
         product = products.value("mylib");
         QVERIFY(!!product);
         propertyName = QStringList() << "modules" << "dummy" << "defines";
         propertyValue = product->moduleProperties->property(propertyName);
         QCOMPARE(propertyValue.toStringList(), QStringList() << "BUILD_MYLIB");
+        QVERIFY(product->moduleProperties->moduleProperty("dummy", "somePath").toString()
+                .endsWith("/subdir"));
 
         product = products.value("mylib2");
         QVERIFY(!!product);