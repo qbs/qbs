qbs/qbs

commit 3653385af94ae7fe042d1c4845293b495bdc9854

Qt templates: Make moc rule handle C++ amalgamation correctly
In C++ amalgamation mode, the source files need to be moc'ed before
merging, because otherwise including .moc files will not work.

Task-number: QBS-1252
Change-Id: I6e4bef4b46ec7bc3cf7ce7a950b380d469cc0afd
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/lib/qtprofilesetup/templates/core.qbs
@@ -255,8 +255,11 @@ Module {
 
     Rule {
         name: "QtCoreMocRule"
-        inputs: ["objcpp", "cpp", "hpp"]
-        auxiliaryInputs: ["qt_plugin_metadata"]
+        property string cppInput: cpp.combineCxxSources ? "cpp.combine" : "cpp"
+        property string objcppInput: cpp.combineCxxSources ? "objcpp.combine" : "cpp"
+        property stringList additionalAuxInputs: cpp.combineCxxSources ? ["cpp", "objcpp"] : []
+        inputs: [objcppInput, cppInput, "hpp"]
+        auxiliaryInputs: ["qt_plugin_metadata"].concat(additionalAuxInputs)
         excludedInputs: ["unmocable"]
         outputFileTags: ["hpp", "cpp", "moc_cpp", "unmocable"]
         outputArtifacts: {

src/lib/corelib/buildgraph/qtmocscanner.cpp
@@ -63,12 +63,14 @@ namespace Internal {
 struct CommonFileTags
 {
     const FileTag cpp = "cpp";
+    const FileTag cppCombine = "cpp.combine";
     const FileTag hpp = "hpp";
     const FileTag moc_cpp = "moc_cpp";
     const FileTag moc_cpp_plugin = "moc_cpp_plugin";
     const FileTag moc_hpp_plugin = "moc_hpp_plugin";
     const FileTag moc_hpp = "moc_hpp";
     const FileTag objcpp = "objcpp";
+    const FileTag objcppCombine = "objcpp.combine";
 };
 
 Q_GLOBAL_STATIC(CommonFileTags, commonFileTags)
@@ -117,9 +119,9 @@ QtMocScanner::~QtMocScanner()
 
 ScannerPlugin *QtMocScanner::scannerPluginForFileTags(const FileTags &ft)
 {
-    if (ft.contains(m_tags.objcpp))
+    if (ft.contains(m_tags.objcpp) || ft.contains(m_tags.objcppCombine))
         return m_objcppScanner;
-    if (ft.contains(m_tags.cpp))
+    if (ft.contains(m_tags.cpp) || ft.contains(m_tags.cppCombine))
         return m_cppScanner;
     return m_hppScanner;
 }
@@ -133,8 +135,16 @@ static RawScanResult runScanner(ScannerPlugin *scanner, const Artifact *artifact
     RawScanResults::ScanData &scanData = rawScanResults.findScanData(artifact, &depScanner,
                                                                      artifact->properties);
     if (scanData.lastScanTime < artifact->timestamp()) {
-        const QByteArray tagsForScanner
-                = artifact->fileTags().toStringList().join(QLatin1Char(',')).toLatin1();
+        FileTags tags = artifact->fileTags();
+        if (tags.contains(commonFileTags->cppCombine)) {
+            tags.remove(commonFileTags->cppCombine);
+            tags.insert(commonFileTags->cpp);
+        }
+        if (tags.contains(commonFileTags->objcppCombine)) {
+            tags.remove(commonFileTags->objcppCombine);
+            tags.insert(commonFileTags->objcpp);
+        }
+        const QByteArray tagsForScanner = tags.toStringList().join(QLatin1Char(',')).toLatin1();
         void *opaq = scanner->open(filepath.utf16(), tagsForScanner.constData(),
                                    ScanForDependenciesFlag | ScanForFileTagsFlag);
         if (!opaq || !scanner->additionalFileTags)

tests/auto/blackbox/testdata-qt/moc-and-cxx-combining/main.cpp
@@ -1,0 +1,6 @@
+#include "myobject.h"
+
+int main()
+{
+    useMyObject();
+}

tests/auto/blackbox/testdata-qt/moc-and-cxx-combining/moc-and-cxx-combining.qbs
@@ -1,0 +1,6 @@
+import qbs
+
+QtApplication {
+    cpp.combineCxxSources: true
+    files: ["main.cpp", "myobject.h", "myobject.cpp"]
+}

tests/auto/blackbox/testdata-qt/moc-and-cxx-combining/myobject.cpp
@@ -1,0 +1,14 @@
+#include "myobject.h"
+
+#include <QObject>
+
+class MyObject : public QObject
+{
+    Q_OBJECT
+public:
+    void use() {}
+};
+
+void useMyObject() { MyObject().use(); }
+
+#include <myobject.moc>

tests/auto/blackbox/testdata-qt/moc-and-cxx-combining/myobject.h
@@ -1,0 +1,6 @@
+#ifndef MYOBJECT_H
+#define MYOBJECT_H
+
+void useMyObject();
+
+#endif

tests/auto/blackbox/tst_blackboxqt.cpp
@@ -179,6 +179,12 @@ void TestBlackboxQt::mixedBuildVariants()
     }
 }
 
+void TestBlackboxQt::mocAndCppCombining()
+{
+    QDir::setCurrent(testDataDir + "/moc-and-cxx-combining");
+    QCOMPARE(runQbs(), 0);
+}
+
 void TestBlackboxQt::mocFlags()
 {
     QDir::setCurrent(testDataDir + "/moc-flags");

tests/auto/blackbox/tst_blackboxqt.h
@@ -50,6 +50,7 @@ private slots:
     void dbusInterfaces();
     void lrelease();
     void mixedBuildVariants();
+    void mocAndCppCombining();
     void mocFlags();
     void mocSameFileName();
     void pkgconfig();

commit 7d9f8bde75cbd6ed16c925706a8fc6bb3fa1226c

Qt templates: Fix moc rule
Commit 3653385af9 broke moc on macOS.

Change-Id: Ie0875a1fcc7b891c241ab1cbd00ce6a908cd04ec
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/lib/qtprofilesetup/templates/core.qbs
@@ -256,10 +256,16 @@ Module {
     Rule {
         name: "QtCoreMocRule"
         property string cppInput: cpp.combineCxxSources ? "cpp.combine" : "cpp"
-        property string objcppInput: cpp.combineCxxSources ? "objcpp.combine" : "cpp"
-        property stringList additionalAuxInputs: cpp.combineCxxSources ? ["cpp", "objcpp"] : []
+        property string objcppInput: cpp.combineObjcxxSources ? "objcpp.combine" : "objcpp"
         inputs: [objcppInput, cppInput, "hpp"]
-        auxiliaryInputs: ["qt_plugin_metadata"].concat(additionalAuxInputs)
+        auxiliaryInputs: {
+            var auxInputs = ["qt_plugin_metadata"];
+            if (cpp.combineCxxSources)
+                auxInputs.push("cpp");
+            if (cpp.combineObjcxxSources)
+                auxInputs.push("objcpp");
+            return auxInputs;
+        }
         excludedInputs: ["unmocable"]
         outputFileTags: ["hpp", "cpp", "moc_cpp", "unmocable"]
         outputArtifacts: {