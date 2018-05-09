b29168f50c9b57c229364fccd25f20ed9b2a01c7
Christian Kandeler <christian.kandeler@qt.io>
2018-05-09 14:20:13 +0200

    Fix FileInfo::isDir() on Windows
    The function returned true for non-existing files.
    
    Change-Id: If3c3d9d1d89b3b1b70414d1b3168bd091d4f75a8
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/lib/corelib/tools/fileinfo.cpp
+++ src/lib/corelib/tools/fileinfo.cpp
@@ -339,7 +339,7 @@ FileTime FileInfo::lastStatusChange() const
 
 bool FileInfo::isDir() const
 {
-    return z(m_stat)->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
+    return exists() && z(m_stat)->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
 }
 
 static QString resolveSymlinks(const QString &fileName)
