qbs/qbs

commit da6c0e50588f08e4650f3a335310509e6be5fecd

Benchmarker: Keep the temp data in case of a failure
So the problem can be more easily investigated.

Change-Id: Ia968a4e42164cce5b3f67d9e8a29ccd5b24e3eaf
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

tests/benchmarker/benchmarker-main.cpp
@@ -104,12 +104,12 @@ static void printResults(Activities activities, const BenchmarkResults &results,
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication app(argc, argv);
+    CommandLineParser clParser;
+    clParser.parse();
+    Benchmarker benchmarker(clParser.activies(), clParser.oldCommit(), clParser.newCommit(),
+                            clParser.testProjectFilePath(), clParser.qbsRepoDirPath());
     try {
-        QCoreApplication app(argc, argv);
-        CommandLineParser clParser;
-        clParser.parse();
-        Benchmarker benchmarker(clParser.activies(), clParser.oldCommit(), clParser.newCommit(),
-                                clParser.testProjectFilePath(), clParser.qbsRepoDirPath());
         benchmarker.benchmark();
         printResults(clParser.activies(), benchmarker.results(), clParser.regressionThreshold());
         if (hasRegression) {
@@ -118,7 +118,10 @@ int main(int argc, char *argv[])
                          "under " << qPrintable(benchmarker.rawDataBaseDir()) << '.' << std::endl;
         }
     } catch (const Exception &e) {
+        benchmarker.keepRawData();
         std::cerr << qPrintable(e.description()) << std::endl;
+        std::cerr << "Build data available under " << qPrintable(benchmarker.rawDataBaseDir())
+                  << '.' << std::endl;
         return EXIT_FAILURE;
     }
 }

commit 5e89502cd9af7175c380234472784c4d444debb5

benchmarker: Fix crash when called with no parameters
The crash was accidentally introduced in da6c0e5058.

Change-Id: If3a1ed1633fc8bb6aaf736859a89e1b7c17777c2
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

tests/benchmarker/benchmarker-main.cpp
@@ -105,8 +105,15 @@ static void printResults(Activities activities, const BenchmarkResults &results,
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+
     CommandLineParser clParser;
-    clParser.parse();
+    try {
+        clParser.parse();
+    } catch (const Exception &e) {
+        std::cerr << qPrintable(e.description()) << std::endl;
+        return EXIT_FAILURE;
+    }
+
     Benchmarker benchmarker(clParser.activies(), clParser.oldCommit(), clParser.newCommit(),
                             clParser.testProjectFilePath(), clParser.qbsRepoDirPath());
     try {