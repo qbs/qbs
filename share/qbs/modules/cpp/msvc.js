c158ed407067526ba74471c6e6bbf2b66eddecc7
Christian Kandeler <christian.kandeler@qt.io>
2018-12-03 13:28:46 +0100

    MSVC: Fix handling of intermediate objects
    Amends 2e830e5e4f.
    
    Change-Id: Ifecc822412c481d45121fa7d792e1fb2aa4e2064
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- share/qbs/modules/cpp/msvc.js
+++ share/qbs/modules/cpp/msvc.js
@@ -188,7 +188,8 @@ function prepareCompiler(project, product, inputs, outputs, input, output, expli
     if (product.cpp.debugInformation && product.cpp.separateDebugInformation)
         args.push("/Fd" + product.targetName + ".cl" + product.cpp.debugInfoSuffix);
 
-    var objOutput = outputs.obj ? outputs.obj[0] : undefined
+    var objectMap = outputs.obj || outputs.intermediate_obj
+    var objOutput = objectMap ? objectMap[0] : undefined
     args.push('/Fo' + FileInfo.toWindowsSeparators(objOutput.filePath))
     args.push(FileInfo.toWindowsSeparators(input.filePath))
 
