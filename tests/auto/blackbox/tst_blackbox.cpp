ad11bb7383bf730ca0241af24a8b4179af157c7f
Christian Kandeler <christian.kandeler@qt.io>
2019-10-02 16:03:25 +0200

    Darwin: Adapt to Xcode 11
    - xcspec files can now refer to variables in variable names, as in the
    following line:
        CONTENTS_FOLDER_PATH = "$(CONTENTS_FOLDER_PATH_SHALLOW_BUNDLE_$
    (SHALLOW_BUNDLE))
    - Some new environment variables have been introduced and need to be set
    when running our probes; e.g. "SWIFT_PLATFORM_TARGET_PREFIX".
    - The symbolLinkMode autotest made an invalid assumption about order of
    output.
    
    Change-Id: Ic845c2c4a8eafb4ece0f0bb04e6e492681a02979
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- tests/auto/blackbox/tst_blackbox.cpp
+++ tests/auto/blackbox/tst_blackbox.cpp
@@ -4262,7 +4262,8 @@ void TestBlackbox::symbolLinkMode()
         rmDirR(relativeBuildDir());
         params.arguments = QStringList() << commonArgs << "project.lazy:true";
         QCOMPARE(runQbs(params), 0);
-        QVERIFY2(m_qbsStdout.contains("meow\nLib was loaded!\n"), m_qbsStdout.constData());
+        QVERIFY2(m_qbsStdout.contains("meow\n") && m_qbsStdout.contains("Lib was loaded!\n"),
+                 m_qbsStdout.constData());
     }
 }
 
