c17a82b08d5fd5d411bcb6a03222be64779e88c0
Ivan Komissarov <ABBAPOH@gmail.com>
2019-11-24 16:09:29 +0900

    Fix 'performance-unnecessary-value-param' in Logger
    Most Logger's shift operators call .write() which "mutates" passed
    object except three operators which simply use QString overload. This
    produces clang-tidy warning about unnecessary copy. Use direct call to
    .write() to suppress the warning and have consistent function signatures
    
    Change-Id: I1c42e16948147315e40edf263e887ebeee4a4388
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/lib/corelib/logging/logger.cpp
+++ src/lib/corelib/logging/logger.cpp
@@ -183,17 +183,20 @@ LogWriter operator<<(LogWriter w, const QVariant &variant)
 
 LogWriter operator<<(LogWriter w, int n)
 {
-    return w << QString::number(n);
+    w.write(QString::number(n));
+    return w;
 }
 
 LogWriter operator<<(LogWriter w, qint64 n)
 {
-    return w << QString::number(n);
+    w.write(QString::number(n));
+    return w;
 }
 
 LogWriter operator<<(LogWriter w, bool b)
 {
-    return w << QString::fromLatin1(b ? "true" : "false");
+    w.write(QString::fromLatin1(b ? "true" : "false"));
+    return w;
 }
 
 LogWriter operator<<(LogWriter w, const MessageTag &tag)
