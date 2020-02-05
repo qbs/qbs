qbs/qbs

commit e86375117120f8c7b06afa573d9772d3c783f737

Evaluate probes before Profile items
Profile items can reference project properties, but accessing properties of
project-level probes failed. This patch ensures that probes are executed
before Profile items are evaluated.

Fixes: QBS-1490
Change-Id: I5c215b91ded51ac895e433faa2e6fcfbc3d3e212
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/lib/corelib/language/moduleloader.cpp
@@ -688,6 +688,9 @@ void ModuleLoader::handleProject(ModuleLoaderResult *loadResult,
                                                               m_qbsVersion.toString()));
     }
 
+    resolveProbes(&dummyProductContext, projectItem);
+    projectContext.topLevelProject->probes << dummyProductContext.info.probes;
+
     handleProfileItems(projectItem, &projectContext);
 
     QList<Item *> multiplexedProducts;
@@ -698,9 +701,6 @@ void ModuleLoader::handleProject(ModuleLoaderResult *loadResult,
     }
     for (Item * const additionalProductItem : qAsConst(multiplexedProducts))
         Item::addChild(projectItem, additionalProductItem);
-
-    resolveProbes(&dummyProductContext, projectItem);
-    projectContext.topLevelProject->probes << dummyProductContext.info.probes;
 
     const QList<Item *> originalChildren = projectItem->children();
     for (Item * const child : originalChildren) {

tests/auto/api/testdata/project-with-probe-and-profile-item/project-with-probe-and-profile-item.qbs
@@ -1,0 +1,19 @@
+Project {
+
+    property bool probesEvaluated: probe.found
+
+    Probe {
+        id: probe
+        configure: {
+            found = true;
+        }
+    }
+
+    Profile {
+        name: "the-profile"
+        cpp.includePaths: {
+            if (!probesEvaluated)
+                throw "project-level probes not evaluated";
+        }
+    }
+}

tests/auto/api/tst_api.cpp
@@ -2374,6 +2374,12 @@ void TestApi::projectWithPropertiesItem()
     VERIFY_NO_ERROR(errorInfo);
 }
 
+void TestApi::projectWithProbeAndProfileItem()
+{
+    const qbs::ErrorInfo errorInfo = doBuildProject("project-with-probe-and-profile-item");
+    VERIFY_NO_ERROR(errorInfo);
+}
+
 void TestApi::propertiesBlocks()
 {
     const qbs::ErrorInfo errorInfo = doBuildProject("properties-blocks");

tests/auto/api/tst_api.h
@@ -122,6 +122,7 @@ private slots:
     void projectLocking();
     void projectPropertiesByName();
     void projectWithPropertiesItem();
+    void projectWithProbeAndProfileItem();
     void propertiesBlocks();
     void rc();
     void referencedFileErrors();

commit bf059105305fd33440940338a7aeb67ec75cb18e

Fix probes referencing project properties
This patch fixes a regression introduced in e8637511. Probes in Project items
could no longer reference project properties by writing project.xxx on the
right hand side of a property assignment expression.

Change-Id: I7d04c71f4c493481a00411f12666bd6c2caa8caa
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/lib/corelib/language/moduleloader.cpp
@@ -688,6 +688,9 @@ void ModuleLoader::handleProject(ModuleLoaderResult *loadResult,
                                                               m_qbsVersion.toString()));
     }
 
+    for (Item * const child : projectItem->children())
+        child->setScope(projectContext.scope);
+
     resolveProbes(&dummyProductContext, projectItem);
     projectContext.topLevelProject->probes << dummyProductContext.info.probes;
 
@@ -695,7 +698,6 @@ void ModuleLoader::handleProject(ModuleLoaderResult *loadResult,
 
     QList<Item *> multiplexedProducts;
     for (Item * const child : projectItem->children()) {
-        child->setScope(projectContext.scope);
         if (child->type() == ItemType::Product)
             multiplexedProducts << multiplexProductItem(&dummyProductContext, child);
     }

tests/auto/blackbox/testdata/probeProperties/probeProperties.qbs
@@ -1,29 +1,40 @@
 import qbs.Probes
 
-CppApplication {
-    Probes.PathProbe {
-        id: probe1
-        names: ["bin/tool"]
-        platformSearchPaths: [product.sourceDirectory]
+Project {
+
+    CppApplication {
+        Probes.PathProbe {
+            id: probe1
+            names: ["bin/tool"]
+            platformSearchPaths: [product.sourceDirectory]
+        }
+
+        Probes.PathProbe {
+            id: probe2
+            names: ["tool"]
+            platformSearchPaths: [product.sourceDirectory + "/bin"]
+        }
+
+        targetName: {
+            console.info("probe1.fileName=" + probe1.fileName);
+            console.info("probe1.path=" + probe1.path);
+            console.info("probe1.filePath=" + probe1.filePath);
+
+            console.info("probe2.fileName=" + probe2.fileName);
+            console.info("probe2.path=" + probe2.path);
+            console.info("probe2.filePath=" + probe2.filePath);
+
+            console.info("probe3.fileName=" + probe3.fileName);
+            console.info("probe3.path=" + probe3.path);
+            console.info("probe3.filePath=" + probe3.filePath);
+            return name;
+        }
     }
 
     Probes.PathProbe {
-        id: probe2
+        id: probe3
         names: ["tool"]
-        platformSearchPaths: [product.sourceDirectory + "/bin"]
+        platformSearchPaths: [project.sourceDirectory + "/bin"]
     }
 
-    targetName: {
-        console.info("probe1.fileName=" + probe1.fileName);
-        console.info("probe1.path=" + probe1.path);
-        console.info("probe1.filePath=" + probe1.filePath);
-
-        console.info("probe2.fileName=" + probe2.fileName);
-        console.info("probe2.path=" + probe2.path);
-        console.info("probe2.filePath=" + probe2.filePath);
-
-        return name;
-    }
-
-    files: ["main.c"]
 }

tests/auto/blackbox/tst_blackbox.cpp
@@ -3009,6 +3009,9 @@ void TestBlackbox::probeProperties()
     QVERIFY2(m_qbsStdout.contains("probe2.fileName=tool"), m_qbsStdout.constData());
     QVERIFY2(m_qbsStdout.contains("probe2.path=" + dir + "/bin"), m_qbsStdout.constData());
     QVERIFY2(m_qbsStdout.contains("probe2.filePath=" + dir + "/bin/tool"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe3.fileName=tool"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe3.path=" + dir + "/bin"), m_qbsStdout.constData());
+    QVERIFY2(m_qbsStdout.contains("probe3.filePath=" + dir + "/bin/tool"), m_qbsStdout.constData());
 }
 
 void TestBlackbox::probesAndShadowProducts()