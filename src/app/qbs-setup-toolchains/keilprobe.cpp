cbe49235025e3134f93f2be898c6c2a8af702937
Denis Shienkov <denis.shienkov@gmail.com>
2020-04-02 17:35:01 +0300

    baremetal: Move Keil compiler version detection to separate functions
    ... for a compilers specified as a paths.
    
    Change-Id: I039c7cb51bab4b8d44fee7b921f8ab837cf9470f
    Reviewed-by: Ivan Komissarov <ABBAPOH@gmail.com>
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/app/qbs-setup-toolchains/keilprobe.cpp
+++ src/app/qbs-setup-toolchains/keilprobe.cpp
@@ -103,74 +103,83 @@ static Profile createKeilProfileHelper(const ToolchainInstallInfo &info,
     return profile;
 }
 
+static Version dumpMcsCompilerVersion(const QFileInfo &compiler)
+{
+    QTemporaryFile fakeIn;
+    if (!fakeIn.open()) {
+        qbsWarning() << Tr::tr("Unable to open temporary file %1 for output: %2")
+                        .arg(fakeIn.fileName(), fakeIn.errorString());
+        return Version{};
+    }
+    fakeIn.write("#define VALUE_TO_STRING(x) #x\n");
+    fakeIn.write("#define VALUE(x) VALUE_TO_STRING(x)\n");
+    fakeIn.write("#define VAR_NAME_VALUE(var) \"\"\"|\"#var\"|\"VALUE(var)\n");
+    fakeIn.write("#ifdef __C51__\n");
+    fakeIn.write("#pragma message(VAR_NAME_VALUE(__C51__))\n");
+    fakeIn.write("#endif\n");
+    fakeIn.write("#ifdef __CX51__\n");
+    fakeIn.write("#pragma message(VAR_NAME_VALUE(__CX51__))\n");
+    fakeIn.write("#endif\n");
+    fakeIn.close();
+
+    const QStringList args = {fakeIn.fileName()};
+    QProcess p;
+    p.start(compiler.absoluteFilePath(), args);
+    p.waitForFinished(3000);
+    const auto es = p.exitStatus();
+    if (es != QProcess::NormalExit) {
+        const QByteArray out = p.readAll();
+        qbsWarning() << Tr::tr("Compiler dumping failed:\n%1")
+                        .arg(QString::fromUtf8(out));
+        return Version{};
+    }
+
+    const QByteArray dump = p.readAllStandardOutput();
+    const int verCode = extractVersion(dump, "\"__C51__\"|\"");
+    if (verCode < 0) {
+        qbsWarning() << Tr::tr("No '__C51__' token was found"
+                               " in the compiler dump:\n%1")
+                        .arg(QString::fromUtf8(dump));
+        return Version{};
+    }
+    return Version{verCode / 100, verCode % 100};
+}
+
+static Version dumpArmCompilerVersion(const QFileInfo &compiler)
+{
+    const QStringList args = {QStringLiteral("-E"),
+                              QStringLiteral("--list-macros"),
+                              QStringLiteral("nul")};
+    QProcess p;
+    p.start(compiler.absoluteFilePath(), args);
+    p.waitForFinished(3000);
+    const auto es = p.exitStatus();
+    if (es != QProcess::NormalExit) {
+        const QByteArray out = p.readAll();
+        qbsWarning() << Tr::tr("Compiler dumping failed:\n%1")
+                        .arg(QString::fromUtf8(out));
+        return Version{};
+    }
+
+    const QByteArray dump = p.readAll();
+    const int verCode = extractVersion(dump, "__ARMCC_VERSION ");
+    if (verCode < 0) {
+        qbsWarning() << Tr::tr("No '__ARMCC_VERSION' token was found "
+                               "in the compiler dump:\n%1")
+                        .arg(QString::fromUtf8(dump));
+        return Version{};
+    }
+    return Version{verCode / 1000000, (verCode / 10000) % 100, verCode % 10000};
+}
+
 static Version dumpKeilCompilerVersion(const QFileInfo &compiler)
 {
     const QString arch = guessKeilArchitecture(compiler);
     if (arch == QLatin1String("mcs51")) {
-        QTemporaryFile fakeIn;
-        if (!fakeIn.open()) {
-            qbsWarning() << Tr::tr("Unable to open temporary file %1 for output: %2")
-                            .arg(fakeIn.fileName(), fakeIn.errorString());
-            return Version{};
-        }
-        fakeIn.write("#define VALUE_TO_STRING(x) #x\n");
-        fakeIn.write("#define VALUE(x) VALUE_TO_STRING(x)\n");
-        fakeIn.write("#define VAR_NAME_VALUE(var) \"\"\"|\"#var\"|\"VALUE(var)\n");
-        fakeIn.write("#ifdef __C51__\n");
-        fakeIn.write("#pragma message(VAR_NAME_VALUE(__C51__))\n");
-        fakeIn.write("#endif\n");
-        fakeIn.write("#ifdef __CX51__\n");
-        fakeIn.write("#pragma message(VAR_NAME_VALUE(__CX51__))\n");
-        fakeIn.write("#endif\n");
-        fakeIn.close();
-
-        const QStringList args = {fakeIn.fileName()};
-        QProcess p;
-        p.start(compiler.absoluteFilePath(), args);
-        p.waitForFinished(3000);
-        const auto es = p.exitStatus();
-        if (es != QProcess::NormalExit) {
-            const QByteArray out = p.readAll();
-            qbsWarning() << Tr::tr("Compiler dumping failed:\n%1")
-                            .arg(QString::fromUtf8(out));
-            return Version{};
-        }
-
-        const QByteArray dump = p.readAllStandardOutput();
-        const int verCode = extractVersion(dump, "\"__C51__\"|\"");
-        if (verCode < 0) {
-            qbsWarning() << Tr::tr("No '__C51__' token was found"
-                                   " in the compiler dump:\n%1")
-                            .arg(QString::fromUtf8(dump));
-            return Version{};
-        }
-        return Version{verCode / 100, verCode % 100};
+        return dumpMcsCompilerVersion(compiler);
     } else if (arch == QLatin1String("arm")) {
-        const QStringList args = {QStringLiteral("-E"),
-                                  QStringLiteral("--list-macros"),
-                                  QStringLiteral("nul")};
-        QProcess p;
-        p.start(compiler.absoluteFilePath(), args);
-        p.waitForFinished(3000);
-        const auto es = p.exitStatus();
-        if (es != QProcess::NormalExit) {
-            const QByteArray out = p.readAll();
-            qbsWarning() << Tr::tr("Compiler dumping failed:\n%1")
-                            .arg(QString::fromUtf8(out));
-            return Version{};
-        }
-
-        const QByteArray dump = p.readAll();
-        const int verCode = extractVersion(dump, "__ARMCC_VERSION ");
-        if (verCode < 0) {
-            qbsWarning() << Tr::tr("No '__ARMCC_VERSION' token was found "
-                                   "in the compiler dump:\n%1")
-                            .arg(QString::fromUtf8(dump));
-            return Version{};
-        }
-        return Version{verCode / 1000000, (verCode / 10000) % 100, verCode % 10000};
+        return dumpArmCompilerVersion(compiler);
     }
-
     return Version{};
 }
 
