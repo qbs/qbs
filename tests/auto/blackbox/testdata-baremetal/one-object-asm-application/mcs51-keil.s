36afe8a841567987d618c184d252a564937b1d5e
Denis Shienkov <denis.shienkov@gmail.com>
2020-07-07 15:57:16 +0300

    baremetal: Return 0 in ASM main function for KEIL MCS51 toolchain
    Change-Id: I18ade9e8382c539ba1a29268f7101e656793541f
    Reviewed-by: Ivan Komissarov <ABBAPOH@gmail.com>

--- tests/auto/blackbox/testdata-baremetal/one-object-asm-application/mcs51-keil.s
+++ tests/auto/blackbox/testdata-baremetal/one-object-asm-application/mcs51-keil.s
@@ -1,7 +1,8 @@
-PUBLIC  main
-
+PUBLIC      main
 MAIN_SEG    SEGMENT CODE
             RSEG    MAIN_SEG
 main:
+            MOV     R6, #0x0
+            MOV     R7, #0x0
             RET
-END
+            END
