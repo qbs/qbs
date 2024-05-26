1111c53af3f76e1dc903eeaf2bfab0a7c49d2cb5
Ivan Komissarov <abbapoh@gmail.com>
2024-05-26 15:08:45 +0300

    Remove useless code from grpc test
    Change-Id: I9333a1ec4473a74d15cb71db841d2673ed992af2
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- tests/auto/blackbox/tst_blackbox.cpp
+++ tests/auto/blackbox/tst_blackbox.cpp
@@ -8719,13 +8719,6 @@ void TestBlackbox::grpc()
 
     rmDirR(relativeBuildDir());
     QbsRunParameters resolveParams("resolve", QStringList{"-f", projectFile});
-    if (QTest::currentDataTag() == QLatin1String("cpp")) {
-        if (const QString extraLibs = qEnvironmentVariable("QBS_EXTRA_GRPC_LIBS");
-                !extraLibs.isEmpty()) {
-            resolveParams.arguments << (QLatin1String("modules.protobuf.cpp._extraGrpcLibs:")
-                                        + extraLibs);
-        }
-    }
     resolveParams.arguments << arguments;
     QCOMPARE(runQbs(resolveParams), 0);
     const bool withGrpc = m_qbsStdout.contains("has grpc: true");
